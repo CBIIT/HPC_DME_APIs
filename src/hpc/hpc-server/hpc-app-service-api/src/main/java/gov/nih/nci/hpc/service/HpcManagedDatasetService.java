74cfc38581c15fa8ad3ffeb77038dee546962621
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-05-11 11:25:54 +0000

    SVN-Revision: 6

---
+++ src/hpc/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcManagedDatasetService.java
@@ -0,0 +1,38 @@
+/**
+ * HpcManagedDatasetService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.service;
+
+import gov.nih.nci.hpc.dto.HpcDatasetRegistrationInputDTO;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * <p>
+ * HPC Managed Dataset Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public interface HpcManagedDatasetService 
+{         
+    /**
+     * Add managed dataset.
+     *
+     * @param registrationInputDTO The dataset registration input DTO.
+     * 
+     * @throws HpcException
+     */
+    public void add(HpcDatasetRegistrationInputDTO registrationInputDTO)
+    		       throws HpcException;
+}
+
+ 
