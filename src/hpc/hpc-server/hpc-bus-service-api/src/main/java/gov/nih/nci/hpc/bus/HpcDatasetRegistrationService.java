5b975b6d24f23fd523eec2f6a830c8e1947de88c
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-05-11 12:00:42 +0000

    SVN-Revision: 9

---
+++ src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDatasetRegistrationService.java
@@ -0,0 +1,39 @@
+/**
+ * HpcDatasetRegistrationService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.bus;
+
+import gov.nih.nci.hpc.dto.HpcDatasetRegistrationInputDTO;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * <p>
+ * HPC Dataset Registration Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public interface HpcDatasetRegistrationService 
+{         
+    /**
+     * Register Dataset.
+     *
+     * @param registrationInputDTO The dataset registration input DTO.
+     * 
+     * @throws HpcException
+     */
+    public void registerDataset(
+    		            HpcDatasetRegistrationInputDTO registrationInputDTO)
+    		            throws HpcException;
+}
+
+ 
---
+++ src/hpc/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcDatasetRegistrationServiceImpl.java
@@ -0,0 +1,96 @@
+/**
+ * HpcDatasetRegistrationServiceImpl.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.bus.impl;
+
+import gov.nih.nci.hpc.bus.HpcDatasetRegistrationService;
+import gov.nih.nci.hpc.service.HpcManagedDatasetService;
+import gov.nih.nci.hpc.dto.HpcDatasetRegistrationInputDTO;
+import gov.nih.nci.hpc.exception.HpcException;
+import gov.nih.nci.hpc.exception.HpcErrorType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * HPC Dataset Service Implementation.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public class HpcDatasetRegistrationServiceImpl 
+             implements HpcDatasetRegistrationService
+{         
+    //---------------------------------------------------------------------//
+    // Instance members
+    //---------------------------------------------------------------------//
+
+    // The Managed Dataset application service instance.
+    private HpcManagedDatasetService managedDatasetService = null;
+    
+    // The logger instance.
+	private final Logger logger = 
+			             LoggerFactory.getLogger(this.getClass().getName());
+	
+    //---------------------------------------------------------------------//
+    // Constructors
+    //---------------------------------------------------------------------//
+	
+    /**
+     * Default Constructor.
+     * 
+     * @throws HpcException Constructor is disabled.
+     */
+    private HpcDatasetRegistrationServiceImpl() throws HpcException
+    {
+    	throw new HpcException("Constructor Disabled",
+                               HpcErrorType.SPRING_CONFIGURATION_ERROR);
+    }   
+    
+    /**
+     * Constructor for Spring Dependency Injection.
+     * 
+     * @param managedDatasetService The managed dataset application service.
+     * 
+     * @throws HpcException If managedDatasetService is null.
+     */
+    private HpcDatasetRegistrationServiceImpl(
+    		          HpcManagedDatasetService managedDatasetService)
+                      throws HpcException
+    {
+    	if(managedDatasetService == null) {
+     	   throw new HpcException("Null HpcManagedDatasetService instance",
+     			                  HpcErrorType.SPRING_CONFIGURATION_ERROR);
+     	}
+    	
+    	this.managedDatasetService = managedDatasetService;
+    }  
+    
+    //---------------------------------------------------------------------//
+    // Methods
+    //---------------------------------------------------------------------//
+    
+    //---------------------------------------------------------------------//
+    // HpcDatasetRegistrationService Interface Implementation
+    //---------------------------------------------------------------------//  
+    
+    @Override
+    public void registerDataset(
+                        HpcDatasetRegistrationInputDTO registrationInputDTO)
+                        throws HpcException
+    {
+    	// TODO: managedDatasetService.
+    }
+}
+
+ 
