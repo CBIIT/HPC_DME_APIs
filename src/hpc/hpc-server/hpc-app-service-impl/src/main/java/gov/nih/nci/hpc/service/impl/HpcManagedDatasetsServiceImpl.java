90362b6293a1fba320be0cbf643a2d33156709ab
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-05-13 01:48:13 +0000

    SVN-Revision: 39

--- src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcManagedDatasetsServiceImpl.java
+++ src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcManagedDatasetsServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * HpcManagedDatasetServiceImpl.java
+ * HpcManagedDatasetsServiceImpl.java
  *
  * Copyright SVG, Inc.
  * Copyright Leidos Biomedical Research, Inc
@@ -8,34 +8,37 @@
  * See http://ncip.github.com/HPC/LICENSE.txt for details.
  */
 
-package gov.nih.nci.hpc.bus.impl;
+package gov.nih.nci.hpc.service.impl;
 
-import gov.nih.nci.hpc.service.HpcManagedDatasetService;
-import gov.nih.nci.hpc.dto.api.HpcDatasetsRegistrationInputDTO;
-import gov.nih.nci.hpc.dao.HpcManagedDatasetDAO;
+import gov.nih.nci.hpc.service.HpcManagedDatasetsService;
+import gov.nih.nci.hpc.dto.types.HpcDataset;
+import gov.nih.nci.hpc.dao.HpcManagedDatasetsDAO;
 import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.exception.HpcErrorType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 /**
  * <p>
- * HPC Dataset Service Implementation.
+ * HPC Managed Datasets Application Service Implementation.
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  * @version $Id$
  */
 
-public class HpcManagedDatasetServiceImpl implements HpcManagedDatasetService
+public class HpcManagedDatasetsServiceImpl 
+             implements HpcManagedDatasetsService
 {         
     //---------------------------------------------------------------------//
     // Instance members
     //---------------------------------------------------------------------//
 
-    // The Managed Dataset DAO instance.
-    private HpcManagedDatasetDAO managedDatasetDAO = null;
+    // The Managed Datasets DAO instance.
+    private HpcManagedDatasetsDAO managedDatasetsDAO = null;
     
     // The logger instance.
 	private final Logger logger = 
@@ -50,7 +53,7 @@ public class HpcManagedDatasetServiceImpl implements HpcManagedDatasetService
      * 
      * @throws HpcException Constructor is disabled.
      */
-    private HpcManagedDatasetServiceImpl() throws HpcException
+    private HpcManagedDatasetsServiceImpl() throws HpcException
     {
     	throw new HpcException("Constructor Disabled",
                                HpcErrorType.SPRING_CONFIGURATION_ERROR);
@@ -61,16 +64,16 @@ public class HpcManagedDatasetServiceImpl implements HpcManagedDatasetService
      * 
      * @param managedDatasetDAO The managed dataset DAO instance.
      */
-    private HpcManagedDatasetServiceImpl(
-    		                        HpcManagedDatasetDAO managedDatasetDAO)
-                                    throws HpcException
+    private HpcManagedDatasetsServiceImpl(
+    		                  HpcManagedDatasetsDAO managedDatasetsDAO)
+                              throws HpcException
     {
-    	if(managedDatasetDAO == null) {
-     	   throw new HpcException("Null HpcManagedDatasetDAO instance",
+    	if(managedDatasetsDAO == null) {
+     	   throw new HpcException("Null HpcManagedDatasetsDAO instance",
      			                  HpcErrorType.SPRING_CONFIGURATION_ERROR);
      	}
     	
-    	this.managedDatasetDAO = managedDatasetDAO;
+    	this.managedDatasetsDAO = managedDatasetsDAO;
     }  
     
     //---------------------------------------------------------------------//
@@ -78,14 +81,13 @@ public class HpcManagedDatasetServiceImpl implements HpcManagedDatasetService
     //---------------------------------------------------------------------//
     
     //---------------------------------------------------------------------//
-    // HpcManagedDatasetService Interface Implementation
+    // HpcManagedDatasetsService Interface Implementation
     //---------------------------------------------------------------------//  
     
     @Override
-    public void add(HpcDatasetsRegistrationInputDTO registrationInputDTO)
-		           throws HpcException
+    public void add(List<HpcDataset> datasets) throws HpcException
     {
-    	
+    	// TODO - Implement me
     }
 }
 
