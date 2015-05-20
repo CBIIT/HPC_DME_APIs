30334a2cef8dd126a20708ad58b18e11e0d996ff
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-05-20 23:45:52 +0000

    SVN-Revision: 102

--- src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataRegistrationService.java
+++ src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataRegistrationService.java
@@ -28,12 +28,13 @@ public interface HpcDataRegistrationService
      * Register Datasets.
      *
      * @param registrationInputDTO The datas registration input DTO.
+     * @return The registered data ID.
      * 
      * @throws HpcException
      */
-    public void registerData(
-    		            HpcDataRegistrationInputDTO registrationInputDTO)
-    		            throws HpcException;
+    public String registerData(
+    		              HpcDataRegistrationInputDTO registrationInputDTO)
+    		              throws HpcException;
 }
 
  
