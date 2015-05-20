16f0b0a36d7005b1528466b5d3950932145fc2be
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-05-20 02:54:30 +0000

    SVN-Revision: 89

--- src/hpc/hpc-server/hpc-dao-mongo-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcManagedDataCodec.java
+++ src/hpc/hpc-server/hpc-dao-mongo-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcManagedDataCodec.java
@@ -1,5 +1,5 @@
 /**
- * HpcManagedDatasetsCodec.java
+ * HpcManagedDataCodec.java
  *
  * Copyright SVG, Inc.
  * Copyright Leidos Biomedical Research, Inc
@@ -11,7 +11,8 @@
 package gov.nih.nci.hpc.dao.mongo.codec;
 
 import gov.nih.nci.hpc.dto.types.HpcDataset;
-import gov.nih.nci.hpc.domain.HpcManagedDatasets;
+import gov.nih.nci.hpc.dto.types.HpcManagedDataType;
+import gov.nih.nci.hpc.domain.HpcManagedData;
 
 import org.bson.BsonReader;
 import org.bson.BsonString;
@@ -24,24 +25,28 @@ import org.bson.codecs.EncoderContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Calendar;
 import java.util.List;
 
 /**
  * <p>
- * HPC Managed Datasets Codec. 
+ * HPC Managed Data Codec. 
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  * @version $Id$
  */
 
-public class HpcManagedDatasetsCodec extends HpcCodec<HpcManagedDatasets>
+public class HpcManagedDataCodec extends HpcCodec<HpcManagedData>
 { 
     //---------------------------------------------------------------------//
     // Constants
     //---------------------------------------------------------------------//    
     
     // BSON Document keys.
+	private final static String ID_KEY = "id"; 
+	private final static String TYPE_KEY = "type"; 
+	private final static String CREATED_KEY = "created"; 
     private final static String DATASETS_KEY = "datasets"; 
     
     //---------------------------------------------------------------------//
@@ -60,7 +65,7 @@ public class HpcManagedDatasetsCodec extends HpcCodec<HpcManagedDatasets>
      * Default Constructor.
      * 
      */
-    public HpcManagedDatasetsCodec()
+    public HpcManagedDataCodec()
     {
     }   
     
@@ -69,52 +74,69 @@ public class HpcManagedDatasetsCodec extends HpcCodec<HpcManagedDatasets>
     //---------------------------------------------------------------------//
     
     //---------------------------------------------------------------------//
-    // Codec<HpcManagedDatasets> Interface Implementation
+    // Codec<HpcManagedData> Interface Implementation
     //---------------------------------------------------------------------//  
     
 	@Override
-	public void encode(BsonWriter writer, 
-					   HpcManagedDatasets managedDatasets,
+	public void encode(BsonWriter writer, HpcManagedData managedData,
 					   EncoderContext encoderContext) 
 	{
 		Document document = new Document();
  
 		// Extract the data from the domain object.
-		List<HpcDataset> datasets = managedDatasets.getDatasets();
+		String id = managedData.getId();
+		HpcManagedDataType type = managedData.getType();
+		Calendar created = managedData.getCreated();
+		List<HpcDataset> datasets = managedData.getDatasets();
  
 		// Set the data on the BSON document.
+		if(id != null) {
+		   document.put(ID_KEY, id);
+		}
+		if(type != null) {
+		   document.put(TYPE_KEY, type.value());
+		}
+		if(created != null) {
+		   document.put(CREATED_KEY, created);
+		}
 		if(datasets != null && datasets.size() > 0) {
 		   document.put(DATASETS_KEY, datasets);
 		}
 
-		getRegistry().get(Document.class).encode(writer, document, encoderContext);
+		getRegistry().get(Document.class).encode(writer, document, 
+				                                 encoderContext);
 	}
  
 	@Override
-	public HpcManagedDatasets decode(BsonReader reader, 
-			                         DecoderContext decoderContext) 
+	public HpcManagedData decode(BsonReader reader, 
+			                     DecoderContext decoderContext) 
 	{
 		// Get the BSON Document.
 		Document document = 
-				 getRegistry().get(Document.class).decode(reader, decoderContext);
+				 getRegistry().get(Document.class).decode(reader, 
+						                                  decoderContext);
 		
 		// Map the BSON Document to a domain object.
-		HpcManagedDatasets managedDatasets = new HpcManagedDatasets();
+		HpcManagedData managedData = new HpcManagedData();
+		managedData.setId(document.get(ID_KEY, String.class));
+		managedData.setType( 
+		       HpcManagedDataType.fromValue(document.get(TYPE_KEY, String.class)));
+		managedData.setCreated(document.get(CREATED_KEY, Calendar.class));
 		List<HpcDataset> datasets = 
 				         (List<HpcDataset>) document.get(DATASETS_KEY);
 		if(datasets != null) {
 		   for(HpcDataset dataset : datasets) {
-			   managedDatasets.getDatasets().add(dataset);
+			   managedData.getDatasets().add(dataset);
 		   }
 		}
 		
-		return managedDatasets;
+		return managedData;
 	}
 	
 	@Override
-	public Class<HpcManagedDatasets> getEncoderClass() 
+	public Class<HpcManagedData> getEncoderClass() 
 	{
-		return HpcManagedDatasets.class;
+		return HpcManagedData.class;
 	}
 }
 
