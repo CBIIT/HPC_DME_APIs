92e5440f5d01866c9867fa3038563ab6d7ca709b
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-06-07 23:22:12 +0000

    SVN-Revision: 159

---
+++ src/hpc/hpc-server/hpc-client/src/main/java/gov/nih/nci/hpc/client/impl/HpcProxyImpl.java
@@ -0,0 +1,82 @@
+/**
+ * HpcProxyImpl.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.client.impl;
+
+import gov.nih.nci.hpc.client.HpcProxy;
+import gov.nih.nci.hpc.ws.rs.HpcDataRegistrationRestService;
+
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+
+/**
+ * <p>
+ * HPC Proxy Implementation.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public class HpcProxyImpl implements HpcProxy
+{   
+    //---------------------------------------------------------------------//
+    // Instance members
+    //---------------------------------------------------------------------//
+
+    // The Data Registration Proxy instance.
+    private String baseAddress = null;
+    
+    //---------------------------------------------------------------------//
+    // constructors
+    //---------------------------------------------------------------------//
+     
+    /**
+     * Default Constructor.
+     * 
+     * @throws HpcException Constructor is disabled.
+     */
+    private HpcProxyImpl()
+    {
+    	throw new RuntimeException("Constructor Disabled");
+    }  
+    
+    /**
+     * Constructor for Spring Dependency Injection.
+     * 
+     * @param baseAdress The service base address.
+     * 
+     * @throws HpcException If the base address is not provided by Spring.
+     */
+    private HpcProxyImpl(String baseAddress)
+    {
+    	if(baseAddress == null || baseAddress.isEmpty()) {
+    	   throw new RuntimeException("Null or empty base address");
+    	}
+    	
+    	this.baseAddress = baseAddress;
+    }  
+    
+    //---------------------------------------------------------------------//
+    // Methods
+    //---------------------------------------------------------------------//
+    
+    //---------------------------------------------------------------------//
+    // HpcProxy Interface Implementation
+    //---------------------------------------------------------------------//  
+	
+    @Override
+    public HpcDataRegistrationRestService getRegistrationServiceProxy()
+    {
+    	return JAXRSClientFactory.create(baseAddress, 
+    			                         HpcDataRegistrationRestService.class);
+	}
+}
+
+ 
