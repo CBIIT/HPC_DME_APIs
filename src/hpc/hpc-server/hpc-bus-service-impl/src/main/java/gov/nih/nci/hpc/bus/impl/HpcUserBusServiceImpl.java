bf94f74bb6b9ee9248180d56c603471bf2c941ac
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-06-19 22:46:05 +0000

    SVN-Revision: 228

---
+++ src/hpc/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcUserBusServiceImpl.java
@@ -0,0 +1,132 @@
+/**
+ * HpcUserBusServiceImpl.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.bus.impl;
+
+import gov.nih.nci.hpc.bus.HpcUserBusService;
+import gov.nih.nci.hpc.service.HpcManagedUserService;
+import gov.nih.nci.hpc.dto.user.HpcUserRegistrationDTO;
+import gov.nih.nci.hpc.dto.user.HpcUserDTO;
+import gov.nih.nci.hpc.domain.model.HpcManagedUser;
+import gov.nih.nci.hpc.exception.HpcException;
+import gov.nih.nci.hpc.exception.HpcErrorType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * HPC User Business Service Implementation.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public class HpcUserBusServiceImpl implements HpcUserBusService
+{         
+    //---------------------------------------------------------------------//
+    // Instance members
+    //---------------------------------------------------------------------//
+
+    // Application service instances.
+    private HpcManagedUserService managedUserService = null;
+    
+    // The logger instance.
+	private final Logger logger = 
+			             LoggerFactory.getLogger(this.getClass().getName());
+	
+    //---------------------------------------------------------------------//
+    // Constructors
+    //---------------------------------------------------------------------//
+	
+    /**
+     * Default Constructor.
+     * 
+     * @throws HpcException Constructor is disabled.
+     */
+    private HpcUserBusServiceImpl() throws HpcException
+    {
+    	throw new HpcException("Constructor Disabled",
+                               HpcErrorType.SPRING_CONFIGURATION_ERROR);
+    }   
+    
+    /**
+     * Constructor for Spring Dependency Injection.
+     * 
+     * @param managedUserService The managed user application service.
+     * 
+     * @throws HpcException If managedUserService is null.
+     */
+    private HpcUserBusServiceImpl(HpcManagedUserService managedUserService)
+                                 throws HpcException
+    {
+    	if(managedUserService == null) {
+     	   throw new HpcException("Null App Service(s) instance",
+     			                  HpcErrorType.SPRING_CONFIGURATION_ERROR);
+     	}
+    	
+    	this.managedUserService = managedUserService;
+    } 
+    
+    //---------------------------------------------------------------------//
+    // Methods
+    //---------------------------------------------------------------------//
+    
+    //---------------------------------------------------------------------//
+    // HpcUserBusService Interface Implementation
+    //---------------------------------------------------------------------//  
+    
+    @Override
+    public String registerUser(HpcUserRegistrationDTO userRegistrationDTO)  
+    		                     throws HpcException
+    {
+    	logger.info("Invoking registerDataset(HpcDatasetDTO): " + 
+                    userRegistrationDTO);
+    	
+    	// Input validation.
+    	if(userRegistrationDTO == null) {
+    	   throw new HpcException("Null HpcUserRegistrationDTO",
+    			                  HpcErrorType.INVALID_INPUT);	
+    	}
+    	
+    	// Add the user to the managed collection.
+    	return managedUserService.add(userRegistrationDTO.getUser());
+    }
+    
+    @Override
+    public HpcUserDTO getUser(String nihUserId) throws HpcException
+    {
+    	logger.info("Invoking getDataset(String nihUserId): " + nihUserId);
+    	
+    	// Input validation.
+    	if(nihUserId == null) {
+    	   throw new HpcException("Null NIH User ID",
+    			                  HpcErrorType.INVALID_INPUT);	
+    	}
+    	
+    	// Get the managed data domain object.
+    	HpcManagedUser managedUser = managedUserService.get(nihUserId);
+    	if(managedUser == null) {
+    	   return null;
+    	}
+    	
+    	// Map it to the DTO.
+    	HpcUserDTO userDTO = new HpcUserDTO();
+    	userDTO.setId(managedUser.getId());
+    	userDTO.setUser(managedUser.getUser());
+    	userDTO.setCreated(managedUser.getCreated());
+    	userDTO.setLastUpdated(managedUser.getLastUpdated());
+    	
+    	return userDTO;
+    }
+}
+
+ 
