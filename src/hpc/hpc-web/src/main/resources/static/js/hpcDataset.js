09403e3c420b33db800d6f2f8e231976fe44bb16
Prasad Konka <prasad.konka@nih.gov>
2015-06-22 17:25:42 +0000

    Integration with hpc-server for Sprint 2
    SVN-Revision: 251

--- src/hpc/hpc-web/src/main/resources/static/js/hpcDataset.js
+++ src/hpc/hpc-web/src/main/resources/static/js/hpcDataset.js
@@ -1,12 +1,17 @@
 var app = angular.module('myApp', ['ngGrid']);
 var linkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
-'  <a href="dataset?id={{row.getProperty(\'name\')}}">{{row.getProperty(col.field)}}</a>' +
+'  <a href="dataset?id={{row.getProperty(\'id\')}}">{{row.getProperty(col.field)}}</a>' +
 '</div>';
-app.controller('MyCtrl', function($scope, $http) {
-	$http.get('http://localhost:7737/hpc-server/dataset/96999fbf-5ec1-4e16-9dc5-e05cfc54f63e').
+app.controller('MyCtrl', function($scope, $http, $q, $attrs) {
+	var deferred = $q.defer();
+	//$http({method: 'GET', url: '/users', params: {'limit': $scope.pagingOptions.pageSize, 'page': $scope.pagingOptions.currentPage}})
+	$http.get('http://localhost:7737/hpc-server/dataset/?creatorId=pkonka').
+	//$http.get('/js/hpcDatasets.json').
 	  success(function(data, status, headers, config) {
-				console.log('Success', data["gov.nih.nci.hpc.dto.datasetregistration.HpcDatasetDTO"]);
-				$scope.hpcData = data["gov.nih.nci.hpc.dto.datasetregistration.HpcDatasetDTO"];
+		        var datasets = data["gov.nih.nci.hpc.dto.dataset.HpcDatasetDTO"];
+				console.log('Success', datasets["datasets"]);
+				$scope.hpcData = datasets["datasets"];
+				deferred.resolve($scope.hpcData);
 	  }).
 	  error(function(data, status, headers, config) {
 		// called asynchronously if an error occurs
@@ -21,27 +26,27 @@ $scope.gridOptions = {
         showSelectionCheckbox: false,
         selectedItems:$scope.selectedRows,
         columnDefs: [{
-           field: 'name',
-           displayName: 'Dataset Name',
-           enableCellEdit: false,
-           cellTemplate: linkCellTemplate
-        }, {
-           field: 'primaryInvestigatorId',
-           displayName: 'PI Id',
-           enableCellEdit: false
-        }, {
-           field: 'creatorId',
-           displayName: 'Creator Id',
-           enableCellEdit: false
-        },
-        {
-           field: 'files.length',
-           displayName: 'Number of files',
-           enableCellEdit: false
-        },        {
-           field: 'created',
-           displayName: 'Created Date',
-           enableCellEdit: false
-        }]
-      };
+            field: 'name',
+            displayName: 'Dataset Name',
+            enableCellEdit: false,
+            cellTemplate: linkCellTemplate
+         }, {
+            field: 'primaryInvestigatorId',
+            displayName: 'PI Id',
+            enableCellEdit: false
+         }, {
+            field: 'creatorId',
+            displayName: 'Creator Id',
+            enableCellEdit: false
+         },
+         {
+            field: 'files.length',
+            displayName: 'Number of files',
+            enableCellEdit: false
+         },        {
+            field: 'created',
+            displayName: 'Created Date',
+            enableCellEdit: false
+         }]
+       };
 });
