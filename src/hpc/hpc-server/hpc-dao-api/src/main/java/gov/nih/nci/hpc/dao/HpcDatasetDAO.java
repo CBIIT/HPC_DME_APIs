286d7a3fb88a9a840796a46d21d47e983c18c1bb
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-06-28 16:54:14 +0000

    SVN-Revision: 255

--- src/hpc/hpc-server/hpc-dao-api/src/main/java/gov/nih/nci/hpc/dao/HpcDatasetDAO.java
+++ src/hpc/hpc-server/hpc-dao-api/src/main/java/gov/nih/nci/hpc/dao/HpcDatasetDAO.java
@@ -1,5 +1,5 @@
 /**
- * HpcManagedDatasetDAO.java
+ * HpcDatasetDAO.java
  *
  * Copyright SVG, Inc.
  * Copyright Leidos Biomedical Research, Inc
@@ -10,7 +10,7 @@
 
 package gov.nih.nci.hpc.dao;
 
-import gov.nih.nci.hpc.domain.model.HpcManagedDataset;
+import gov.nih.nci.hpc.domain.model.HpcDataset;
 import gov.nih.nci.hpc.domain.dataset.HpcDatasetUserAssociation;
 import gov.nih.nci.hpc.exception.HpcException;
 
@@ -18,46 +18,46 @@ import java.util.List;
 
 /**
  * <p>
- * HPC Managed Dataset DAO Interface.
+ * HPC Dataset DAO Interface.
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  * @version $Id$
  */
 
-public interface HpcManagedDatasetDAO 
+public interface HpcDatasetDAO 
 {         
     /**
-     * Add managed dataset to the repository.
+     * Add dataset to the repository.
      *
-     * @param managedDataset The managed dataset to add to the DB.
+     * @param dataset The managed dataset to add to the DB.
      * 
      * @throws HpcException
      */
-    public void add(HpcManagedDataset managedDataset) throws HpcException;
+    public void add(HpcDataset dataset) throws HpcException;
     
     /**
      * Get managed dataset from the repository by ID.
      *
-     * @param id the managed dataset ID.
-     * @return The managed dataset if found, or null otherwise.
+     * @param id The dataset ID.
+     * @return The dataset if found, or null otherwise.
      * 
      * @throws HpcException
      */
-    public HpcManagedDataset get(String id) throws HpcException;
+    public HpcDataset get(String id) throws HpcException;
     
     /**
      * Get managed datasets associated with a specific user.
      *
      * @param userId the user id.
      * @param association The association between the dataset and the user.
-     * @return ManagedDataset collection, or null if no results found.
+     * @return HpcDataset collection, or null if no results found.
      * 
      * @throws HpcException
      */
-    public List<HpcManagedDataset> 
-           get(String userId, HpcDatasetUserAssociation association) 
-        	  throws HpcException;
+    public List<HpcDataset> get(String userId, 
+    		                    HpcDatasetUserAssociation association) 
+        	                   throws HpcException;
 }
 
  
