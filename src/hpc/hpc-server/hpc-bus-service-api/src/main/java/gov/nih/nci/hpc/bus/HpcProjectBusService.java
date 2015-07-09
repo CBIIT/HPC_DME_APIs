012ef08656a74a02725f30e4bcb1468e40c8602c
Prasad Konka <prasad.konka@nih.gov>
2015-07-09 15:14:23 +0000

    ProejctAPI changes
    SVN-Revision: 307

---
+++ src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcProjectBusService.java
@@ -0,0 +1,67 @@
+/**
+ * HpcProjectBusService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.bus;
+
+import gov.nih.nci.hpc.dto.project.HpcProjectDTO;
+import gov.nih.nci.hpc.dto.project.HpcProjectRegistrationDTO;
+import gov.nih.nci.hpc.dto.project.HpcProjectCollectionDTO;
+import gov.nih.nci.hpc.domain.dataset.HpcDatasetUserAssociation;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * <p>
+ * HPC Project Business Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:prasad.konka@nih.gov">Prasad Konka</a>
+ * @version $Id:  $
+ */
+
+public interface HpcProjectBusService 
+{         
+    /**
+     * Register Project.
+     *
+     * @param projectDTO The project  DTO.
+     * @return The registered project ID.
+     * 
+     * @throws HpcException
+     */
+    public String registerProject(HpcProjectRegistrationDTO projectDTO) 
+    		                     throws HpcException;
+    
+    /**
+     * Get a Project by its ID.
+     *
+     * @param id The project id.
+     * @return The dataset DTO or null if not found.
+     * 
+     * @throws HpcException
+     */
+    public HpcProjectDTO getProject(String id) throws HpcException;
+    
+    /**
+     * Get datasets associated with a specific user.
+     *
+     * @param nihUserId the user id.
+     * @param association The association between the project and the user.
+     * @return Collection of Project DTO, or null if not found.
+     * 
+     * @throws HpcException
+     */
+    public HpcProjectCollectionDTO 
+              getProjects(String nihUserId, 
+    		              HpcDatasetUserAssociation association) 
+    		             throws HpcException;
+    
+}
+
+ 
