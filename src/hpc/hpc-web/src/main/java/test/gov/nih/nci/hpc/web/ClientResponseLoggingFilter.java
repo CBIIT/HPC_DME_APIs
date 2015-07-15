53e867ff72cb4a937b59682e04c227331aaa8a8f
Prasad Konka <prasad.konka@nih.gov>
2015-07-15 21:03:01 +0000

    LDAP Authentication service
    SVN-Revision: 334

---
+++ src/hpc/hpc-web/src/main/java/test/gov/nih/nci/hpc/web/ClientResponseLoggingFilter.java
@@ -0,0 +1,32 @@
+package test.gov.nih.nci.hpc.web;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
+ 
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
+ 
+public class ClientResponseLoggingFilter implements ClientResponseFilter {
+ 
+	@Override
+	public void filter(final ClientRequestContext reqCtx,
+			final ClientResponseContext resCtx) throws IOException {
+		System.out.println("status: " + resCtx.getStatus());
+		System.out.println("date: " + resCtx.getDate());
+		System.out.println("last-modified: " + resCtx.getLastModified());
+		System.out.println("location: " + resCtx.getLocation());
+		System.out.println("headers:");
+		for (Entry<String, List<String>> header : resCtx.getHeaders()
+				.entrySet()) {
+			System.out.print("\t" + header.getKey() + " :");
+			for (String value : header.getValue()) {
+				System.out.print(value + ", ");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("media-type: " + resCtx.getMediaType().getType());
+	}
+ 
+}
