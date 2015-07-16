a23d35685e5670ec586bc98979bd7d25142e1c0d
Prasad Konka <prasad.konka@nih.gov>
2015-07-16 14:37:14 +0000

    Sprint3 integration
    SVN-Revision: 338

--- src/hpc/hpc-web/src/main/java/test/gov/nih/nci/hpc/web/HpcProjectControllerTest.java
+++ src/hpc/hpc-web/src/main/java/test/gov/nih/nci/hpc/web/HpcProjectControllerTest.java
@@ -1,14 +1,21 @@
 package test.gov.nih.nci.hpc.web;
 
+import gov.nih.nci.hpc.domain.dataset.HpcDataTransferLocations;
+import gov.nih.nci.hpc.domain.dataset.HpcFileLocation;
+import gov.nih.nci.hpc.domain.dataset.HpcFileType;
+import gov.nih.nci.hpc.domain.dataset.HpcFileUploadRequest;
+import gov.nih.nci.hpc.domain.metadata.HpcFilePrimaryMetadata;
 import gov.nih.nci.hpc.domain.metadata.HpcProjectMetadata;
 import gov.nih.nci.hpc.dto.dataset.HpcDatasetDTO;
 import gov.nih.nci.hpc.dto.dataset.HpcDatasetRegistrationDTO;
+import gov.nih.nci.hpc.dto.project.HpcProjectCollectionDTO;
 import gov.nih.nci.hpc.dto.project.HpcProjectDTO;
 import gov.nih.nci.hpc.dto.project.HpcProjectRegistrationDTO;
 import gov.nih.nci.hpc.web.Application;
 
 import java.net.URL;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -29,21 +36,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest({ "server.port=8080" })
-public class HpcProjectRegistrationControllerTest {
+public class HpcProjectControllerTest {
 
 	@Value("${local.server.port}")
 	private int port;
+	//private final String baseurl = "http://fr-s-hpcdm-gp-d.ncifcrf.gov:8080/hpc-server/project";
+	private final String baseurl = "http://localhost:7737/hpc-server/project";
 
-	private URL base;
 	private RestTemplate template;
 
 	@Before
 	public void setUp() throws Exception {
-		this.base = new URL("http://localhost:7737/hpc-server/project");
 		template = new TestRestTemplate();
 	}
 
@@ -62,30 +71,77 @@ public class HpcProjectRegistrationControllerTest {
 		metadata.setPrimaryInvestigatorNihUserId("konkapv");
 		metadata.setRegistratorNihUserId("konkapv");
 		dto.setMetadata(metadata);
+		dto.getHpcDatasetRegistrationDTO().add(getDataset());
+		ObjectMapper mapper = new ObjectMapper();
+		 System.out.println(mapper.writeValueAsString(dto));		
 		Client client = ClientBuilder.newClient().register(
 				ClientResponseLoggingFilter.class);
 		Response res = client
-				.target("http://localhost:7737/hpc-server/project").request()
+				.target(baseurl).request()
 				.post(Entity.entity(dto, MediaType.APPLICATION_XML));
 		if (res.getStatus() != 201) {
 			throw new RuntimeException("Failed : HTTP error code : "
 					+ res.getStatus());
 		}
-		String projectId = (String) res.getEntity();
-		System.out.println("res: " + res);
 	}
 
+	private HpcDatasetRegistrationDTO getDataset()
+	{
+		HpcDatasetRegistrationDTO dto = new HpcDatasetRegistrationDTO();
+		dto.setName("Set1");
+		dto.setDescription("Set1 description");
+		dto.setComments("Set1 comments");
+
+		// TODO: Lookup Id
+		String files = "/~/C/globus-data/test2.fastq,/~/C/globus-data/test.fastq.docx";
+		StringTokenizer tokens = new StringTokenizer(files, ",");
+		while (tokens.hasMoreTokens()) {
+			HpcFileUploadRequest upload = new HpcFileUploadRequest();
+			HpcFileType fileType;
+			HpcDataTransferLocations locations = new HpcDataTransferLocations();
+			HpcFileLocation source = new HpcFileLocation();
+			source.setEndpoint("pkonka#hpc1234");
+			String filePath = tokens.nextToken();
+			source.setPath(filePath);
+			HpcFileLocation destination = new HpcFileLocation();
+			destination.setEndpoint("nihfnlcr#gridftp1");
+			destination.setPath(filePath);
+			locations.setDestination(destination);
+			locations.setSource(source);
+			upload.setLocations(locations);
+			// TODO: Identify file type
+			upload.setType(HpcFileType.UNKONWN);
+
+			// TODO: Metadata funding organization
+			HpcFilePrimaryMetadata metadata = new HpcFilePrimaryMetadata();
+			metadata.setDataEncrypted(false);
+			metadata.setDataContainsPII(false);
+			metadata.setDataCompressed(false);
+			metadata.setDataContainsPHI(false);
+			metadata.setFundingOrganization("NCI");
+			metadata.setPrimaryInvestigatorNihUserId("konkapv");
+			metadata.setRegistrarNihUserId("konkapv");
+			metadata.setDescription("Description");
+			// TODO: ID Lookup
+			metadata.setCreatorName("PRasad Konka");
+			metadata.setLabBranch("CCR");
+			upload.setMetadata(metadata);
+			dto.getUploadRequests().add(upload);
+		}		
+		return dto;
+	}
 	@Test
 	public void getProjectById() throws Exception {
 		try {
 			Client client = ClientBuilder.newClient().register(
 					ClientResponseLoggingFilter.class);
 			WebTarget resourceTarget = client
-					.target("http://localhost:7737/hpc-server/project/2fabe981-de44-4fb5-84fd-b74c4be7b978");
+					.target(baseurl+"/35a93600-e569-4f55-a7ad-4da3bdb7712c");
 			Invocation invocation = resourceTarget.request(
 					MediaType.APPLICATION_XML).buildGet();
 			HpcProjectDTO response = invocation.invoke(HpcProjectDTO.class);
-			System.out.println(response);
+			 ObjectMapper mapper = new ObjectMapper();
+			 System.out.println(mapper.writeValueAsString(response));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -94,16 +150,17 @@ public class HpcProjectRegistrationControllerTest {
 	}
 
 	@Test
-	public void getProjectByRegistrator() throws Exception {
+	public void getProjectsByRegistrator() throws Exception {
 		try {
 			Client client = ClientBuilder.newClient().register(
 					ClientResponseLoggingFilter.class);
 			WebTarget resourceTarget = client
-					.target("http://localhost:7737/hpc-server/project/query/registrator/konkapv");
+					.target(baseurl+"/query/registrator/konkapv");
 			Invocation invocation = resourceTarget.request(
 					MediaType.APPLICATION_XML).buildGet();
-			HpcProjectDTO response = invocation.invoke(HpcProjectDTO.class);
-			System.out.println(response);
+			HpcProjectCollectionDTO response = invocation.invoke(HpcProjectCollectionDTO.class);
+			 ObjectMapper mapper = new ObjectMapper();
+			 System.out.println(mapper.writeValueAsString(response));
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to get Project by Registrator");
@@ -111,16 +168,17 @@ public class HpcProjectRegistrationControllerTest {
 	}
 
 	@Test
-	public void getProjectByInvestigator() throws Exception {
+	public void getProjectsByInvestigator() throws Exception {
 		try {
 			Client client = ClientBuilder.newClient().register(
 					ClientResponseLoggingFilter.class);
 			WebTarget resourceTarget = client
-					.target("http://localhost:7737/hpc-server/project/query/investigator/konkapv");
+					.target(baseurl+"/query/investigator/konkapv");
 			Invocation invocation = resourceTarget.request(
 					MediaType.APPLICATION_XML).buildGet();
-			HpcProjectDTO response = invocation.invoke(HpcProjectDTO.class);
-			System.out.println(response);
+			HpcProjectCollectionDTO response = invocation.invoke(HpcProjectCollectionDTO.class);
+			 ObjectMapper mapper = new ObjectMapper();
+			 System.out.println(mapper.writeValueAsString(response));
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to get Project by Registrator");
