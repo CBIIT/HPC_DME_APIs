3d7eff3506f69d78f96fb7f9c3517d11f9ed8918
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-07-18 02:08:02 +0000

    SVN-Revision: 347

--- src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/provider/HpcAuthenticationExceptionMapper.java
+++ src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/provider/HpcAuthenticationExceptionMapper.java
@@ -1,5 +1,5 @@
 /**
- * HpcAccessDeniedExceptionMapper.java
+ * HpcAuthenticationExceptionMapper.java
  *
  * Copyright SVG, Inc.
  * Copyright Leidos Biomedical Research, Inc
@@ -8,33 +8,37 @@
  * See http://ncip.github.com/HPC/LICENSE.txt for details.
  */
 
-package gov.nih.nci.hpc.ws.rs.impl;
+package gov.nih.nci.hpc.ws.rs.provider;
 
 import gov.nih.nci.hpc.domain.error.HpcErrorType;
 import gov.nih.nci.hpc.dto.error.HpcExceptionDTO;
+import gov.nih.nci.hpc.exception.HpcAuthenticationException;
 
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
-
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-
 /**
  * <p>
- * Mapping AccessDeniedException to REST response.
+ * Mapping HPC Exceptions to REST response.
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  * @version $Id$
  */
 
-public class HpcAccessDeniedExceptionMapper implements ExceptionMapper<AuthenticationCredentialsNotFoundException> 
+public class HpcAuthenticationExceptionMapper 
+             implements ExceptionMapper<HpcAuthenticationException>
 {
-    public Response toResponse(AuthenticationCredentialsNotFoundException exception) 
+	@Context
+    private HttpHeaders headers;
+	
+    public Response toResponse(HpcAuthenticationException exception) 
     {
     	HpcExceptionDTO exceptionDTO = new HpcExceptionDTO();
     	exceptionDTO.setMessage("Access Denied: " + exception.getMessage());
     	exceptionDTO.setErrorType(HpcErrorType.REQUEST_AUTHENTICATION_FAILED);
     	
-        return Response.status(Response.Status.FORBIDDEN).entity(exceptionDTO).build();
+        return Response.status(Response.Status.UNAUTHORIZED).entity(exceptionDTO).type(headers.getMediaType()).build();
     }
 }
