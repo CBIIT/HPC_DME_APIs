6a1450be83f6e6b1531f81c4b78534b542c71794
Eran Rosenberg <eran.rosenberg@nih.gov>
2015-08-14 14:07:54 +0000

    SVN-Revision: 405

--- src/hpc/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcProjectBusServiceImpl.java
+++ src/hpc/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcProjectBusServiceImpl.java
@@ -13,9 +13,9 @@ package gov.nih.nci.hpc.bus.impl;
 import gov.nih.nci.hpc.bus.HpcDatasetBusService;
 import gov.nih.nci.hpc.bus.HpcProjectBusService;
 import gov.nih.nci.hpc.domain.dataset.HpcDatasetUserAssociation;
+import gov.nih.nci.hpc.domain.dataset.HpcFileUploadRequest;
 import gov.nih.nci.hpc.domain.error.HpcErrorType;
 import gov.nih.nci.hpc.domain.error.HpcRequestRejectReason;
-import gov.nih.nci.hpc.domain.metadata.HpcProjectMetadata;
 import gov.nih.nci.hpc.domain.model.HpcProject;
 import gov.nih.nci.hpc.domain.model.HpcUser;
 import gov.nih.nci.hpc.dto.dataset.HpcDatasetCollectionDTO;
@@ -60,7 +60,7 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
 	
 	// Dataset business service instance.
 	@Autowired
-    private HpcDatasetBusService datasetService = null;
+    private HpcDatasetBusService datasetBusService = null;
     
     // The logger instance.
 	private final Logger logger = 
@@ -88,41 +88,38 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
     //---------------------------------------------------------------------//  
     
     @Override
-    public String registerProject(HpcProjectRegistrationDTO projectDTO)  
+    public String registerProject(HpcProjectRegistrationDTO projectRegistrationDTO)  
     		                     throws HpcException
     {
     	logger.info("Invoking registerProject(HpcProjectDTO): " + 
-    			projectDTO);
+    			     projectRegistrationDTO);
     	
     	// Input validation.
-    	if(projectDTO == null) {
+    	if(projectRegistrationDTO == null) {
     	   throw new HpcException("Null HpcProjectDTO",
     			                  HpcErrorType.INVALID_REQUEST_INPUT);	
     	}
     	
     	// Validate the associated users with this project have valid NIH 
-    	// account registered with HPC. In addition, validate the registrator
+    	// account registered with HPC. In addition, validate the registrar
     	// has a valid data transfer account.
-    	validateAssociatedUsers(projectDTO);
-    	validateMetadata(projectDTO);
+    	validateAssociatedUsers(projectRegistrationDTO);
     	
-
-    	List<gov.nih.nci.hpc.dto.dataset.HpcDatasetRegistrationDTO> datasets = projectDTO.getHpcDatasetRegistrationDTO();
-    	List<String> datasetIds = new ArrayList<String>();
-    	if(datasets != null)
-    	{
-    		for(HpcDatasetRegistrationDTO dataset : datasets)
-    		{
-    			String datasetId = datasetService.registerDataset(dataset);
-    			datasetIds.add(datasetId);
-    		}
-    	}
-    	
+    	// Create and persist the project.
     	String projectId = 
-    		   projectService.add(
-    				   projectDTO.getMetadata(), 
-    				   datasetIds);
+    		   projectService.addProject(projectRegistrationDTO.getMetadata(), 
+    				                     true); 
     	logger.info("Registered projectId  = " + projectId);
+    	
+    	// Create and associate the datasets in this registration request.
+    	if(projectRegistrationDTO.getHpcDatasetRegistrationDTO() != null) {
+    	   for(HpcDatasetRegistrationDTO datasetRegistrationDTO : 
+    		   projectRegistrationDTO.getHpcDatasetRegistrationDTO()) {
+    		   associateProject(datasetRegistrationDTO.getUploadRequests(), 
+    				            projectId);
+    		   datasetBusService.registerDataset(datasetRegistrationDTO);
+    	   }
+    	}
     	
     	return projectId;
     }
@@ -162,13 +159,14 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
     //---------------------------------------------------------------------//  
 	
     /**
-     * Create a dataset DTO from a domain object.
+     * Create a project DTO from a domain object.
      * 
-     * @param dataset the domain object.
+     * @param project the domain object.
      *
      * @return The DTO.
      */
-    private HpcProjectDTO toDTO(HpcProject project) throws HpcException 
+    private HpcProjectDTO toDTO(HpcProject project) 
+    		                   throws HpcException 
     {
     	if(project == null) {
      	   return null;
@@ -178,27 +176,25 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
     	dto.setId(project.getId());
     	dto.setMetadata(project.getMetadata());
     	List<String> datasetIds = project.getDatasetIds();
-    	if(datasetIds != null && datasetIds.size() > 0)
-    	{
+    	if(datasetIds != null && datasetIds.size() > 0) {
     		List<HpcDatasetDTO> datasets = new ArrayList<HpcDatasetDTO>();
-    		for(String datasetId : datasetIds)
-    		{
-    			try
-    			{
-    				HpcDatasetDTO dataset = datasetService.getDataset(datasetId, true);
-    				if(dataset != null)
-    					datasets.add(dataset);
+    		for(String datasetId : datasetIds) {
+    			try {
+    				 HpcDatasetDTO dataset = 
+    						       datasetBusService.getDataset(datasetId, true);
+    				 if(dataset != null) {
+    				 	datasets.add(dataset);
+    				 }
     			}
-    			catch(HpcException e)
-    			{
-    				throw new HpcException("Invalid associated dataset Id: "+datasetId, e);
+    			catch(HpcException e) {
+    				  throw new HpcException("Invalid associated dataset Id: " + 
+    			                             datasetId, e);
     			}
     		}
-    		if(datasets != null && datasets.size() > 0)
-    		{
-    			HpcDatasetCollectionDTO collection = new HpcDatasetCollectionDTO();
-    			collection.getHpcDatasetDTO().addAll(datasets);
-    			dto.setHpcDatasetCollectionDTO(collection);
+    		if(datasets != null && datasets.size() > 0) {
+    		   HpcDatasetCollectionDTO collection = new HpcDatasetCollectionDTO();
+    		   collection.getHpcDatasetDTO().addAll(datasets);
+    		   dto.setHpcDatasetCollectionDTO(collection);
     		}
     	}
     	
@@ -206,13 +202,14 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
     }  
     
     /**
-     * Create a dataset collection DTO from a list of domain objects.
+     * Create a project collection DTO from a list of domain objects.
      * 
      * @param projects the domain object.
      *
      * @return The collection DTO.
      */
-    private HpcProjectCollectionDTO toCollectionDTO(List<HpcProject> projects) throws HpcException 
+    private HpcProjectCollectionDTO toCollectionDTO(List<HpcProject> projects) 
+    		                                       throws HpcException 
     {
     	if(projects == null || projects.size() == 0) {
     	   return null;
@@ -227,50 +224,7 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
  	    return projectCollectionDTO;
     }
     
-    private void validateMetadata(HpcProjectRegistrationDTO projectDTO)
-            throws HpcException
-    {
-    	if(projectDTO.getMetadata() == null)
-    	   	   throw new HpcException("Required metadata is missing for project",
-    	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-    	HpcProjectMetadata metadata = projectDTO.getMetadata();
-    	if(metadata.getName() == null)
- 	   	   throw new HpcException("Name value is missing for project",
- 	   			HpcErrorType.INVALID_REQUEST_INPUT);	
 
-    	if(metadata.getName() == null)
-  	   	   throw new HpcException("Name value is missing for project",
-  	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getPrimaryInvestigatorNihUserId() == null)
-  	   	   throw new HpcException("PI Id value is missing for project",
-  	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getRegistratorNihUserId() == null)
-  	   	   throw new HpcException("Registrator Id value is missing for project",
-  	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getLabBranch() == null)
-  	   	   throw new HpcException("Lab/Branch value is missing for project",
-  	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getDoc() == null)
-  	   	   throw new HpcException("DOCn value is missing for project",
-  	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getInternalProjectId() == null)
-   	   	   throw new HpcException("Internal Project Id value is missing for project",
-   	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getFundingOrganization() == null)
-   	   	   throw new HpcException("Funding Organization value is missing for project",
-   	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-
-    	if(metadata.getExperimentId() == null)
-   	   	   throw new HpcException("Experiment Id value is missing for project",
-   	   			HpcErrorType.INVALID_REQUEST_INPUT);	
-    	
-    }
     
     /**
      * Validate the users associated with the upload request are valid.
@@ -280,9 +234,8 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
      *
      * @throws HpcException if any validation error found.
      */
-    private void validateAssociatedUsers(
-    		HpcProjectRegistrationDTO projectDTO)
-    		             throws HpcException
+    private void validateAssociatedUsers(HpcProjectRegistrationDTO projectDTO)
+    		                            throws HpcException
     {
     	if(projectDTO == null) {
     	   return;
@@ -291,12 +244,12 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
 		// Verify PI, Creator and Registrator are registered with HPC.
 		validateUser(projectDTO.getMetadata().getPrimaryInvestigatorNihUserId(),
 				     HpcDatasetUserAssociation.PRIMARY_INVESTIGATOR);
-		validateUser(projectDTO.getMetadata().getRegistratorNihUserId(),
+		validateUser(projectDTO.getMetadata().getRegistrarNihUserId(),
 		             HpcDatasetUserAssociation.REGISTRAR);
     }
     
     /**
-     * Validate a user is registered with HPC
+     * Validate a user is registered with HPC.
      * 
      * @param nihUserId The NIH User ID.
      * @param userAssociation The user's association to the project.
@@ -318,6 +271,23 @@ public class HpcProjectBusServiceImpl implements HpcProjectBusService
     	
     	return user;
     }
+    
+    /**
+     * Associate a project with the files in a registration file upload requests.
+     * 
+     * @param uploadRequests The upload requests.
+     * @param projectId The project ID.
+     */
+    void associateProject(List<HpcFileUploadRequest> uploadRequests, 
+    		              String projectId)
+    {
+    	if(uploadRequests != null) {
+    	   for(HpcFileUploadRequest uploadRequest : uploadRequests) {
+    		   uploadRequest.getProjectIds().add(projectId);
+    	   }
+    	}
+    }
+    		              
 }
 
  
