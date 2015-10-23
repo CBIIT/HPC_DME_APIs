677b4799bd4ab0706e94f739263d04dff55e4e74
Mahi Narra <prasad.konka@nih.gov>
2015-10-23 15:54:54 +0000

    SVN-Revision: 563

---
+++ src/hpc/hpc-irods-client/src/main/java/gov/nih/nci/hpc/cli/domain/HPCDataObject.java
@@ -0,0 +1,52 @@
+package gov.nih.nci.hpc.cli.domain;
+
+public class HPCDataObject {
+	private String filename;
+	private String location;
+	private String metadataFile;
+	private String objectType;
+
+	public HPCDataObject(String filename, String location, String metadataFile, String objectType) {
+		this.filename = filename;
+		this.location = location;
+		this.metadataFile = metadataFile;
+		this.objectType = objectType;
+	}
+
+	public HPCDataObject(String filename, String metadataFile) {
+		this.filename = filename;
+		this.metadataFile = metadataFile;
+	}
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getMetadataFile() {
+		return metadataFile;
+	}
+
+	public void setMetadataFile(String metadataFile) {
+		this.metadataFile = metadataFile;
+	}
+
+	public String getObjectType() {
+		return objectType;
+	}
+
+	public void setObjectType(String objectType) {
+		this.objectType = objectType;
+	}
+
+}
