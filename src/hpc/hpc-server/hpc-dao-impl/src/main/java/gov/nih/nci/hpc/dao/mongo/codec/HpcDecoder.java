f21c061a7afac9706b70af5dc1aada582b1c9253
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-01-04 13:52:08 +0000

    SVN-Revision: 730

--- src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcDecoder.java
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcDecoder.java
@@ -10,17 +10,10 @@
 
 package gov.nih.nci.hpc.dao.mongo.codec;
 
-import gov.nih.nci.hpc.domain.dataset.HpcFile;
-import gov.nih.nci.hpc.domain.dataset.HpcFileSet;
 import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferLocations;
 import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferReport;
 import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferRequest;
 import gov.nih.nci.hpc.domain.datatransfer.HpcFileLocation;
-import gov.nih.nci.hpc.domain.metadata.HpcFileMetadata;
-import gov.nih.nci.hpc.domain.metadata.HpcFileMetadataVersion;
-import gov.nih.nci.hpc.domain.metadata.HpcFilePrimaryMetadata;
-import gov.nih.nci.hpc.domain.metadata.HpcMetadataItem;
-import gov.nih.nci.hpc.domain.metadata.HpcProjectMetadata;
 import gov.nih.nci.hpc.domain.user.HpcIntegratedSystemAccount;
 import gov.nih.nci.hpc.domain.user.HpcNciAccount;
 
@@ -90,27 +83,6 @@ class HpcDecoder
     //---------------------------------------------------------------------//
     // Dataset Domain Object Decoders
     //---------------------------------------------------------------------//
-    
-    /**
-     * Decode HpcFileSet.
-     *
-     * @param doc The HpcFileSet document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFileSet object.
-     */
-    public static HpcFileSet decodeFileSet(Document doc, 
-    		                               DecoderContext decoderContext, 
-    		                               CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-    	   return null;
-    	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcFileSet.class).decode(docReader, decoderContext);
-	}
     
     /**
      * Decode HpcDataTransferRequest.
@@ -197,137 +169,6 @@ class HpcDecoder
     		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
 		return registry.get(HpcDataTransferReport.class).decode(docReader, decoderContext);
 	}	
-    
-    /**
-     * Decode HpcFileMetadata.
-     *
-     * @param doc The HpcFileMetadata document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFileMetadata object.
-     */
-    public static HpcFileMetadata decodeFileMetadata(Document doc, 
-    		                                         DecoderContext decoderContext,
-    		                                         CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-        	   return null;
-        }
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcFileMetadata.class).decode(docReader, decoderContext);
-	}
-    
-    /**
-     * Decode HpcFileMetadataVersion.
-     *
-     * @param doc The HpcFileMetadataVersion document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFileMetadataVersion object.
-     */
-    public static HpcFileMetadataVersion 
-                  decodeFileMetadataVersion(Document doc, 
-    		                                DecoderContext decoderContext,
-    		                                CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-        	   return null;
-        }
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcFileMetadataVersion.class).decode(docReader, decoderContext);
-	}
-	
-    /**
-     * Decode HpcFilePrimaryMetadata.
-     *
-     * @param doc The HpcFilePrimaryMetadata document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFilePrimaryMetadata object.
-     */
-    public static HpcFilePrimaryMetadata 
-                  decodeFilePrimaryMetadata(Document doc, 
-    		                                DecoderContext decoderContext,
-    		                                CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-     	   return null;
-    	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class,  registry));
-		return registry.get(HpcFilePrimaryMetadata.class).decode(docReader, decoderContext);
-	}
-    
-    /**
-     * Decode HpcMetadataItem.
-     *
-     * @param doc The HpcMetadataItem document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcMetadataItem object.
-     */
-    public static HpcMetadataItem decodeMetadataItem(Document doc, 
-    		                                         DecoderContext decoderContext,
-    		                                         CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-     	   return null;
-    	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcMetadataItem.class).decode(docReader, decoderContext);
-	}
-    
-    /**
-     * Decode HpcFile.
-     *
-     * @param doc The HpcFile document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFile object.
-     */
-    public static HpcFile decodeFile(Document doc, DecoderContext decoderContext,
-    		                         CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-      	   return null;
-     	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcFile.class).decode(docReader, decoderContext);
-	}
-    
-    //---------------------------------------------------------------------//
-    // Project Domain Object Decoders
-    //---------------------------------------------------------------------//
-    
-	/**
-     * Decode HpcProjectMetadata.
-     *
-     * @param doc The HpcProjectMetadata document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcProjectMetadata object.
-     */
-    public static HpcProjectMetadata decodeProjectMetadata(Document doc, 
-    		                                               DecoderContext decoderContext,
-    		                                               CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-       	   return null;
-      	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcProjectMetadata.class).decode(docReader, decoderContext);
-	}
 }
 
  
