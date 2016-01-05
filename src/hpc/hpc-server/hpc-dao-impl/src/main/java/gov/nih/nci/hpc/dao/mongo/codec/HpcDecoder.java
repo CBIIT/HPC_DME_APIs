eb9edd1d7906aa0091d0e5ad5fb153a7d8b56884
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-01-05 01:22:14 +0000

    SVN-Revision: 733

--- src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcDecoder.java
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/mongo/codec/HpcDecoder.java
@@ -10,10 +10,6 @@
 
 package gov.nih.nci.hpc.dao.mongo.codec;
 
-import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferLocations;
-import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferReport;
-import gov.nih.nci.hpc.domain.datatransfer.HpcDataTransferRequest;
-import gov.nih.nci.hpc.domain.datatransfer.HpcFileLocation;
 import gov.nih.nci.hpc.domain.user.HpcIntegratedSystemAccount;
 import gov.nih.nci.hpc.domain.user.HpcNciAccount;
 
@@ -79,96 +75,6 @@ class HpcDecoder
     		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
 		return registry.get(HpcIntegratedSystemAccount.class).decode(docReader, decoderContext);
 	}
-	 
-    //---------------------------------------------------------------------//
-    // Dataset Domain Object Decoders
-    //---------------------------------------------------------------------//
-    
-    /**
-     * Decode HpcDataTransferRequest.
-     *
-     * @param doc The HpcDataTransferRequest document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcDataTransferRequest object.
-     */
-    public static HpcDataTransferRequest 
-           decodeDataTransferRequest(Document doc, DecoderContext decoderContext,
-        		                     CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-     	   return null;
-     	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcDataTransferRequest.class).decode(docReader, decoderContext);
-	}
-    
-    /**
-     * Decode HpcFileLocation.
-     *
-     * @param doc The HpcFileLocation document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcFileLocation object.
-     */
-    public static HpcFileLocation decodeFileLocation(Document doc, 
-    		                                         DecoderContext decoderContext,
-    		                                         CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-      	   return null;
-      	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcFileLocation.class).decode(docReader, decoderContext);
-	}	
-    
-    /**
-     * Decode HpcDataTransferLocations.
-     *
-     * @param doc The HpcDataTransferLocations document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcDataTransferLocations object.
-     */
-    public static HpcDataTransferLocations 
-                  decodeDataTransferLocations(Document doc, 
-    		                                  DecoderContext decoderContext,
-    		                                  CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-       	   return null;
-       	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcDataTransferLocations.class).decode(docReader, decoderContext);
-	}	
-	
-    /**
-     * Decode HpcDataTransferReport.
-     *
-     * @param doc The HpcDataTransferReport document.
-     * @param decoderContext The decoder context.
-     * @param registry Codec registry.
-     * @return Decoded HpcDataTransferReport object.
-     */
-    public static HpcDataTransferReport decodeDataTransferReport(
-    		                                  Document doc, 
-    		                                  DecoderContext decoderContext,
-    		                                  CodecRegistry registry)
-    {
-    	if(doc == null || registry == null) {
-       	   return null;
-       	}
-    	
-    	BsonDocumentReader docReader = 
-    		new BsonDocumentReader(doc.toBsonDocument(Document.class, registry));
-		return registry.get(HpcDataTransferReport.class).decode(docReader, decoderContext);
-	}	
 }
 
  
