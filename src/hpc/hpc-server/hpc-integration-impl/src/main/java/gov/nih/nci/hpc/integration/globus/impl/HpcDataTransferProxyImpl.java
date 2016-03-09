1b9917218d075ab361565fdd07b843c434d568cb
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-03-09 02:50:18 +0000

    SVN-Revision: 964

--- src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/globus/impl/HpcDataTransferProxyImpl.java
+++ src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/globus/impl/HpcDataTransferProxyImpl.java
@@ -26,8 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.sun.security.ntlm.Client;
-
 public class HpcDataTransferProxyImpl implements HpcDataTransferProxy 
 {
     //---------------------------------------------------------------------//
