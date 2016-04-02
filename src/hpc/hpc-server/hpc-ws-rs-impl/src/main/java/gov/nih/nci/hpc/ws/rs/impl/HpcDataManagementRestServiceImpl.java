1ab3a2dd4c1618033bd97277546e1bcb61d923dc
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-04-02 20:12:05 +0000

    SVN-Revision: 1033

--- src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
+++ src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
@@ -27,6 +27,7 @@ import gov.nih.nci.hpc.dto.metadata.HpcMetadataQueryParam;
 import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.ws.rs.HpcDataManagementRestService;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +36,18 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
 
 /**
  * <p>
@@ -292,6 +305,100 @@ public class HpcDataManagementRestServiceImpl extends HpcRestServiceImpl
 		 }
 		 
 		 return queries;
+    }
+    
+    
+    //---------------------------------------------------------------------//
+    // S3 POC
+    //---------------------------------------------------------------------//
+    
+    @Override
+    public Response s3Upload(String path)
+    {
+    	//s3SimpleUpload(path);
+    	s3MultipartUpload(path);
+    	return okResponse(null, false);
+    }
+    
+    private void s3SimpleUpload(String path)
+    {
+    	// Instantiate S3 Client and set the Cleversafe endpoint URL.
+    	BasicAWSCredentials cleversafeCredentials = new BasicAWSCredentials("vDZGQHw6OgBBpeI4D1CA", 
+    			                                                            "OVDNthOhfl5npqdSfAD8T9FsIcJlsCJsmuRdfanr");
+    	AmazonS3 s3client = new AmazonS3Client(cleversafeCredentials);
+    	s3client.setEndpoint("https://8.40.18.82");
+    	
+        try {
+        	 // Put an object.
+             System.out.println("Uploading a new object to S3 from a file\n");
+             File file = new File(path);
+             PutObjectResult result = s3client.putObject(new PutObjectRequest(
+              		                                    "CJ090115", "HPC-generated-key", file));
+            
+             System.out.println("Upload result md5: " + result.getContentMd5());
+            
+         } catch(AmazonServiceException ase) {
+                 System.out.println("Caught an AmazonServiceException, which " +
+            	                    "means your request made it " +
+                                    "to Amazon S3, but was rejected with an error response" +
+                                    " for some reason.");
+                 System.out.println("Error Message:    " + ase.getMessage());
+                 System.out.println("HTTP Status Code: " + ase.getStatusCode());
+                 System.out.println("AWS Error Code:   " + ase.getErrorCode());
+                 System.out.println("Error Type:       " + ase.getErrorType());
+                 System.out.println("Request ID:       " + ase.getRequestId());
+                 
+         } catch(AmazonClientException ace) {
+                 System.out.println("Caught an AmazonClientException, which " +
+            	 "means the client encountered " +
+                 "an internal error while trying to " +
+                 "communicate with S3, " +
+                 "such as not being able to access the network.");
+                 System.out.println("Error Message: " + ace.getMessage());
+        } 
+    }
+    
+    private void s3MultipartUpload(String path)
+    {
+    	// Instantiate a Transfer Manager using Cleversafe AWS credentials
+    	//BasicAWSCredentials cleversafeCredentials = new BasicAWSCredentials("vDZGQHw6OgBBpeI4D1CA", 
+    		//	                                                            "OVDNthOhfl5npqdSfAD8T9FsIcJlsCJsmuRdfanr");
+    	BasicAWSCredentials cleversafeCredentials = new BasicAWSCredentials("rQ5sO4vedFMpCJrbEBqA", 
+                                                                            "J7aNcIKXmJUDm5NUN70wVQq4zyv0WaMdykpBASEh");
+    	
+    	TransferManager tm = new TransferManager(cleversafeCredentials);
+    	tm.getAmazonS3Client().setEndpoint("https://8.40.18.82");
+    	
+    	// Create an upload request
+    	System.out.println("Multipart Uploading a new object to S3 from a file\n");
+        File file = new File(path);
+        PutObjectRequest request = new PutObjectRequest("CJ011916", "HPC-generated-key3", file);
+        
+        // Attach a progress listener.
+        request.setGeneralProgressListener(new ProgressListener() {
+			@Override
+			public void progressChanged(ProgressEvent progressEvent) {
+				System.out.println("Progress Event: " + progressEvent.getEventType() +
+				                   "    ***   Transferred bytes: " + 
+						           progressEvent.getBytesTransferred());
+			}
+		});
+        
+        // Invoke the asynchrnous upload.
+        Upload upload = tm.upload(request);
+
+        // For a demo we'll block and wait for the upload to complete.
+        try {
+        	 upload.waitForCompletion();
+        	 
+        	 System.out.println("Async upload completed");
+        	
+        } catch(AmazonClientException amazonClientException) {
+        	    System.out.println("Unable to upload file, upload was aborted.");
+        	    amazonClientException.printStackTrace();
+        } catch(Exception ioex) {
+        	    System.out.println("Interupted exception: " + ioex);
+        }
     }
 }
 
