dd054022e057902825141840be7a9e4d000e75ad
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-04-22 03:13:55 +0000

    SVN-Revision: 1104

--- src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
+++ src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
@@ -27,6 +27,7 @@ import gov.nih.nci.hpc.dto.metadata.HpcMetadataQueryParam;
 import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.ws.rs.HpcDataManagementRestService;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -308,6 +309,12 @@ public class HpcDataManagementRestServiceImpl extends HpcRestServiceImpl
 		 }
 		 
 		 return queries;
+    }
+    
+    public Response getFile()
+    {
+    	File file = new File("/Users/eran/hpc-client.p12");
+        return okResponse(file);
     }
 }
 
