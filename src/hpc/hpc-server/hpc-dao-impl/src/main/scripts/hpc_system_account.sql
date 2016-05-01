b6f00d904e984566fe4966245fb653488a8ee18a
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-05-01 14:25:43 +0000

    SVN-Revision: 1136

---
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/hpc_system_account.sql
@@ -0,0 +1,28 @@
+--
+-- hpc_system_account.sql
+--
+-- Copyright SVG, Inc.
+-- Copyright Leidos Biomedical Research, Inc
+-- 
+-- Distributed under the OSI-approved BSD 3-Clause License.
+-- See http://ncip.github.com/HPC/LICENSE.txt for details.
+--
+--
+-- @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+-- @version $Id$
+--
+
+DROP TABLE IF EXISTS public."HPC_SYSTEM_ACCOUNT";
+CREATE TABLE public."HPC_SYSTEM_ACCOUNT"
+(
+  "USERNAME" text NOT NULL,
+  "PASSWORD" bytea NOT NULL,
+  "INTEGRATED_SYSTEM" text NOT NULL,
+  "DATA_TRANSFER_TYPE" text,
+  CONSTRAINT "HPC_SYSTEM_ACCOUNT_pkey" PRIMARY KEY ("INTEGRATED_SYSTEM")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."HPC_SYSTEM_ACCOUNT"
+  OWNER TO postgres;
