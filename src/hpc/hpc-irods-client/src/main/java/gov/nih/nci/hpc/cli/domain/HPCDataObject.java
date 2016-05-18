2af010a547507a4c72ae1297c43f996f76248604
Prasad Konka <prasad.konka@nih.gov>
2016-05-18 13:24:43 +0000

    Multithreading support
    SVN-Revision: 1186

--- src/hpc/hpc-irods-client/src/main/java/gov/nih/nci/hpc/cli/domain/HPCDataObject.java
+++ src/hpc/hpc-irods-client/src/main/java/gov/nih/nci/hpc/cli/domain/HPCDataObject.java
@@ -1,36 +1,89 @@
 package gov.nih.nci.hpc.cli.domain;
 
-public class HPCDataObject {
-	private String source;
-	private String collection;
-	private String metadata;
+import java.util.Map;
 
-	public HPCDataObject(String source, String metadata) {
-		this.source = source;
-		this.metadata = metadata;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import gov.nih.nci.hpc.dto.datamanagement.HpcDataObjectRegistrationDTO;
+
+@XmlRootElement
+public class HPCDataObject{
+	private String objectPath;
+	private HpcDataObjectRegistrationDTO dto;
+	private String basePath;
+	private String hpcCertPath;
+	private String hpcCertPassword;
+	private String userId;
+	private String password;
+	private String authToken;
+	
+	public HPCDataObject() {
 	}
 
-	public String getSource() {
-		return source;
+	public String getObjectPath() {
+		return objectPath;
 	}
 
-	public void setSource(String source) {
-		this.source = source;
+	public void setObjectPath(String objectPath) {
+		this.objectPath = objectPath;
+	}
+	
+	public HpcDataObjectRegistrationDTO getDto()
+	{
+		return dto;
+	}
+	
+	public void setDto(HpcDataObjectRegistrationDTO dto)
+	{
+		this.dto = dto;
 	}
 
-	public String getCollection() {
-		return collection;
+	public String getBasePath() {
+		return basePath;
 	}
 
-	public void setCollection(String collection) {
-		this.collection = collection;
+	public void setBasePath(String basePath) {
+		this.basePath = basePath;
 	}
 
-	public String getMetadata() {
-		return metadata;
+	public String getHpcCertPath() {
+		return hpcCertPath;
 	}
 
-	public void setMetadata(String metadata) {
-		this.metadata = metadata;
+	public void setHpcCertPath(String hpcCertPath) {
+		this.hpcCertPath = hpcCertPath;
 	}
+
+	public String getHpcCertPassword() {
+		return hpcCertPassword;
+	}
+
+	public void setHpcCertPassword(String hpcCertPassword) {
+		this.hpcCertPassword = hpcCertPassword;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAuthToken() {
+		return authToken;
+	}
+
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+	
 }
