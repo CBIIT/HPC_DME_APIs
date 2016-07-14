85d3b0a677a6d6d9b3515afceb0ed312f8204b37
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-07-14 20:54:06 +0000

    SVN-Revision: 1322

--- src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcNotificationServiceImpl.java
+++ src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcNotificationServiceImpl.java
@@ -14,15 +14,25 @@ import static gov.nih.nci.hpc.service.impl.HpcDomainValidator.isValidNotificatio
 
 import java.util.List;
 
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import gov.nih.nci.hpc.dao.HpcNotificationDAO;
 import gov.nih.nci.hpc.domain.error.HpcErrorType;
+import gov.nih.nci.hpc.domain.notification.HpcNotificationDeliveryMethod;
 import gov.nih.nci.hpc.domain.notification.HpcNotificationEvent;
+import gov.nih.nci.hpc.domain.notification.HpcNotificationPayloadEntry;
 import gov.nih.nci.hpc.domain.notification.HpcNotificationSubscription;
 import gov.nih.nci.hpc.domain.notification.HpcNotificationType;
 import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.service.HpcNotificationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 /**
  * <p>
  * HPC Notification Application Service Implementation.
@@ -108,16 +118,78 @@ public class HpcNotificationServiceImpl implements HpcNotificationService
     }
     
     @Override
-    public void addDataTransferDownloadCompletedEvent(String userId) throws HpcException
+    public HpcNotificationSubscription getNotificationSubscription(String userId,
+                                                                   HpcNotificationType notificationType) 
+                                                                  throws HpcException
+    {
+    	// Input validation.
+    	if(userId == null || notificationType == null) {
+    	   throw new HpcException("Invalid user ID or notification type",
+    			                  HpcErrorType.INVALID_REQUEST_INPUT);
+    	}
+
+    	// Query the DB.
+    	return notificationDAO.getSubscription(userId, notificationType);
+    }
+    
+    @Override
+    public List<HpcNotificationEvent> getNotificationEvents() throws HpcException
+    {
+    	return notificationDAO.getEvents();
+    }
+    
+    @Override
+    public void deliverNotification(HpcNotificationEvent event, 
+                                    HpcNotificationDeliveryMethod deliveryMethod) 
+                                   throws HpcException
+    {
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+
+                mimeMessage.setRecipient(Message.RecipientType.TO,
+                        new InternetAddress(event.getUserId() + "@nih.gov"));
+                mimeMessage.setFrom(new InternetAddress("HPC_SERVER@nih.gov"));
+                mimeMessage.setText("Data Transfer Completed. Task Id:" +
+                                    event.getNotificationPayloadEntries().get(0));
+            }
+        };
+
+        try {
+        	JavaMailSender mailSender = new JavaMailSenderImpl();
+            mailSender.send(preparator);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    	
+    }
+    
+    @Override
+    public void createDeliveryReceipts(HpcNotificationEvent event) throws HpcException
+    {
+    	
+    }
+    
+    @Override
+    public void addDataTransferDownloadCompletedEvent(String userId,
+    		                                          String dataTransferRequestId) throws HpcException
     {
     	// Construct the event.
+    	HpcNotificationPayloadEntry payloadEntry = new HpcNotificationPayloadEntry();
+    	payloadEntry.setAttribute("DATA_TRANSFER_REQUEST_ID");
+    	payloadEntry.setValue(dataTransferRequestId);
+    	
     	HpcNotificationEvent event = new HpcNotificationEvent();
     	event.setUserId(userId);
     	event.setNotificationType(HpcNotificationType.DATA_TRANSFER_DOWNLOAD_COMPLETED);
+    	event.getNotificationPayloadEntries().add(payloadEntry);
     	
     	// Persist to DB.
     	addNotificationEvent(event);
     }
+    
     
     //---------------------------------------------------------------------//
     // Helper Methods
