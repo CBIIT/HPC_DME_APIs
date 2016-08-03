88ecd448f0b0117028884f8b48233aa1440dd0b6
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-08-03 00:59:01 +0000

    SVN-Revision: 1377

--- src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataManagementBusService.java
+++ src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataManagementBusService.java
@@ -104,11 +104,13 @@ public interface HpcDataManagementBusService
      * Get data objects by metadata query.
      *
      * @param metadataQueries The metadata queries.
+     * @param collectionMetadataQueries (Optional) The metadata queries for the parent collections(s).
      * @return A list of HpcDataObjectDTO.
      * 
      * @throws HpcException
      */
-    public HpcDataObjectListDTO getDataObjects(List<HpcMetadataQuery> metadataQueries) 
+    public HpcDataObjectListDTO getDataObjects(List<HpcMetadataQuery> metadataQueries,
+    		                                   List<HpcMetadataQuery> collectionMetadataQueries) 
     		                                  throws HpcException;
     
     /**
