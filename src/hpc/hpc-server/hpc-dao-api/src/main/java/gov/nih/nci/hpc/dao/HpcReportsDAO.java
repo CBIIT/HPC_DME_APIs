45c453c0888e9c1c70253c602b40f688a3973129
Prasad Konka <prasad.konka@nih.gov>
2016-08-05 13:29:39 +0000

    Reporting
    SVN-Revision: 1384

---
+++ src/hpc/hpc-server/hpc-dao-api/src/main/java/gov/nih/nci/hpc/dao/HpcReportsDAO.java
@@ -0,0 +1,42 @@
+/**
+ * HpcEventDAO.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.dao;
+
+import gov.nih.nci.hpc.domain.notification.HpcEvent;
+import gov.nih.nci.hpc.domain.report.HpcReport;
+import gov.nih.nci.hpc.domain.report.HpcReportCriteria;
+import gov.nih.nci.hpc.exception.HpcException;
+
+import java.util.List;
+
+/**
+ * <p>
+ * HPC Reports DAO Interface.
+ * </p>
+ *
+ * @author <a href="mailto:prasad.konka@nih.gov">Prasad Konka</a>
+ * @version $Id$
+ */
+
+public interface HpcReportsDAO 
+{    
+    /**
+     * Generate report based on given criteria
+     *
+     * @param criteria The report criteria
+     * 
+     * @throws HpcException
+     */
+    public HpcReport generatReport(HpcReportCriteria criteria) throws HpcException;
+    
+}
+
+ 
