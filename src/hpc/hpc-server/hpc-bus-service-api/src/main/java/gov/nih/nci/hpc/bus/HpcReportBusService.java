d4cc9337f39154a353d1f37aae6efb7c0eef991d
Prasad Konka <prasad.konka@nih.gov>
2016-08-05 13:29:02 +0000

    Reporting
    SVN-Revision: 1382

---
+++ src/hpc/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcReportBusService.java
@@ -0,0 +1,41 @@
+/**
+ * HpcReportBusService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.bus;
+
+import gov.nih.nci.hpc.dto.report.HpcReportDTO;
+import gov.nih.nci.hpc.dto.report.HpcReportRequestDTO;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * <p>
+ * HPC Report Business Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:prasad.konka@nih.gov">Prasad Konka</a>
+ * @version $Id$
+ */
+
+public interface HpcReportBusService 
+{         
+	/**
+	 * Generate HPC report
+	 *
+	 * @param criteria
+	 *            to generate the report
+	 * @return HpcReport
+	 * 
+	 * @throws HpcException
+	 */
+	public HpcReportDTO generateReport(HpcReportRequestDTO criteria) throws HpcException;
+    
+}
+
+ 
