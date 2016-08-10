2bffeb2342fb3298a02b51e0a4b3dc09c0870115
Prasad Konka <prasad.konka@nih.gov>
2016-08-10 20:38:20 +0000

    Reporting
    SVN-Revision: 1398

--- src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcReportsDAOImpl.java
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcReportsDAOImpl.java
@@ -14,6 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -55,31 +56,52 @@ public class HpcReportsDAOImpl implements HpcReportsDAO
 	private static final String SUM_OF_DATA_SQL = 
 			"SELECT sum(to_number(meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main where meta_attr_name = 'source_file_size'";
 
+	private static final String SUM_OF_DATA_BY_DATE_SQL = 
+			"SELECT sum(to_number(meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main where meta_attr_name = 'source_file_size' and to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ?";
+
 	private static final String LARGEST_FILE_SQL = 
 			"SELECT max(to_number(meta_attr_value, '9999999999999999999')) maxSize FROM public.r_meta_main where meta_attr_name = 'source_file_size'";
 	
+	private static final String LARGEST_FILE_BY_DATE_SQL = 
+			"SELECT max(to_number(meta_attr_value, '9999999999999999999')) maxSize FROM public.r_meta_main where meta_attr_name = 'source_file_size' and to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ?";
+
 	private static final String AVERAGE_FILE_SQL = 
 			"SELECT avg(to_number(meta_attr_value, '9999999999999999999')) maxSize FROM public.r_meta_main where meta_attr_name = 'source_file_size'";
 
+	private static final String AVERAGE_FILE_BY_DATE_SQL = 
+			"SELECT avg(to_number(meta_attr_value, '9999999999999999999')) maxSize FROM public.r_meta_main where meta_attr_name = 'source_file_size' and to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ?";
+
 	private static final String TOTAL_NUM_OF_USERS_SQL = 
 			"SELECT count(*) totalUsers FROM public.\"HPC_USER\"";
-
+	
+	private static final String TOTAL_NUM_OF_USERS_BY_DATE_SQL = 
+	"SELECT count(*) totalUsers FROM public.\"HPC_USER\" where \"CREATED\" BETWEEN ? and ?";
+	
 	private static final String TOTAL_NUM_OF_DATA_OBJECTS_SQL = 
 			"SELECT count(*) totalObjs FROM public.r_data_main";
 
-	private static final String TOTAL_NUM_OF_COLLECTIONS_SQL = 
-			"SELECT count(*) totalcolls FROM public.r_coll_main";
+	private static final String TOTAL_NUM_OF_DATA_OBJECTS_BY_DATE_SQL = 
+			"SELECT count(*) totalObjs FROM public.r_data_main  where to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ?";
 
 	private static final String TOTAL_NUM_OF_COLLECTIONS_BY_NAME_SQL = 
-			"select a.meta_attr_value, count(a.meta_attr_name) from r_meta_main a, r_coll_main b, r_objt_metamap c where b.coll_id=c.object_id and c.meta_id=a.meta_id and a.meta_attr_name='collection_type' group by a.meta_attr_value;";
+			"select a.meta_attr_value, count(a.meta_attr_name) from r_meta_main a, r_coll_main b, r_objt_metamap c where b.coll_id=c.object_id and c.meta_id=a.meta_id and a.meta_attr_name='collection_type' group by a.meta_attr_value";
+
+	private static final String TOTAL_NUM_OF_COLLECTIONS_BY_NAME_AND_DATE_SQL = 
+			"select a.meta_attr_value, count(a.meta_attr_name) from r_meta_main a, r_coll_main b, r_objt_metamap c where b.coll_id=c.object_id and c.meta_id=a.meta_id and a.meta_attr_name='collection_type' and to_timestamp(CAST(b.create_ts as double precision)) BETWEEN ? AND ? group by a.meta_attr_value";
 
 	private static final String TOTAL_NUM_OF_META_ATTRS_SQL = 
 			"SELECT count(*) totalAttrs FROM public.r_meta_main";
 	
+	private static final String TOTAL_NUM_OF_META_ATTRS_BY_DATE_SQL = 
+			"SELECT count(*) totalAttrs FROM public.r_meta_main where to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ?";
+
 	private static final String FILE_SIZE_RANGE_SQL = 
 			"SELECT count(*) FROM public.r_meta_main where meta_attr_name = 'source_file_size' and to_number(meta_attr_value, '9999999999999999999') BETWEEN ? AND ?";
 	
-    //---------------------------------------------------------------------//
+	private static final String FILE_SIZE_RANGE_BY_DATE_SQL = 
+			"SELECT count(*) FROM public.r_meta_main where meta_attr_name = 'source_file_size' and to_timestamp(CAST(create_ts as double precision)) BETWEEN ? AND ? and to_number(meta_attr_value, '9999999999999999999') BETWEEN ? AND ?";
+
+	//---------------------------------------------------------------------//
     // Instance members
     //---------------------------------------------------------------------//
 	
@@ -114,10 +136,9 @@ public class HpcReportsDAOImpl implements HpcReportsDAO
 	public HpcReport generatReport(HpcReportCriteria criteria) throws HpcException
     {
 		try {
-			if(criteria.getType().equals(HpcReportType.USAGE_SUMMARY))
+			if(criteria.getType().equals(HpcReportType.USAGE_SUMMARY) ||
+					criteria.getType().equals(HpcReportType.USAGE_SUMMARY_BY_DATE_RANGE))
 				return generateUsageSummaryReport(criteria);
-			else if(criteria.getType().equals(HpcReportType.USAGE_SUMMARY_BY_DATE_RANGE))
-				return generateUsageSummaryReportByDateRange(criteria);
 			else if(criteria.getType().equals(HpcReportType.USAGE_SUMMARY_BY_DOC))
 				return generateUsageSummaryByDocReport(criteria);
 			else if(criteria.getType().equals(HpcReportType.USAGE_SUMMARY_BY_DOC_BY_DATE_RANGE))
@@ -145,128 +166,250 @@ public class HpcReportsDAOImpl implements HpcReportsDAO
 	private HpcReport generateUsageSummaryReport(HpcReportCriteria criteria)
 	{
 		HpcReport report = new HpcReport();
-		
+
+		boolean isRangeType = false;
+		if(criteria.getFromDate() != null && criteria.getToDate() != null)
+			isRangeType = true;
 		//Total Users
-		Integer usersSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_USERS_SQL, Integer.class);
+		Integer usersSize = null;
+		Date[] dates = new Date[2];
+		if(isRangeType)
+		{
+			dates[0] = criteria.getFromDate().getTime();
+			dates[1] = criteria.getToDate().getTime();
+		}
+
+		if(isRangeType)
+			usersSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_USERS_BY_DATE_SQL, dates, Integer.class);
+		else
+			usersSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_USERS_SQL, Integer.class);
+			
 		HpcReportEntry userSizeEntry = new HpcReportEntry();
 		userSizeEntry.setAttribute(HpcReportEntryAttribute.TOTAL_NUM_OF_REGISTERED_USERS);
 		userSizeEntry.setValue(Integer.toString(usersSize == null ? 0 : usersSize));
 
 		//Total Size
-		Long totalSize = jdbcTemplate.queryForObject(SUM_OF_DATA_SQL, Long.class);
+		Long totalSize = null;
+		if(isRangeType)
+			totalSize = jdbcTemplate.queryForObject(SUM_OF_DATA_BY_DATE_SQL, dates, Long.class);
+		else
+			totalSize = jdbcTemplate.queryForObject(SUM_OF_DATA_SQL, Long.class);
+			
 		HpcReportEntry sizeEntry = new HpcReportEntry();
 		sizeEntry.setAttribute(HpcReportEntryAttribute.TOTAL_DATA_SIZE);
 		sizeEntry.setValue(Long.toString(totalSize == null ? 0 : totalSize));
 		
 		//Largest file
-		Long largestFileSize = jdbcTemplate.queryForObject(LARGEST_FILE_SQL, Long.class);
+		Long largestFileSize = null;
+		if(isRangeType)
+			largestFileSize = jdbcTemplate.queryForObject(LARGEST_FILE_BY_DATE_SQL, dates, Long.class);
+		else
+			largestFileSize = jdbcTemplate.queryForObject(LARGEST_FILE_SQL, Long.class);
+			
 		HpcReportEntry largestFileSizeEntry = new HpcReportEntry();
 		largestFileSizeEntry.setAttribute(HpcReportEntryAttribute.LARGEST_FILE_SIZE);
 		largestFileSizeEntry.setValue(Long.toString(largestFileSize == null ? 0 : largestFileSize));
 		
 		//Average file
-		Long averageFileSize = jdbcTemplate.queryForObject(AVERAGE_FILE_SQL, Long.class);
+		Long averageFileSize = null;
+		if(isRangeType)
+			averageFileSize = jdbcTemplate.queryForObject(AVERAGE_FILE_BY_DATE_SQL, dates, Long.class);
+		else
+			averageFileSize = jdbcTemplate.queryForObject(AVERAGE_FILE_SQL, Long.class);
 		HpcReportEntry averageFileSizeEntry = new HpcReportEntry();
 		averageFileSizeEntry.setAttribute(HpcReportEntryAttribute.AVERAGE_FILE_SIZE);
 		averageFileSizeEntry.setValue(Long.toString(averageFileSize == null ? 0 : averageFileSize));
 
 		//Total number of data objects
-		Long numOfDataObj = jdbcTemplate.queryForObject(TOTAL_NUM_OF_DATA_OBJECTS_SQL, Long.class);
+		Long numOfDataObj = null;
+		if(isRangeType)
+			numOfDataObj = jdbcTemplate.queryForObject(TOTAL_NUM_OF_DATA_OBJECTS_BY_DATE_SQL, dates, Long.class);
+		else
+			numOfDataObj = jdbcTemplate.queryForObject(TOTAL_NUM_OF_DATA_OBJECTS_SQL, Long.class);
 		HpcReportEntry numOfDataObjEntry = new HpcReportEntry();
 		numOfDataObjEntry.setAttribute(HpcReportEntryAttribute.TOTAL_NUM_OF_DATA_OBJECTS);
 		numOfDataObjEntry.setValue(Long.toString(numOfDataObj == null ? 0 : numOfDataObj));
 
 		//Total number of collections
-		List<Map<String, Object>> list = jdbcTemplate.queryForList(TOTAL_NUM_OF_COLLECTIONS_BY_NAME_SQL);
+		List<Map<String, Object>> list = null;
+		if(isRangeType)
+			list = jdbcTemplate.queryForList(TOTAL_NUM_OF_COLLECTIONS_BY_NAME_AND_DATE_SQL, dates);
+		else
+			list = jdbcTemplate.queryForList(TOTAL_NUM_OF_COLLECTIONS_BY_NAME_SQL);
 		List<String> entryList = new ArrayList<String>();
 		StringBuffer str = new StringBuffer();
-		if(list != null)
-		{
-			for(Map<String, Object> listEntry : list)
-			{
-				Iterator<String> iter = listEntry.keySet().iterator();
-				while(iter.hasNext())
-				{
-					String name = iter.next();
-					String value = (String)listEntry.get(name);
-					str.append(name + ": " + value +"\n");
-				}
-			}
-		}
+        str.append("\n");
+        if(list != null)
+        {
+                for(Map<String, Object> listEntry : list)
+                {
+                        String type = null;
+                        String count = null;
+                        Iterator<String> iter = listEntry.keySet().iterator();
+                        while(iter.hasNext())
+                        {
+                                String name = iter.next();
+                                if(name.equals("cnt"))
+                                {
+                                        Long value = (Long)listEntry.get(name);
+                                        count = value.toString();
+                                } else
+                                        type = (String) listEntry.get(name);
+                        }
+                        str.append(type + ": "+ count + "\n");
+                }
+        }
 		HpcReportEntry numOfCollEntry = new HpcReportEntry();
 		numOfCollEntry.setAttribute(HpcReportEntryAttribute.TOTAL_NUM_OF_COLLECTIONS);
 		numOfCollEntry.setValue(str.toString());
 
 		//Total Meta attributes Size
-		Long totalMetaSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_META_ATTRS_SQL, Long.class);
+		Long totalMetaSize = null;
+		if(isRangeType)
+			totalMetaSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_META_ATTRS_BY_DATE_SQL, dates, Long.class);
+		else
+			totalMetaSize = jdbcTemplate.queryForObject(TOTAL_NUM_OF_META_ATTRS_SQL, Long.class);
 		HpcReportEntry metasizeEntry = new HpcReportEntry();
 		metasizeEntry.setAttribute(HpcReportEntryAttribute.TOTAL_NUMBER_OF_META_ATTRS);
 		metasizeEntry.setValue(Long.toString(totalMetaSize == null ? 0 : totalMetaSize));
 		
 		//Distribution of files
-		Long[] onembargs = new Long[2];
-		onembargs[0] = Long.valueOf(0);
-		onembargs[1] = Long.valueOf(1000000);
-		Long oneMBCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, onembargs );
+		Object[] fileSizeArgs = null;
+		if (isRangeType)
+		{
+			fileSizeArgs = new Object[4];
+			fileSizeArgs[0] = criteria.getFromDate().getTime();
+			fileSizeArgs[1] = criteria.getToDate().getTime();
+			fileSizeArgs[2] = Long.valueOf(0);
+			fileSizeArgs[3] = Long.valueOf(1000000);
+		}
+		else
+		{
+			fileSizeArgs = new Object[2];
+			fileSizeArgs[0] = Long.valueOf(0);
+			fileSizeArgs[1] = Long.valueOf(1000000);
+		}
+		Long mbCount = null;
+		if(isRangeType)
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		else
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
 		HpcReportEntry oneMBEntry = new HpcReportEntry();
 		oneMBEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_BELOW_1_MB);
-		oneMBEntry.setValue(Long.toString(oneMBCount == null ? 0 : oneMBCount));
+		oneMBEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 		
-		Long[] tenmbargs = new Long[2];
-		tenmbargs[0] = Long.valueOf(1000000);
-		tenmbargs[1] = Long.valueOf(10000000);
-		Long tenMBCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, tenmbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(1000000);
+			fileSizeArgs[3] = Long.valueOf(10000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(1000000);
+			fileSizeArgs[1] = Long.valueOf(10000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
+		
 		HpcReportEntry tenMBEntry = new HpcReportEntry();
 		tenMBEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_1_MB_10_MB);
-		tenMBEntry.setValue(Long.toString(tenMBCount == null ? 0 : tenMBCount));
+		tenMBEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 		
-		Long[] fiftymbargs = new Long[2];
-		fiftymbargs[0] = Long.valueOf(10000000);
-		fiftymbargs[1] = Long.valueOf(50000000);
-		Long fiftyMBCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fiftymbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(10000000);
+			fileSizeArgs[3] = Long.valueOf(50000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(10000000);
+			fileSizeArgs[1] = Long.valueOf(50000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry fiftyMBEntry = new HpcReportEntry();
 		fiftyMBEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_10_MB_50_MB);
-		fiftyMBEntry.setValue(Long.toString(fiftyMBCount == null ? 0 : fiftyMBCount));
+		fiftyMBEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
-		Long[] hundredmbargs = new Long[2];
-		hundredmbargs[0] = Long.valueOf(50000000);
-		hundredmbargs[1] = Long.valueOf(100000000);
-		Long hundredMBCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, hundredmbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(50000000);
+			fileSizeArgs[3] = Long.valueOf(100000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(50000000);
+			fileSizeArgs[1] = Long.valueOf(100000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry hundredMBEntry = new HpcReportEntry();
 		hundredMBEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_50_MB_100_MB);
-		hundredMBEntry.setValue(Long.toString(hundredMBCount == null ? 0 : hundredMBCount));
+		hundredMBEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
-		Long[] fivehundredmbargs = new Long[2];
-		fivehundredmbargs[0] = Long.valueOf(100000000);
-		fivehundredmbargs[1] = Long.valueOf(500000000);
-		Long fivehundredMBCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fivehundredmbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(100000000);
+			fileSizeArgs[3] = Long.valueOf(500000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(100000000);
+			fileSizeArgs[1] = Long.valueOf(500000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry fivehundredMBEntry = new HpcReportEntry();
 		fivehundredMBEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_100_MB_500_MB);
-		fivehundredMBEntry.setValue(Long.toString(fivehundredMBCount == null ? 0 : fivehundredMBCount));
+		fivehundredMBEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
-		Long[] onegbargs = new Long[2];
-		onegbargs[0] = Long.valueOf(500000000);
-		onegbargs[1] = Long.valueOf(1000000000);
-		Long onegbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, onegbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(500000000);
+			fileSizeArgs[3] = Long.valueOf(1000000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(500000000);
+			fileSizeArgs[1] = Long.valueOf(1000000000);
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry onegbEntry = new HpcReportEntry();
 		onegbEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_500_MB_1_GB);
-		onegbEntry.setValue(Long.toString(onegbCount == null ? 0 : onegbCount));
+		onegbEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
-		Long[] tengbargs = new Long[2];
-		tengbargs[0] = Long.valueOf(1000000000);
-		tengbargs[1] = new Long("10000000000");
-		Long tengbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, tengbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = Long.valueOf(1000000000);
+			fileSizeArgs[3] = new Long("10000000000");
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = Long.valueOf(1000000000);
+			fileSizeArgs[1] = new Long("10000000000");
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry tengbEntry = new HpcReportEntry();
 		tengbEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_1_GB_10_GB);
-		tengbEntry.setValue(Long.toString(tengbCount == null ? 0 : tengbCount));
+		tengbEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
-		Long[] overtengbargs = new Long[2];
-		overtengbargs[1] = new Long("10000000000");
-		overtengbargs[1] = new Long("100000000000000");
-		Long overtengbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, overtengbargs);
+		if(isRangeType)
+		{
+			fileSizeArgs[2] = new Long("10000000000");
+			fileSizeArgs[3] = new Long("100000000000000");
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_BY_DATE_SQL, Long.class, fileSizeArgs );
+		}
+		else
+		{
+			fileSizeArgs[0] = new Long("10000000000");
+			fileSizeArgs[1] = new Long("100000000000000");
+			mbCount = jdbcTemplate.queryForObject(FILE_SIZE_RANGE_SQL, Long.class, fileSizeArgs );
+		}
 		HpcReportEntry overtengbEntry = new HpcReportEntry();
 		overtengbEntry.setAttribute(HpcReportEntryAttribute.FILE_SIZE_OVER_10_GB);
-		overtengbEntry.setValue(Long.toString(overtengbCount == null ? 0 : overtengbCount));
+		overtengbEntry.setValue(Long.toString(mbCount == null ? 0 : mbCount));
 
 		report.setGeneratedOn(Calendar.getInstance());
 		report.setType(criteria.getType());
