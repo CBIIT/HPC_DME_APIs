a5b5eef8e98bf4d416447bbd4842fbc24062963e
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-09-15 22:56:40 +0000

    SVN-Revision: 1488

--- src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/hpc_hierarchical_metadata.sql
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/hpc_hierarchical_metadata.sql
@@ -14,15 +14,17 @@
 
 
 -- Drop all views first.
-DROP VIEW r_coll_hierarchy_meta_main;
-DROP VIEW r_coll_hierarchy_meta_main_ovrd;
-DROP VIEW r_coll_hierarchy_metamap;
-DROP VIEW r_data_hierarchy_meta_main;
-DROP VIEW r_data_hierarchy_meta_main_ovrd;
-DROP VIEW r_data_hierarchy_metamap;
+DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_meta_main;
+DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_meta_main_ovrd;
+DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_metamap;
+DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_meta_main;
+DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_meta_main_ovrd;
+DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_metamap;
+
+-- Create all materialized views
 
 -- Hierarchy data metamap.
-CREATE VIEW r_data_hierarchy_metamap AS
+CREATE MATERIALIZED VIEW r_data_hierarchy_metamap AS
 WITH RECURSIVE r_data_hierarchy(parent_coll_name, coll_name, coll_id, object_id, object_path, level)
 AS (SELECT data_coll.coll_name, cast(null as varchar), cast(null as bigint), data_base.data_id, 
            data_coll.coll_name || '/' || data_base.data_name, 1 
@@ -36,6 +38,7 @@ SELECT data_hierarchy.object_id, data_hierarchy.object_path, metamap.meta_id, da
 FROM r_data_hierarchy data_hierarchy, r_objt_metamap metamap 
 WHERE ((data_hierarchy.object_id = metamap.object_id and data_hierarchy.coll_id is null) or 
        data_hierarchy.coll_id = metamap.object_id);
+CREATE UNIQUE INDEX r_data_hierarchy_metamap_unique ON r_data_hierarchy_metamap (object_id, meta_id, level);       
 COMMENT ON COLUMN r_data_hierarchy_metamap.object_id IS 
                   'Data object Hierarchy ID: r_data_main.data_id';
 COMMENT ON COLUMN r_data_hierarchy_metamap.object_path IS 
@@ -48,7 +51,7 @@ COMMENT ON COLUMN r_data_hierarchy_metamap.level IS
                   'The level of the metadata in the hierarchy, starting with 1 at the data-object level';
 
 -- Hierarchy data meta_main w/ override rules
-CREATE VIEW r_data_hierarchy_meta_main_ovrd AS
+CREATE MATERIALIZED VIEW r_data_hierarchy_meta_main_ovrd AS
 SELECT data_hierarchy_metamap.object_id, 
 	   (array_agg(data_hierarchy_metamap.object_path order by data_hierarchy_metamap.level))[1] AS object_path, 
        (array_agg(data_hierarchy_metamap.coll_id order by data_hierarchy_metamap.level))[1] AS coll_id, 
@@ -60,6 +63,8 @@ FROM r_meta_main meta_main, r_data_hierarchy_metamap data_hierarchy_metamap
 WHERE data_hierarchy_metamap.meta_id = meta_main.meta_id 
 GROUP BY data_hierarchy_metamap.object_id, meta_main.meta_attr_name 
 ORDER BY data_hierarchy_metamap.object_id;
+CREATE UNIQUE INDEX r_data_hierarchy_meta_main_ovrd_unique ON r_data_hierarchy_meta_main_ovrd (object_id, meta_id, level);
+CREATE INDEX r_data_hierarchy_meta_main_ovrd_query ON r_data_hierarchy_meta_main_ovrd (meta_attr_name, meta_attr_value);
 COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.object_id IS 
                   'Data object Hierarchy ID: r_data_main.data_id';
 COMMENT ON COLUMN r_data_hierarchy_metamap.object_path IS 
@@ -76,12 +81,13 @@ COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.meta_attr_value IS
                   'Metadata value: r_meta_main.meta_attr_value';
 
 -- Hierarchy data meta_main w/o override rules
-CREATE VIEW r_data_hierarchy_meta_main AS
+CREATE MATERIALIZED VIEW r_data_hierarchy_meta_main AS
 SELECT data_hierarchy_metamap.object_id, data_hierarchy_metamap.object_path, data_hierarchy_metamap.coll_id, 
        data_hierarchy_metamap.meta_id, data_hierarchy_metamap.level, meta_main.meta_attr_name, meta_main.meta_attr_value
 FROM r_meta_main meta_main, r_data_hierarchy_metamap data_hierarchy_metamap 
 WHERE data_hierarchy_metamap.meta_id = meta_main.meta_id 
 ORDER BY data_hierarchy_metamap.object_id;
+CREATE UNIQUE INDEX r_data_hierarchy_meta_main_unique ON r_data_hierarchy_meta_main (object_id, meta_id, level);
 COMMENT ON COLUMN r_data_hierarchy_meta_main.object_id IS 
                   'Data object Hierarchy ID: r_data_main.data_id';
 COMMENT ON COLUMN r_data_hierarchy_metamap.object_path IS 
@@ -98,7 +104,7 @@ COMMENT ON COLUMN r_data_hierarchy_meta_main.meta_attr_value IS
                   'Metadata value: r_meta_main.meta_attr_value';
 
 -- Hierarchy collection metamap.
-CREATE VIEW r_coll_hierarchy_metamap AS
+CREATE MATERIALIZED VIEW r_coll_hierarchy_metamap AS
 WITH RECURSIVE r_coll_hierarchy(parent_coll_name, coll_name, coll_id, object_id, object_path, level)
 AS (SELECT coll_base.parent_coll_name, coll_base.coll_name, coll_base.coll_id, coll_base.coll_id, coll_base.coll_name, 1 
     FROM r_coll_main coll_base
@@ -109,6 +115,7 @@ AS (SELECT coll_base.parent_coll_name, coll_base.coll_name, coll_base.coll_id, c
 SELECT coll_hierarchy.object_id, coll_hierarchy.object_path, metamap.meta_id, coll_hierarchy.coll_id, coll_hierarchy.level 
 FROM r_coll_hierarchy coll_hierarchy, r_objt_metamap metamap 
 WHERE coll_hierarchy.coll_id = metamap.object_id;
+CREATE UNIQUE INDEX r_coll_hierarchy_metamap_unique ON r_coll_hierarchy_metamap (object_id, meta_id, level);
 COMMENT ON COLUMN r_coll_hierarchy_metamap.object_id IS 
                   'Collection Hierarchy ID: r_coll_main.coll_id';
 COMMENT ON COLUMN r_coll_hierarchy_metamap.object_path IS 
@@ -121,7 +128,7 @@ COMMENT ON COLUMN r_coll_hierarchy_metamap.level IS
                   'The level of the metadata in the hierarchy, starting with 1 at the collection level';
 
 -- Hierarchy collection meta_main w/ override rules
-CREATE VIEW r_coll_hierarchy_meta_main_ovrd AS
+CREATE MATERIALIZED VIEW r_coll_hierarchy_meta_main_ovrd AS
 SELECT coll_hierarchy_metamap.object_id, 
        (array_agg(coll_hierarchy_metamap.object_path order by coll_hierarchy_metamap.level))[1] AS object_path, 
        (array_agg(coll_hierarchy_metamap.coll_id order by coll_hierarchy_metamap.level))[1] AS coll_id, 
@@ -133,6 +140,8 @@ FROM r_meta_main meta_main, r_coll_hierarchy_metamap coll_hierarchy_metamap
 WHERE coll_hierarchy_metamap.meta_id = meta_main.meta_id 
 GROUP BY coll_hierarchy_metamap.object_id, meta_main.meta_attr_name 
 ORDER BY coll_hierarchy_metamap.object_id;
+CREATE UNIQUE INDEX r_coll_hierarchy_meta_main_ovrd_unique ON r_coll_hierarchy_meta_main_ovrd (object_id, meta_id, level);
+CREATE INDEX r_coll_hierarchy_meta_main_ovrd_query ON r_coll_hierarchy_meta_main_ovrd (meta_attr_name, meta_attr_value);
 COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.object_id IS 
                   'Collection Hierarchy ID: r_coll_main.coll_id';
 COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.object_path IS 
@@ -149,12 +158,13 @@ COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.meta_attr_value IS
                   'Metadata value: r_meta_main.meta_attr_value';
 
 -- Hierarchy collection meta_main w/o override rules
-CREATE VIEW r_coll_hierarchy_meta_main AS
+CREATE MATERIALIZED VIEW r_coll_hierarchy_meta_main AS
 SELECT coll_hierarchy_metamap.object_id, coll_hierarchy_metamap.object_path, coll_hierarchy_metamap.coll_id, 
        coll_hierarchy_metamap.meta_id, coll_hierarchy_metamap.level, meta_main.meta_attr_name, meta_main.meta_attr_value
 FROM r_meta_main meta_main, r_coll_hierarchy_metamap coll_hierarchy_metamap 
 WHERE coll_hierarchy_metamap.meta_id = meta_main.meta_id 
 ORDER BY coll_hierarchy_metamap.object_id;
+CREATE UNIQUE INDEX r_coll_hierarchy_meta_main_unique ON r_coll_hierarchy_meta_main (object_id, meta_id, level);
 COMMENT ON COLUMN r_coll_hierarchy_meta_main.object_id IS 
                   'Collection Hierarchy ID: r_coll_main.coll_id';
 COMMENT ON COLUMN r_coll_hierarchy_meta_main.object_path IS 
@@ -169,6 +179,28 @@ COMMENT ON COLUMN r_coll_hierarchy_meta_main.meta_attr_name IS
                   'Metadata attribute: r_meta_main.meta_attr_name';
 COMMENT ON COLUMN r_coll_hierarchy_meta_main.meta_attr_value IS 
                   'Metadata value: r_meta_main.meta_attr_value';
+                  
+-- Define a function to check for numeric values.
+CREATE OR REPLACE FUNCTION less_than(text, text) RETURNS BOOLEAN AS $$
+DECLARE attribute NUMERIC;
+DECLARE value NUMERIC;
+BEGIN
+    attribute = $1::NUMERIC;
+    value = $2::NUMERIC;
+    RETURN attribute < value;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE;
 
 
+-- Refresh views
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_meta_main;
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_meta_main_ovrd;
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_metamap;
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_meta_main;
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_meta_main_ovrd;
+-- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_metamap;
 
