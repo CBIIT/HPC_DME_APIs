1eca35ab8e9cdfe1cfe12da04a91f8be597c684f
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-11-11 13:23:57 +0000

    SVN-Revision: 1603

--- src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/hpc_hierarchical_metadata.sql
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/hpc_hierarchical_metadata.sql
@@ -15,10 +15,8 @@
 
 -- Drop all views first.
 DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_meta_main;
-DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_meta_main_ovrd;
 DROP MATERIALIZED VIEW IF EXISTS r_coll_hierarchy_metamap;
 DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_meta_main;
-DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_meta_main_ovrd;
 DROP MATERIALIZED VIEW IF EXISTS r_data_hierarchy_metamap;
 
 -- Create all materialized views
@@ -50,37 +48,7 @@ COMMENT ON COLUMN r_data_hierarchy_metamap.coll_id IS
 COMMENT ON COLUMN r_data_hierarchy_metamap.level IS 
                   'The level of the metadata in the hierarchy, starting with 1 at the data-object level';
 
--- Hierarchy data meta_main w/ override rules
-CREATE MATERIALIZED VIEW r_data_hierarchy_meta_main_ovrd AS
-SELECT data_hierarchy_metamap.object_id, 
-	   (array_agg(data_hierarchy_metamap.object_path order by data_hierarchy_metamap.level))[1] AS object_path, 
-       (array_agg(data_hierarchy_metamap.coll_id order by data_hierarchy_metamap.level))[1] AS coll_id, 
-       (array_agg(data_hierarchy_metamap.meta_id order by data_hierarchy_metamap.level))[1] AS meta_id, 
-       min(data_hierarchy_metamap.level) AS level, 
-       meta_main.meta_attr_name,
-       (array_agg(meta_main.meta_attr_value order by data_hierarchy_metamap.level))[1] AS meta_attr_value
-FROM r_meta_main meta_main, r_data_hierarchy_metamap data_hierarchy_metamap 
-WHERE data_hierarchy_metamap.meta_id = meta_main.meta_id 
-GROUP BY data_hierarchy_metamap.object_id, meta_main.meta_attr_name 
-ORDER BY data_hierarchy_metamap.object_id;
-CREATE UNIQUE INDEX r_data_hierarchy_meta_main_ovrd_unique ON r_data_hierarchy_meta_main_ovrd (object_id, meta_id, level);
-CREATE INDEX r_data_hierarchy_meta_main_ovrd_metadata_query ON r_data_hierarchy_meta_main_ovrd (meta_attr_name, meta_attr_value);
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.object_id IS 
-                  'Data object Hierarchy ID: r_data_main.data_id';
-COMMENT ON COLUMN r_data_hierarchy_metamap.object_path IS 
-                  'Data object Hierarchy Path:  r_coll_main.coll_name / r_data_main.data_name';
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.meta_id IS 
-                  'Metadata ID: r_meta_main.meta_id';
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.coll_id IS 
-                  'Collection (in the hierarchy) ID: r_coll_main.coll_id. Null if the metadata is associated with the data object itself';
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.level IS 
-                  'The level of the metadata in the hierarchy, starting with 1 at the data-object level';
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.meta_attr_name IS 
-                  'Metadata attribute: r_meta_main.meta_attr_name. Note: w/ override rules, metadata name is unique within the hierarchy, associated with the metadata value of the lowest level';
-COMMENT ON COLUMN r_data_hierarchy_meta_main_ovrd.meta_attr_value IS 
-                  'Metadata value: r_meta_main.meta_attr_value';
-
--- Hierarchy data meta_main w/o override rules
+-- Hierarchy data meta_main
 CREATE MATERIALIZED VIEW r_data_hierarchy_meta_main AS
 SELECT data_hierarchy_metamap.object_id, data_hierarchy_metamap.object_path, data_hierarchy_metamap.coll_id, 
        data_hierarchy_metamap.meta_id, data_hierarchy_metamap.level, meta_main.meta_attr_name, meta_main.meta_attr_value
@@ -129,37 +97,7 @@ COMMENT ON COLUMN r_coll_hierarchy_metamap.coll_id IS
 COMMENT ON COLUMN r_coll_hierarchy_metamap.level IS 
                   'The level of the metadata in the hierarchy, starting with 1 at the collection level';
 
--- Hierarchy collection meta_main w/ override rules
-CREATE MATERIALIZED VIEW r_coll_hierarchy_meta_main_ovrd AS
-SELECT coll_hierarchy_metamap.object_id, 
-       (array_agg(coll_hierarchy_metamap.object_path order by coll_hierarchy_metamap.level))[1] AS object_path, 
-       (array_agg(coll_hierarchy_metamap.coll_id order by coll_hierarchy_metamap.level))[1] AS coll_id, 
-       (array_agg(coll_hierarchy_metamap.meta_id order by coll_hierarchy_metamap.level))[1] AS meta_id, 
-       min(coll_hierarchy_metamap.level) AS level, 
-       meta_main.meta_attr_name,
-       (array_agg(meta_main.meta_attr_value order by coll_hierarchy_metamap.level))[1] AS meta_attr_value
-FROM r_meta_main meta_main, r_coll_hierarchy_metamap coll_hierarchy_metamap 
-WHERE coll_hierarchy_metamap.meta_id = meta_main.meta_id 
-GROUP BY coll_hierarchy_metamap.object_id, meta_main.meta_attr_name 
-ORDER BY coll_hierarchy_metamap.object_id;
-CREATE UNIQUE INDEX r_coll_hierarchy_meta_main_ovrd_unique ON r_coll_hierarchy_meta_main_ovrd (object_id, meta_id, level);
-CREATE INDEX r_coll_hierarchy_meta_main_ovrd_metadata_query ON r_coll_hierarchy_meta_main_ovrd (meta_attr_name, meta_attr_value);
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.object_id IS 
-                  'Collection Hierarchy ID: r_coll_main.coll_id';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.object_path IS 
-                  'Collection Hierarchy Path: r_coll_main.coll_name';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.meta_id IS 
-                  'Metadata ID: r_meta_main.meta_id';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.coll_id IS 
-                  'Collection (in the hierarchy) ID: r_coll_main.coll_id. Same as object_id if the metadata is associated with the collection itself';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.level IS 
-                  'The level of the metadata in the hierarchy, starting with 1 at the collection level';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.meta_attr_name IS 
-                  'Metadata attribute: r_meta_main.meta_attr_name. Note: w/ override rules, metadata name is unique within the hierarchy, associated with the metadata value of the lowest level';
-COMMENT ON COLUMN r_coll_hierarchy_meta_main_ovrd.meta_attr_value IS 
-                  'Metadata value: r_meta_main.meta_attr_value';
-
--- Hierarchy collection meta_main w/o override rules
+-- Hierarchy collection meta_main
 CREATE MATERIALIZED VIEW r_coll_hierarchy_meta_main AS
 SELECT coll_hierarchy_metamap.object_id, coll_hierarchy_metamap.object_path, coll_hierarchy_metamap.coll_id, 
        coll_hierarchy_metamap.meta_id, coll_hierarchy_metamap.level, meta_main.meta_attr_name, meta_main.meta_attr_value
@@ -230,10 +168,8 @@ LANGUAGE plpgsql IMMUTABLE;
 
 -- Refresh views
 -- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_metamap;
--- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_meta_main_ovrd;
 -- REFRESH MATERIALIZED VIEW CONCURRENTLY r_coll_hierarchy_meta_main;
 -- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_metamap;
--- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_meta_main_ovrd;
 -- REFRESH MATERIALIZED VIEW CONCURRENTLY r_data_hierarchy_meta_main;
 
 
