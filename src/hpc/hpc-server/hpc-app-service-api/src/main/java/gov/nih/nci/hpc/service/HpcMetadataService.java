6db2a311f475fec9caebc4e68dd9c41bd7ca95a5
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-11-23 17:27:44 +0000

    SVN-Revision: 1677

---
+++ src/hpc/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcMetadataService.java
@@ -0,0 +1,106 @@
+/**
+ * HpcMetadataService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.service;
+
+import gov.nih.nci.hpc.domain.metadata.HpcMetadataEntry;
+import gov.nih.nci.hpc.domain.model.HpcSystemGeneratedMetadata;
+import gov.nih.nci.hpc.exception.HpcException;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * <p>
+ * HPC Metadata Application Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public interface HpcMetadataService 
+{  
+    /**
+     * Add metadata to a collection.
+     *
+     * @param path The collection path.
+     * @param metadataEntries The metadata entries to add.
+     * 
+     * @throws HpcException
+     */
+    public void addMetadataToCollection(String path, 
+    		                            List<HpcMetadataEntry> metadataEntries) 
+    		                           throws HpcException; 
+    
+    /**
+     * Update a collection's metadata.
+     *
+     * @param path The collection path.
+     * @param metadataEntries The metadata entries to update.
+     * 
+     * @throws HpcException
+     */
+    public void updateCollectionMetadata(String path, 
+    		                             List<HpcMetadataEntry> metadataEntries) 
+    		                            throws HpcException; 
+    
+    /**
+     * Generate system metadata and attach to a collection.
+     * System generated metadata is:
+     * 		1. UUID.
+     * 		2. Registrar user ID.
+     * 		3. Registrar name.
+     *      4. Registrar DOC.
+     *
+     * @param path The collection path.
+     * 
+     * @throws HpcException
+     */
+    public void addSystemGeneratedMetadataToCollection(String path) 
+    		                                          throws HpcException; 
+    
+    /**
+     * Get the system generated metadata of a collection.
+     *
+     * @param path The collection path.
+     * @return HpcSystemGeneratedMetadata The system generated metadata.
+     * 
+     * @throws HpcException
+     */
+    public HpcSystemGeneratedMetadata 
+              getCollectionSystemGeneratedMetadata(String path) throws HpcException; 
+    
+    /**
+     * Get the system generated metadata object from a list of entries.
+     *
+     * @param systemGeneratedMetadataEntries The system generated metadata entries.
+     * @return HpcSystemGeneratedMetadata The system generated metadata.
+     * 
+     * @throws HpcException
+     */
+    public HpcSystemGeneratedMetadata 
+              toSystemGeneratedMetadata(List<HpcMetadataEntry> systemGeneratedMetadataEntries) 
+            	                       throws HpcException; 
+    
+    /**
+     * convert a list of metadata entries to Map<attribute, value>
+     *
+     * @param metadataEntries The list of metadata entries
+     * @return Map<String, String>
+     * 
+     * @throws HpcException
+     */
+    public Map<String, String> toMap(List<HpcMetadataEntry> metadataEntries);
+
+}
+
+ 
---
+++ src/hpc/hpc-server/hpc-ws-rs-api/src/main/java/gov/nih/nci/hpc/ws/rs/HpcDataSearchRestService.java
@@ -0,0 +1,207 @@
+/**
+ * HpcDataSearchRestService.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ *
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.ws.rs;
+
+import gov.nih.nci.hpc.domain.metadata.HpcMetadataQuery;
+import gov.nih.nci.hpc.dto.datamanagement.HpcCompoundMetadataQueryDTO;
+import gov.nih.nci.hpc.dto.metadata.HpcMetadataQueryParam;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * <p>
+ * HPC Data Search REST Service Interface.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+@Path("/new")
+public interface HpcDataSearchRestService
+{
+//    /**
+//     * GET Collections by metadata query.
+//     *
+//     * @param metadataQueries A list of metadata entries to query for.
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@GET
+//	@Path("/collection")
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response getCollections(
+//			           @QueryParam("metadataQuery") List<HpcMetadataQueryParam> metadataQueries,
+//			           @QueryParam("detailedResponse") Boolean detailedResponse,
+//			           @QueryParam("page") Integer page);
+//	
+//    /**
+//     * POST Collections query.
+//     *
+//     * @param metadataQueries A list of metadata entries to query for. 
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@POST
+//	@Path("/collection/query/simple")
+//	@Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryCollections(List<HpcMetadataQuery> metadataQueries,
+//			                         @QueryParam("detailedResponse") Boolean detailedResponse,
+//			                         @QueryParam("page") Integer page);
+//	
+//    /**
+//     * POST Collections query.
+//     *
+//     * @param compoundMetadataQueryDTO A compund metadata query DTO.
+//     * @return Response The REST service response.
+//     */
+//	@POST
+//	@Path("/collection/query/compound")
+//	@Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryCollections(HpcCompoundMetadataQueryDTO compoundMetadataQueryDTO);
+//	
+//    /**
+//     * GET Collections by named query.
+//     *
+//     * @param name A named query.
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@GET
+//	@Path("/collection/query/compound/{queryName}")
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryCollections(@PathParam("queryName") String queryName,
+//			                         @QueryParam("detailedResponse") Boolean detailedResponse,
+//			                         @QueryParam("page") Integer page);
+//	
+//    /**
+//     * GET Data objects by metadata query.
+//     *
+//     * @param metadataQueries A list of metadata entries to query for.
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@GET
+//	@Path("/dataObject")
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response getDataObjects(
+//			           @QueryParam("metadataQuery") List<HpcMetadataQueryParam> metadataQueries,
+//			           @QueryParam("detailedResponse") Boolean detailedResponse,
+//			           @QueryParam("page") Integer page);
+//	
+//    /**
+//     * POST Data objects query.
+//     *
+//     * @param metadataQueries A list of metadata entries to query for.
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@POST
+//	@Path("/dataObject/query/simple")
+//	@Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryDataObjects(List<HpcMetadataQuery> metadataQueries,
+//			                         @QueryParam("detailedResponse") Boolean detailedResponse,
+//			                         @QueryParam("page") Integer page);
+//	
+//    /**
+//     * POST Data objects query.
+//     *
+//     * @param compoundMetadataQueryDTO A compund metadata query DTO.
+//     * @return Response The REST service response.
+//     */
+//	@POST
+//	@Path("/dataObject/query/compound")
+//	@Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryDataObjects(HpcCompoundMetadataQueryDTO compoundMetadataQueryDTO);
+//	
+//    /**
+//     * GET Data objects by named query.
+//     *
+//     * @param name A named query
+//     * @param detailedResponse If set to true, return entity details (attributes + metadata).
+//     * @param page The requested results page.
+//     * @return Response The REST service response.
+//     */
+//	@GET
+//	@Path("/dataObject/query/compound/{queryName}")
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response queryDataObjects(@PathParam("queryName") String queryName,
+//			                         @QueryParam("detailedResponse") Boolean detailedResponse,
+//			                         @QueryParam("page") Integer page);
+//	
+//    /**
+//     * GET A list of metadata attributes currently registered.
+//     *
+//     * @param level Filter the results by level.
+//     * @param levelOperatorStr The operator to use in the level filter.
+//     * @return Response The REST service response.
+//     */
+//	@GET
+//	@Path("/metadataAttributes")
+//	@Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//	public Response getMetadataAttributes(
+//			           @QueryParam("level") Integer level,
+//			           @QueryParam("levelOperator") String levelOperatorStr);
+//	
+//    /**
+//     * Save a query.
+//     *
+//     * @param queryName The query name.
+//     * @param updateUserRequestDTO The update request DTO.
+//     * @return Response The REST service response.
+//     */
+//    @POST
+//    @Path("/query/{queryName}")
+//    @Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//    public Response saveQuery(@PathParam("queryName") String queryName,
+//    		                  HpcCompoundMetadataQueryDTO compoundMetadataQueryDTO);
+//    
+//    /**
+//     * Delete a query.
+//     *
+//     * @param queryName The query name.
+//     * @return Response The REST service response.
+//     */
+//    @DELETE
+//    @Path("/query/{queryName}")
+//    public Response deleteQuery(@PathParam("queryName") String queryName);
+//
+//    /**
+//     * Get all saved queries for a user.
+//     *
+//     * @return Response The REST service response.
+//     */
+//    @GET
+//    @Path("/query")
+//    @Produces(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+//    public Response getQueries();
+}
+
