f8c1081ee394bd32b17f9d92034fbf395fc1eaff
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-11-25 21:16:19 +0000

    SVN-Revision: 1687

--- src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementNewRestServiceImpl.java
+++ src/hpc/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementNewRestServiceImpl.java
@@ -11,16 +11,24 @@
 package gov.nih.nci.hpc.ws.rs.impl;
 
 import gov.nih.nci.hpc.bus.HpcDataManagementNewBusService;
+import gov.nih.nci.hpc.domain.error.HpcErrorType;
 import gov.nih.nci.hpc.domain.metadata.HpcMetadataEntry;
 import gov.nih.nci.hpc.dto.datamanagement.HpcCollectionDTO;
 import gov.nih.nci.hpc.dto.datamanagement.HpcCollectionListDTO;
+import gov.nih.nci.hpc.dto.datamanagement.HpcDataObjectRegistrationDTO;
 import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.ws.rs.HpcDataManagementNewRestService;
+import gov.nih.nci.hpc.ws.rs.provider.HpcMultipartProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +57,10 @@ public class HpcDataManagementNewRestServiceImpl extends HpcRestServiceImpl
     // Instance members
     //---------------------------------------------------------------------//
 
+	// The multipart provider.
+	@Autowired
+	private HpcMultipartProvider multipartProvider = null;
+	
 	// The Logger instance.
 	private final Logger logger = 
 			             LoggerFactory.getLogger(this.getClass().getName());
@@ -123,10 +135,72 @@ public class HpcDataManagementNewRestServiceImpl extends HpcRestServiceImpl
 		return okResponse(!collections.getCollections().isEmpty() ? collections : null , true);
     }
     
+    @Override
+    public Response registerDataObject(String path, 
+    		                           HpcDataObjectRegistrationDTO dataObjectRegistration,
+    		                           InputStream dataObjectInputStream)
+    {	
+    	long start = System.currentTimeMillis();
+    	path = toAbsolutePath(path);
+		logger.info("Invoking RS: PUT /dataObject" + path);
+		
+		File dataObjectFile = null;
+		boolean created = true;
+
+		try {
+			 dataObjectFile = toFile(dataObjectInputStream);
+			 created = dataManagementBusService.registerDataObject(path, 
+					                                               dataObjectRegistration,
+					                                               dataObjectFile);
+			 
+		} catch(HpcException e) {
+			    logger.error("RS: PUT /dataObject" + path + " failed:", e);
+			    return errorResponse(e);
+			    
+		} finally {
+			       // Delete the temporary file (if provided).
+	    	       FileUtils.deleteQuietly(dataObjectFile);
+		}
+		
+		long stop = System.currentTimeMillis();
+		logger.info((stop-start) + " registerDataObject: Total time - " + path);
+		
+		if(created) {
+		   return createdResponse(null);
+		} else {
+				return okResponse(null, false);
+		}
+	}
+    
     //---------------------------------------------------------------------//
     // Helper Methods
     //---------------------------------------------------------------------//
     
+    /**
+     * Copy input stream to File and close the input stream.
+     * 
+     * @param dataObjectInputStream The input stream.
+     * @return File
+     * 
+     * @throws HpcException if copy of input stream failed.
+     */
+    private File toFile(InputStream dataObjectInputStream) throws HpcException
+    {
+    	if(dataObjectInputStream == null) {
+    	   return null;
+    	}
+
+    	File dataObjectFile = FileUtils.getFile(multipartProvider.getTempDirectory(), 
+    			                                UUID.randomUUID().toString());
+    	try {
+	         FileUtils.copyInputStreamToFile(dataObjectInputStream, dataObjectFile);
+	         
+    	} catch(IOException e) {
+    		    throw new HpcException("Failed to copy input stream", HpcErrorType.UNEXPECTED_ERROR, e);
+    	}
+    	
+    	return dataObjectFile;
+    }
 }
 
  
