e8bd3c7583729b3d3bc1e6e294453e8390ec219d
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-12-26 22:59:42 +0000

    SVN-Revision: 1889

---
+++ src/hpc/hpc-server/hpc-ws-rs-test/src/test/java/gov/nih/nci/hpc/ws/rs/test/HpcAuthenticationTestInterceptor.java
@@ -0,0 +1,91 @@
+/**
+ * HpcAuthentcationTestInterceptor.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.ws.rs.test;
+
+import gov.nih.nci.hpc.domain.model.HpcUser;
+import gov.nih.nci.hpc.domain.user.HpcIntegratedSystem;
+import gov.nih.nci.hpc.domain.user.HpcIntegratedSystemAccount;
+import gov.nih.nci.hpc.domain.user.HpcNciAccount;
+import gov.nih.nci.hpc.service.HpcSecurityService;
+
+import org.apache.cxf.interceptor.security.SecureAnnotationsInterceptor;
+import org.apache.cxf.interceptor.security.SimpleAuthorizingInterceptor;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <p>
+ * HPC Authentication Interceptor for JUnit Tests.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ * @version $Id$
+ */
+
+public class HpcAuthenticationTestInterceptor 
+             extends AbstractPhaseInterceptor<Message> 
+{
+    //---------------------------------------------------------------------//
+    // Instance members
+    //---------------------------------------------------------------------//
+
+    // The Security Business Service instance.
+	@Autowired
+    private HpcSecurityService securityService = null;
+	
+    //---------------------------------------------------------------------//
+    // Constructors
+    //---------------------------------------------------------------------//
+
+    /**
+     * Constructor for Spring Dependency Injection.
+     * 
+     */
+    private HpcAuthenticationTestInterceptor() 
+    {
+        super(Phase.RECEIVE);
+
+        // We need to authenticate first, and then authorize.
+        getBefore().add(SecureAnnotationsInterceptor.class.getName());
+        getBefore().add(SimpleAuthorizingInterceptor.class.getName());
+    }
+    
+    //---------------------------------------------------------------------//
+    // Methods
+    //---------------------------------------------------------------------//
+    
+    //---------------------------------------------------------------------//
+    // AbstractPhaseInterceptor<Message> Interface Implementation
+    //---------------------------------------------------------------------//  
+
+    @Override
+    public void handleMessage(Message message) 
+    {
+    	HpcNciAccount nciAccount = new HpcNciAccount();
+    	nciAccount.setDoc("UNIT_TEST");
+    	nciAccount.setFirstName("Unit");
+    	nciAccount.setLastName("Test");
+    	nciAccount.setUserId("unittest");
+    	
+    	HpcIntegratedSystemAccount dataManagementAccount = new HpcIntegratedSystemAccount();
+    	dataManagementAccount.setIntegratedSystem(HpcIntegratedSystem.IRODS);
+    	dataManagementAccount.setUsername("unittest");
+    	dataManagementAccount.setPassword("pwd");
+    	
+    	HpcUser user = new HpcUser();
+    	user.setNciAccount(nciAccount);
+    	user.setDataManagementAccount(dataManagementAccount);
+    	
+    	securityService.setRequestInvoker(user, true);
+    }
+} 
