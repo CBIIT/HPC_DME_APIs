154c43c5a472c85c748179c37b0d282d523a2a34
Eran Rosenberg <eran.rosenberg@nih.gov>
2016-12-26 22:59:42 +0000

    SVN-Revision: 1889

--- src/hpc/hpc-server/hpc-ws-rs-test/src/test/java/gov/nih/nci/hpc/ws/rs/test/HpcDataManagementRestServiceTest.java
+++ src/hpc/hpc-server/hpc-ws-rs-test/src/test/java/gov/nih/nci/hpc/ws/rs/test/HpcDataManagementRestServiceTest.java
@@ -10,11 +10,21 @@
 
 package gov.nih.nci.hpc.ws.rs.test;
 
+import gov.nih.nci.hpc.domain.datamanagement.HpcCollection;
+import gov.nih.nci.hpc.domain.datamanagement.HpcPathAttributes;
+import gov.nih.nci.hpc.domain.user.HpcIntegratedSystemAccount;
+import gov.nih.nci.hpc.exception.HpcException;
 import gov.nih.nci.hpc.ws.rs.HpcDataManagementRestService;
 
 import javax.ws.rs.core.Response;
 
 import org.junit.Test;
+
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.*;
+
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,9 +51,22 @@ public class HpcDataManagementRestServiceTest extends HpcRestServiceTest
     //---------------------------------------------------------------------//
     
     @Test
-    public void testRegistration() 
+    public void testRegistration() throws HpcException 
     {
     	logger.info("*** ERAN test 1 ***");
+    	
+    	HpcPathAttributes pathAttributes = new HpcPathAttributes();
+    	pathAttributes.setExists(true);
+    	pathAttributes.setIsAccessible(true);
+    	pathAttributes.setIsDirectory(true);
+    	pathAttributes.setIsFile(false);
+    	when(dataManagementProxyMock.getPathAttributes(anyObject(), anyString())).thenReturn(pathAttributes);
+    	
+    	HpcCollection collection = new HpcCollection();
+    	collection.setCollectionId(12345);
+    	collection.setAbsolutePath("/eran");
+    	when(dataManagementProxyMock.getCollection(anyObject(), anyString())).thenReturn(collection);
+    	
     	HpcDataManagementRestService dataManagementClient = getDataManagementClient();
     	Response response = dataManagementClient.getCollection("/eran");
     	logger.error("ERAN: " + response);
