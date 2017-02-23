0be96d5b17b09079387147f22198615b494010c0
Prasad Konka <prasad.konka@nih.gov>
2017-02-23 16:01:48 +0000

    SVN-Revision: 2087

---
+++ src/hpc/hpc-web/src/main/java/gov/nih/nci/hpc/web/model/HpcBrowserEntry.java
@@ -0,0 +1,47 @@
+package gov.nih.nci.hpc.web.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+public class HpcBrowserEntry {
+	@JsonView(Views.Public.class)
+	private String name;
+
+	@JsonView(Views.Public.class)
+	private String fullPath;
+
+	@JsonView(Views.Public.class)
+	private List<HpcBrowserEntry> children;
+	
+	@JsonView(Views.Public.class)
+	private boolean isFolder; 
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<HpcBrowserEntry> getChildren() {
+		if(children == null)
+			children = new ArrayList<HpcBrowserEntry>();
+		return children;
+	}
+	public void setChildren(List<HpcBrowserEntry> children) {
+		this.children = children;
+	}
+	public boolean isFolder() {
+		return isFolder;
+	}
+	public void setFolder(boolean isFolder) {
+		this.isFolder = isFolder;
+	}
+	public String getFullPath() {
+		return fullPath;
+	}
+	public void setFullPath(String fullPath) {
+		this.fullPath = fullPath;
+	}
+}
