dccbe339230c273873c873194a6f2843bad882a7
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-03-23 19:42:57 +0000

    SVN-Revision: 2199

--- src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
+++ src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
@@ -732,10 +732,14 @@ public class HpcDataManagementProxyImpl implements HpcDataManagementProxy
     		                     throws HpcException
     {
     	try {
-    		 String where = "lower(" + RodsGenQueryEnum.COL_USER_GROUP_NAME.getName() + ") " + 
+    		 /*String where = "lower(" + RodsGenQueryEnum.COL_USER_GROUP_NAME.getName() + ") " + 
     	                    "LIKE lower('" + groupSearchCriteria + "') and " +
     				        RodsGenQueryEnum.COL_USER_TYPE.getName() + " = '" + 
-    	                    UserTypeEnum.RODS_GROUP.getTextValue() + "'";
+    	                    UserTypeEnum.RODS_GROUP.getTextValue() + "'";*/
+    		String where = RodsGenQueryEnum.COL_USER_GROUP_NAME.getName() + " " + 
+                    "LIKE '" + groupSearchCriteria + "' and " +
+			        RodsGenQueryEnum.COL_USER_TYPE.getName() + " = '" + 
+                    UserTypeEnum.RODS_GROUP.getTextValue() + "'";
     		 logger.error("ERAN: WHERE: " + where);
     		 return toHpcGroupNames(irodsConnection.getUserGroupAO(authenticatedToken).findWhere(where));
     		 
