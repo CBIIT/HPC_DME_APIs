d21ccdf4b3b0ea62a0bc691dd66a1a78fec447b6
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-03-25 15:13:16 +0000

    SVN-Revision: 2209

--- src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
+++ src/hpc/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
@@ -673,7 +673,7 @@ public class HpcDataManagementProxyImpl implements HpcDataManagementProxy
 	                        RodsGenQueryEnum.COL_USER_TYPE.getName() + " = '" + 
                             UserTypeEnum.RODS_GROUP.getTextValue() + "'";
 			 logger.error("ERAN: WHERE: " + where);
-			 return irodsConnection.getUserGroupAO(authenticatedToken).findByName(groupName) != null;
+			 return irodsConnection.getUserGroupAO(authenticatedToken).findWhere(where) != null;
 
 		} catch(Exception e) {
                 throw new HpcException("Failed to get a group: " + e.getMessage(),
