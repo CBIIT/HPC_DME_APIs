3dd0aa4c35b8454ea615c3a3b5e85b98f65070c7
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-03-25 13:46:24 +0000

    Updated the tests to reflect the change in the API
    SVN-Revision: 2204

--- src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataManagementServiceImpl.java
+++ src/hpc/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataManagementServiceImpl.java
@@ -236,7 +236,7 @@ public class HpcDataManagementServiceImpl implements HpcDataManagementService
                                        throws HpcException
     {
     	// Validate the permission request - ensure the subject is NOT a system account.
-    	validatePermissionRequest(subjectPermission);
+    	//validatePermissionRequest(subjectPermission);
     	
         dataManagementProxy.setCollectionPermission(dataManagementAuthenticator.getAuthenticatedToken(), 
     		    		                            path, subjectPermission);
@@ -253,7 +253,7 @@ public class HpcDataManagementServiceImpl implements HpcDataManagementService
                                        throws HpcException
     {
     	// Validate the permission request - ensure the subject is NOT a system account.
-    	validatePermissionRequest(subjectPermission);
+    	//validatePermissionRequest(subjectPermission);
     	
     	dataManagementProxy.setDataObjectPermission(dataManagementAuthenticator.getAuthenticatedToken(), 
                                                     path, subjectPermission);
