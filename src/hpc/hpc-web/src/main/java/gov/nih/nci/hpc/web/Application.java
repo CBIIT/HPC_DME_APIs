aedb17febcdb35bcc3a7d815e22715923ec4f3e1
Prasad Konka <prasad.konka@nih.gov>
2017-04-05 18:03:55 +0000

    SVN-Revision: 2255

--- src/hpc/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
+++ src/hpc/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
@@ -2,12 +2,21 @@ package gov.nih.nci.hpc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
   public static void main(String[] args) {
-    ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application.class, args);
   }
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(applicationClass);
+  }
+
+  private static Class<Application> applicationClass = Application.class;  
 }
