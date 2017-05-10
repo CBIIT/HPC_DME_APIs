2ae61bd5fdaffdf5a8a438c21c976448bf3d3497
George Zaki <george.zaki@nih.gov>
2017-05-10 14:39:27 +0000

    Updated permission information
    SVN-Revision: 2366

--- src/hpc/hpc-server/hpc-ws-rs-api/test/sampledata/register-projects-and-dataobjects.sh
+++ src/hpc/hpc-server/hpc-ws-rs-api/test/sampledata/register-projects-and-dataobjects.sh
@@ -25,11 +25,40 @@ function register_verify {
 
 
 }
+
+function permission_verify {
+
+    RESPONSE_HEADER="permission-response-header.tmp"
+    RESPONSE_MSG="permission-response-message.json.tmp"
+
+
+    if [ -e  "$RESPONSE_HEADER" ]
+    then
+        rm $RESPONSE_HEADER
+    fi
+
+    if [ -e  "$RESPONSE_MSG" ]
+    then
+        rm $RESPONSE_MSG
+    fi
+
+    update_single_permission $1 $2 $3 
+    #verify_registration $RESPONSE_HEADER
+
+    if [[ $(cat $RESPONSE_MSG ) == *"true"* ]]; then
+        echo "Successfully gave $2 $3 permission on $1"
+    else  
+        echo "ERROR in giving $2 $3 permission on $1"
+        exit
+    fi
+}
+
+
 source $HPC_DM_TEST/utils/functions
 
 
 #Give dice_user_sys_admin OWN permission on the root folder
-#update_single_permission $BASE_FOLDER dice_user_sys_admin OWN
+update_single_permission collection/$(get_basefolder) dice_user_sys_admin OWN
 
 
 BASE_FOLDER=$HPC_DM_TEST/sampledata/
@@ -37,12 +66,20 @@ BASE_FOLDER=$HPC_DM_TEST/sampledata/
 #Register all the collections
 
 register_verify $BASE_FOLDER/project1_metadata.json "$(get_basefolder)/dice_project1" collection
+permission_verify collection/$(get_basefolder)/dice_project1  dice_user_group_admin WRITE 
+permission_verify collection/$(get_basefolder)/dice_project1  dice_user NONE
 
 register_verify $BASE_FOLDER/project2_metadata.json "$(get_basefolder)/dice_project2" collection
+permission_verify collection/$(get_basefolder)/dice_project2  dice_user_group_admin WRITE 
+permission_verify collection/$(get_basefolder)/dice_project2  dice_user WRITE 
 
 register_verify $BASE_FOLDER/project3_metadata.json "$(get_basefolder)/dice_project3" collection
+permission_verify collection/$(get_basefolder)/dice_project3  dice_user_group_admin WRITE 
+permission_verify collection/$(get_basefolder)/dice_project3  dice_user NONE
 
 register_verify $BASE_FOLDER/project4_metadata.json "$(get_basefolder)/dice_project4" collection
+update_single_permission collection/$(get_basefolder)/dice_project4  dice_user_group_admin WRITE 
+update_single_permission collection/$(get_basefolder)/dice_project4  dice_user NONE
 
 register_verify $BASE_FOLDER/project1_sub1_metadata.json "$(get_basefolder)/dice_project1/sub1" collection
 
@@ -54,20 +91,20 @@ OBJECT_FILE="dummy-file.txt"
 echo "test-data" > $OBJECT_FILE
 
 
-register_verify $BASE_FOLDER/project1_datafile1_metadata.json "$(get_basefolder)/dice_project1/dice_object_1" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project1_datafile1_metadata.json "$(get_basefolder)/dice_project1/dice_object_1" dataObject $OBJECT_FILE 
 
-register_verify $BASE_FOLDER/project2_datafile2_metadata.json "$(get_basefolder)/dice_project2/dice_object_2" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project2_datafile2_metadata.json "$(get_basefolder)/dice_project2/dice_object_2" dataObject $OBJECT_FILE 
 
-register_verify $BASE_FOLDER/project1_sub1_object1_metadata.json "$(get_basefolder)/dice_project1/sub1/object1" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project1_sub1_object1_metadata.json "$(get_basefolder)/dice_project1/sub1/object1" dataObject $OBJECT_FILE
 
-register_verify $BASE_FOLDER/project1_sub1_object2_metadata.json "$(get_basefolder)/dice_project1/sub1/object2" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project1_sub1_object2_metadata.json "$(get_basefolder)/dice_project1/sub1/object2" dataObject $OBJECT_FILE
 
-register_verify $BASE_FOLDER/project2_sub2_object1_metadata.json "$(get_basefolder)/dice_project2/sub2/object1" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project2_sub2_object1_metadata.json "$(get_basefolder)/dice_project2/sub2/object1" dataObject $OBJECT_FILE 
 
-register_verify $BASE_FOLDER/project2_sub2_object2_metadata.json "$(get_basefolder)/dice_project2/sub2/object2" dataObject dummy-file.txt
+register_verify $BASE_FOLDER/project2_sub2_object2_metadata.json "$(get_basefolder)/dice_project2/sub2/object2" dataObject $OBJECT_FILE 
 
 #Add more than 100 objects in project 4 to test the pagination
-for i in `seq 3 101`
+for i in `seq  3 101`
 do 
     register_verify $BASE_FOLDER/project4_datafile.json "$(get_basefolder)/dice_project4/dice_object_$i" dataObject dummy-file.txt
 done
