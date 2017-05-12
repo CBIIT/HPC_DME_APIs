2e83d385f107ad47603dc469d43c177da6c2ec4f
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-05-12 20:47:38 +0000

    SVN-Revision: 2382

--- src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/restore/hpc_doc_configuration_dev.sql
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/scripts/restore/hpc_doc_configuration_dev.sql
@@ -12,10 +12,57 @@
 -- @version $Id$
 --
 
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('NOHIERARCHY', '/NO_HIERARCHY');
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('FNLCR', '/FNL_SF_Archive');
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('CCBR', '/CCBR_SF_Archive');
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('DUMMY', '/TEST_Archive');
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('CCR-LEEMAX', '/CCR_LEEMAX_Archive');
-INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('HiTIF', '/HiTIF_Archive');
+INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('NOHIERARCHY', '/NO_HIERARCHY', NULL);
+
+INSERT INTO public."HPC_DOC_CONFIGURATION" 
+VALUES('FNLCR', '/FNL_SF_Archive',
+       '{
+      	 "collectionType": "PI_Lab",
+      	 "isDataObjectContainer": false,
+      	 "subCollections": [
+        	{
+          	 "collectionType": "Project",
+          	 "isDataObjectContainer": false,
+          	 "subCollections": [
+            	{
+              	 "collectionType": "Flowcell",
+              	 "isDataObjectContainer": false,
+              	 "subCollections": [
+                	{
+                     "collectionType": "Sample",
+                     "isDataObjectContainer": true
+                	}
+              	 ]
+               }
+          	 ]
+        	}
+      	 ]
+    	}');
+
+INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('CCBR', '/CCBR_SF_Archive', NULL);
+
+INSERT INTO public."HPC_DOC_CONFIGURATION" 
+VALUES('DUMMY', '/TEST_Archive',
+       '{
+      	 "collectionType": "Project",
+      	 "isDataObjectContainer": true,
+      	 "subCollections": [
+        	{
+          	 "collectionType": "Dataset",
+          	 "isDataObjectContainer": true
+        	},
+        	{
+          	 "collectionType": "Run",
+          	 "isDataObjectContainer": false
+        	},
+        	{
+          	 "collectionType": "Folder",
+          	 "isDataObjectContainer": true
+        	}
+      	 ]
+        }');
+
+INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('CCR-LEEMAX', '/CCR_LEEMAX_Archive', NULL);
+
+INSERT INTO public."HPC_DOC_CONFIGURATION" VALUES('HiTIF', '/HiTIF_Archive', NULL);
 
