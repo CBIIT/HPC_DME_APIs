23219609e82549a0dd5af8ce4ddb120d3060f8c8
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-05-14 17:38:51 +0000

    SVN-Revision: 2387

--- src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcDocConfigurationDAOImpl.java
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcDocConfigurationDAOImpl.java
@@ -13,11 +13,13 @@ package gov.nih.nci.hpc.dao.postgresql.impl;
 import gov.nih.nci.hpc.dao.HpcDocConfigurationDAO;
 import gov.nih.nci.hpc.domain.datamanagement.HpcDataHierarchy;
 import gov.nih.nci.hpc.domain.error.HpcErrorType;
+import gov.nih.nci.hpc.domain.metadata.HpcMetadataValidationRule;
 import gov.nih.nci.hpc.domain.model.HpcDocConfiguration;
 import gov.nih.nci.hpc.exception.HpcException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -110,6 +112,10 @@ public class HpcDocConfigurationDAOImpl implements HpcDocConfigurationDAO
 			docConfiguration.setBasePath(rs.getString("BASE_PATH"));
 			try {
 			     docConfiguration.setDataHierarchy(getDataHierarchyFromJSONStr(rs.getString("DATA_HIERARCHY")));
+			     docConfiguration.getCollectionMetadataValidationRules().addAll(
+			        getMetadataValidationRulesFromJSONStr(rs.getString("COLLECTION_METADATA_VALIDATION_RULES")));
+			     docConfiguration.getDataObjectMetadataValidationRules().addAll(
+					        getMetadataValidationRulesFromJSONStr(rs.getString("DATA_OBJECT_METADATA_VALIDATION_RULES")));
 			     
 			} catch(HpcException e) {
 				    throw new SQLException(e);
@@ -177,7 +183,7 @@ public class HpcDocConfigurationDAOImpl implements HpcDocConfigurationDAO
 		if(!jsonDataHierarchy.containsKey("collectionType") ||
 		   !jsonDataHierarchy.containsKey("isDataObjectContainer")) {
  	       throw new HpcException("Invalid Data Hierarchy Definition: " + jsonDataHierarchy,
- 	                               HpcErrorType.SPRING_CONFIGURATION_ERROR);
+ 	                               HpcErrorType.DATABASE_ERROR);
 		}
 		
 		dataHierarchy.setCollectionType((String)jsonDataHierarchy.get("collectionType"));
@@ -193,6 +199,102 @@ public class HpcDocConfigurationDAOImpl implements HpcDocConfigurationDAO
   	    }
   	    
   	    return dataHierarchy;
+    }
+    
+    /**
+     * Instantiate metadata validation rules from string.
+     * 
+     * @param metadataValidationRulesJSONStr The metadata validation rules JSON String.
+     * @return a list of metadata validation rules.
+     * @throws HpcException on service failure.
+     */
+	private List<HpcMetadataValidationRule> 
+	        getMetadataValidationRulesFromJSONStr(String metadataValidationRulesJSONStr) 
+			                                     throws HpcException
+    {
+		if(metadataValidationRulesJSONStr == null) {
+		   return new ArrayList<HpcMetadataValidationRule>();
+		}
+		
+		try {
+	         JSONArray jsonMetadataValidationRules = 
+	        		   (JSONArray) ((JSONObject) new JSONParser().parse(metadataValidationRulesJSONStr)).
+	        		                                              get("metadataValidationRules");
+	         if(jsonMetadataValidationRules == null) {
+	       	    throw new HpcException("Empty validation rules", HpcErrorType.DATABASE_ERROR); 
+	         }
+	         
+	         return metadataValidationRulesFromJSON(jsonMetadataValidationRules);
+	         
+		} catch(Exception e) {
+		    throw new HpcException("Failed to parse metadata validation rules JSON: " + metadataValidationRulesJSONStr,
+                                   HpcErrorType.DATABASE_ERROR, e);
+		}
+    }   
+	
+    /**
+     * Instantiate list metadata validation rules from JSON.
+     *
+     * @param jsonMetadataValidationRules The validation rules JSON array. 
+     * @return A collection of metadata validation rules.
+     * @throws HpcException If failed to parse the JSON.
+     */
+    @SuppressWarnings("unchecked")
+	private List<HpcMetadataValidationRule> metadataValidationRulesFromJSON(JSONArray jsonMetadataValidationRules) 
+    		                                                               throws HpcException
+    {
+    	List<HpcMetadataValidationRule> validationRules = new ArrayList<>();
+    	
+    	// Iterate through the rules and map to POJO.
+    	Iterator<JSONObject> rulesIterator = jsonMetadataValidationRules.iterator();
+    	while(rulesIterator.hasNext()) {
+    		  JSONObject jsonMetadataValidationRule = rulesIterator.next();
+    		
+	    	  if(!jsonMetadataValidationRule.containsKey("attribute") ||
+	    		 !jsonMetadataValidationRule.containsKey("mandatory") ||
+	    		 !jsonMetadataValidationRule.containsKey("ruleEnabled") ||
+	    		 !jsonMetadataValidationRule.containsKey("docs")) {
+	    		 throw new HpcException("Invalid rule JSON object: " + jsonMetadataValidationRule,
+	    		                        HpcErrorType.SPRING_CONFIGURATION_ERROR);	
+	    	  }
+	    			
+	    	  // JSON -> POJO.
+	    	  HpcMetadataValidationRule metadataValidationRule = new HpcMetadataValidationRule();
+	    	  metadataValidationRule.setAttribute((String) jsonMetadataValidationRule.get("attribute"));
+	    	  metadataValidationRule.setMandatory((Boolean) jsonMetadataValidationRule.get("mandatory"));
+	    	  metadataValidationRule.setRuleEnabled((Boolean) jsonMetadataValidationRule.get("ruleEnabled"));
+	    	  metadataValidationRule.setDefaultValue((String) jsonMetadataValidationRule.get("defaultValue"));
+	    	  metadataValidationRule.setDefaultUnit((String) jsonMetadataValidationRule.get("defaultUnit"));
+	    	  
+	    	  JSONArray jsonDOC = (JSONArray) jsonMetadataValidationRule.get("docs");
+	    	  if(jsonDOC != null) {
+		    	 Iterator<String> docIterator = jsonDOC.iterator();
+		    	 while(docIterator.hasNext()) {
+		    	   	   metadataValidationRule.getDocs().add(docIterator.next());
+		    	 }
+	    	  }
+	    	  
+	    	  JSONArray jsonCollectionTypes = (JSONArray) jsonMetadataValidationRule.get("collectionTypes");
+	    	  if(jsonCollectionTypes != null) {
+		    	 Iterator<String> collectionTypeIterator = jsonCollectionTypes.iterator();
+		    	 while(collectionTypeIterator.hasNext()) {
+		    	   	   metadataValidationRule.getCollectionTypes().add(collectionTypeIterator.next());
+		    	 }
+	    	  }
+	    	  
+	    	  // Extract the valid values.
+	    	  JSONArray jsonValidValues = (JSONArray) jsonMetadataValidationRule.get("validValues");
+	    	  if(jsonValidValues != null) {
+	    	     Iterator<String> validValuesIterator = jsonValidValues.iterator();
+	    	     while(validValuesIterator.hasNext()) {
+	    	    	   metadataValidationRule.getValidValues().add(validValuesIterator.next());
+	    	     }
+	    	  }
+	    	  
+	    	  validationRules.add(metadataValidationRule);
+    	}
+    	
+    	return validationRules;
     }
 }
 
