2e87c016217cf9150f323bbdbbdb9345cb0db996
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-06-01 15:27:54 -0400

    Test check-in to master

--- src/hpc/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcDataManagementSecurityService.java
+++ src/hpc/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcDataManagementSecurityService.java
@@ -22,7 +22,7 @@ import java.util.List;
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
- * @version $Id$ (Last commit before master)
+ * @version $Id$
  */
 
 public interface HpcDataManagementSecurityService 
