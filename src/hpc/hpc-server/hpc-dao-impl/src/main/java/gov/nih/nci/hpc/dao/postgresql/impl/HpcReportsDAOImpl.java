d30d930895dd40809c9965a42f76ddeed98bd868
Prasad Konka <prasad>
2017-06-08 17:16:42 -0400

    reports

--- src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcReportsDAOImpl.java
+++ src/hpc/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/postgresql/impl/HpcReportsDAOImpl.java
@@ -174,7 +174,7 @@ public class HpcReportsDAOImpl implements HpcReportsDAO
 			"(select distinct b.object_id from public.r_meta_main a, public.r_objt_metamap b where a.meta_attr_name='registered_by' and a.meta_attr_value=? and a.meta_id=b.meta_id)";
 
 	private static final String AVERAGE_FILE_BY_USER_SQL = 
-			"SELECT avg(to_number(a.meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main a, public.r_objt_metamap b, public.r_data_main c where a.meta_attr_name = 'source_file_size' and b.object_id=c.data_id and b.object_id in " +
+			"SELECT avg(to_number(a.meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main a, public.r_objt_metamap b, public.r_data_main c where a.meta_attr_name = 'source_file_size' and a.meta_id=b.meta_id and b.object_id=c.data_id and b.object_id in " +
 			"(select distinct b.object_id from public.r_meta_main a, public.r_objt_metamap b,  public.r_data_main c where a.meta_attr_name='registered_by' and a.meta_attr_value=? and a.meta_id=b.meta_id and b.object_id=c.data_id)";
 
 	private static final String TOTAL_NUM_OF_DATA_OBJECTS_BY_USER_SQL = 
@@ -203,7 +203,7 @@ public class HpcReportsDAOImpl implements HpcReportsDAO
 			"(select distinct b.object_id from public.r_meta_main a, public.r_objt_metamap b where a.meta_attr_name='registered_by' and a.meta_attr_value=? and a.meta_id=b.meta_id) and CAST(a.create_ts as double precision) BETWEEN ? AND ?";
 
 	private static final String AVERAGE_FILE_BY_USER_DATE_SQL = 
-			"SELECT avg(to_number(a.meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main a, public.r_objt_metamap b, public.r_data_main c where a.meta_attr_name = 'source_file_size' and b.object_id=c.data_id and b.object_id  in " +
+			"SELECT avg(to_number(a.meta_attr_value, '9999999999999999999')) totalSize FROM public.r_meta_main a, public.r_objt_metamap b, public.r_data_main c where a.meta_attr_name = 'source_file_size' and a.meta_id=b.meta_id and b.object_id=c.data_id and b.object_id  in " +
 			"(select b.object_id from public.r_meta_main a, public.r_objt_metamap b, public.r_data_main c where a.meta_attr_name='registered_by' and a.meta_attr_value=? and a.meta_id=b.meta_id and b.object_id=c.data_id) and CAST(c.create_ts as double precision) BETWEEN ? AND ?";
 
 	private static final String TOTAL_NUM_OF_DATA_OBJECTS_BY_USER_DATE_SQL = 
