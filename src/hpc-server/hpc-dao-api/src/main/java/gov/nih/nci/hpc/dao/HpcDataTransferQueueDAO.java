6124ef1429f195d9e42b742e2069a4d9511195b8
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-08-16 20:42:51 -0700

    globus upload queue

---
+++ src/hpc-server/hpc-dao-api/src/main/java/gov/nih/nci/hpc/dao/HpcDataTransferQueueDAO.java
@@ -0,0 +1,35 @@
+/**
+ * HpcDataTransferQueueDAO.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.dao;
+
+import gov.nih.nci.hpc.domain.datatransfer.HpcDataObjectUploadRequest;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * <p>
+ * HPC Globus transfer (upload/download) queues DAO Interface.
+ * </p>
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ */
+
+public interface HpcDataTransferQueueDAO 
+{    
+    /**
+     * Store a new data object upload request.
+     *
+     * @param uploadRequest The data object upload request to persist.
+     * @throws HpcException on database error.
+     */
+    public void upsertUploadQueue(HpcDataObjectUploadRequest uploadRequest) throws HpcException;
+}
+
+ 
