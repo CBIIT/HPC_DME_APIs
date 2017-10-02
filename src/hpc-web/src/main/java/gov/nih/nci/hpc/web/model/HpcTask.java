4f98d8902a5fd20374d9f1676e36e25828ef4308
Prasad Konka <prasad.konka@gmail.com>
2017-10-02 17:03:53 -0400

    Download tasks UI

---
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/model/HpcTask.java
@@ -0,0 +1,64 @@
+package gov.nih.nci.hpc.web.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+public class HpcTask {
+	@JsonView(Views.Public.class)
+    protected String taskId;
+	@JsonView(Views.Public.class)
+    protected String path;
+	@JsonView(Views.Public.class)
+    protected String type;
+	@JsonView(Views.Public.class)
+    protected String created;
+	@JsonView(Views.Public.class)
+    protected String completed;
+	@JsonView(Views.Public.class)
+    protected String result;
+	@JsonView(Views.Public.class)
+    protected String status;
+
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getCompleted() {
+		return completed;
+	}
+	public void setCompleted(String completed) {
+		this.completed = completed;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
---
+++ src/hpc-web/src/main/resources/static/js/hpcTaskboard.js
@@ -0,0 +1,33 @@
+var app = angular.module('DashBoard', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns']);
+var linkSearchNameCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
+'  <a href="search?queryName={{row.getProperty(\'message\')}}">{{row.getProperty(col.field)}}</a>' +
+'</div>';
+
+app.controller('DashBoardCtrl', ['$scope', '$http', function ($scope, $http) {
+	$scope.searchesloading = true;
+	$http.get('/downloadTasks').
+	  success(function(data, status, headers, config) {
+        	$scope.hpcQueries = data;
+        	$scope.searchesloading = false;
+	  }).
+	  error(function(data, status, headers, config) {
+		console.log('Failure', status);
+	  });
+
+  $scope.gridOptions1 = {
+    data: 'hpcQueries',
+    paginationPageSize: 100,
+    enableFiltering: true,
+    enableRowHeaderSelection: false, 
+    multiSelect: false,
+    enableGridMenu: true,
+    enableSorting: true,
+    enableColumnResizing: true, 
+    columnDefs: [
+                 { field: 'path', width:300, displayName: 'Path', cellTemplate: '<div class="ui-grid-cell-contents" title="TOOLTIP"><a href="task?taskId={{row.entity.taskId}}&amp;type={{row.entity.type}}">{{COL_FIELD CUSTOM_FILTERS}}</a></div>'  },
+                 { field: 'type', width:200, displayName : 'Type'},
+                 { field: 'result', width:200, displayName: 'Transfer Result'},
+                 { field: 'taskId', width:200, displayName : 'Task Id' },
+               ],
+  };
+}]);
