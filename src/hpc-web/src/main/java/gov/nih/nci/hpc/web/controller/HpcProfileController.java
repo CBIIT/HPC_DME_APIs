e319e5f03da808232e8cb6063506fce3a5377fe0
kanozad <douglas.kanoza@nih.gov>
2017-11-02 13:39:43 -0400

    Interim commit

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcProfileController.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcProfileController.java
@@ -3,11 +3,12 @@ package gov.nih.nci.hpc.web.controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/profile")
-public class HpcSettingsController {
+public class HpcProfileController extends AbstractHpcController {
 
 	@RequestMapping(method = RequestMethod.GET)
     public String profile() {
