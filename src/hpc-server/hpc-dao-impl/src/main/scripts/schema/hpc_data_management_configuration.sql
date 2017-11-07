5c4bc0347bbbf7ae43e58ffeffdcd380777e20a4
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-11-06 21:09:33 -0800

    Added documentation to 5 tables schema creation scripts. Updated SDD with generated data dictionary

--- src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_management_configuration.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_management_configuration.sql
@@ -28,3 +28,26 @@ CREATE TABLE public."HPC_DATA_MANAGEMENT_CONFIGURATION"
 WITH (
   OIDS=FALSE
 );
+
+COMMENT ON TABLE public."HPC_DATA_MANAGEMENT_CONFIGURATION" IS 
+                 'The data management configurations supported by HPC-DME';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."ID" IS 
+                  'The configuration ID';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."BASE_PATH" IS 
+                  'The base path to apply this configuration to';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."DOC" IS 
+                  'The DOC that own this configuration';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."S3_URL" IS 
+                  'The S3 archive (Cleversafe) URL';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."S3_VAULT" IS 
+                  'The S3 archive (Cleversafe) vault';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."S3_OBJECT_ID" IS 
+                  'The S3 archive (Cleversafe) object id prefix';
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."S3_ARCHIVE_TYPE" IS 
+                  'The S3 archive type (Archive / Temp Archive). Note: Temp Archive is currently not used';  
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."DATA_HIERARCHY" IS 
+                  'The data hierarchy policy';    
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."COLLECTION_METADATA_VALIDATION_RULES" IS 
+                  'The collection metadata validation rules';    
+COMMENT ON COLUMN public."HPC_DATA_MANAGEMENT_CONFIGURATION"."DATA_OBJECT_METADATA_VALIDATION_RULES" IS 
+                  'The data object metadata validation rules';           
