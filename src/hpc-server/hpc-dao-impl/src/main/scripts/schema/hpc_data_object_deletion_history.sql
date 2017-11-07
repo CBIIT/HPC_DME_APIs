f8fae8ff04471ca02b1c0a5ba09759bcbe2f5f94
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-11-06 21:09:33 -0800

    Added documentation to 5 tables schema creation scripts. Updated SDD with generated data dictionary

--- src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_object_deletion_history.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_object_deletion_history.sql
@@ -27,3 +27,24 @@ CREATE TABLE public."HPC_DATA_OBJECT_DELETION_HISTORY"
 WITH (
   OIDS=FALSE
 );
+
+COMMENT ON TABLE public."HPC_DATA_OBJECT_DELETION_HISTORY" IS 
+                 'Data object deletion requests';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."USER_ID" IS 
+                  'The user ID who submitted the request';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."PATH" IS 
+                  'The data object path';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."METADATA" IS 
+                  'The data object metadata at the time of deletion';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."ARCHIVE_FILE_CONTAINER_ID" IS 
+                  'The data object archive container ID';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."ARCHIVE_FILE_ID" IS 
+                  'The data object archive file ID';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."ARCHIVE_DELETE_STATUS" IS 
+                  'Deletion from archive (Cleversafe) success/fail indicator';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."DATA_MANAGEMENT_DELETE_STATUS" IS 
+                  'Deletion from data management (iRODS) success/fail indicator';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."MESSAGE" IS 
+                  'Error message if the deletion failed (iRODS or Cleversafe)';
+COMMENT ON COLUMN public."HPC_DATA_OBJECT_DELETION_HISTORY"."DELETED" IS 
+                  'The date/time the request was submitted';
