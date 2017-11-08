581e341887be9e4d491c9515dcadc4da4e7e4c1e
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-11-07 19:53:30 -0800

    Completed data dictionary in SQL schema scripts and updated SDD

--- src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_user.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_user.sql
@@ -28,3 +28,24 @@ CREATE TABLE public."HPC_USER"
 WITH (
   OIDS=FALSE
 );
+
+COMMENT ON TABLE public."HPC_USER" IS 
+                 'HPC-DME Users';
+COMMENT ON COLUMN public."HPC_USER"."USER_ID" IS 
+                  'The user ID';
+COMMENT ON COLUMN public."HPC_USER"."FIRST_NAME" IS 
+                  'The user first name';
+COMMENT ON COLUMN public."HPC_USER"."LAST_NAME" IS 
+                  'The user last name';
+COMMENT ON COLUMN public."HPC_USER"."DOC" IS 
+                  'The DOC the user belongs to';
+COMMENT ON COLUMN public."HPC_USER"."DEFAULT_CONFIGURATION_ID" IS 
+                  'The default configuration ID associated with the user';
+COMMENT ON COLUMN public."HPC_USER"."ACTIVE" IS 
+                  'User active indicator';
+COMMENT ON COLUMN public."HPC_USER"."CREATED" IS 
+                  'The date / time the user was created';
+COMMENT ON COLUMN public."HPC_USER"."LAST_UPDATED" IS 
+                  'The date / time the user was updated';
+COMMENT ON COLUMN public."HPC_USER"."ACTIVE_UPDATED_BY" IS 
+                  'The administrator user ID that activated this user';
