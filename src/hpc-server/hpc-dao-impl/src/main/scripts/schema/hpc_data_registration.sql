2803fadbedbe53004be202cc6c74bc66e37d2de5
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-11-07 19:53:30 -0800

    Completed data dictionary in SQL schema scripts and updated SDD

--- src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_registration.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_data_registration.sql
@@ -24,6 +24,19 @@ WITH (
   OIDS=FALSE
 );
 
+COMMENT ON TABLE public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK" IS 
+                 'Bulk data object registration tasks';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK"."ID" IS 
+                  'The bulk registration task ID';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK"."USER_ID" IS 
+                  'The user ID who submitted the request';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK"."STATUS" IS 
+                  'The bulk registration task status';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK"."ITEMS" IS 
+                  'The list individual data object registrations included in this bulk registration request, in JSON format';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_TASK"."CREATED" IS 
+                  'The data/time the bulk registration request was submitted';
+
 DROP TABLE IF EXISTS public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT";
 CREATE TABLE public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"
 (
@@ -39,3 +52,19 @@ WITH (
   OIDS=FALSE
 );
 
+COMMENT ON TABLE public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT" IS 
+                 'Bulk data object registration task results';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."ID" IS 
+                  'The bulk registration task ID';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."USER_ID" IS 
+                  'The user ID who submitted the request';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."RESULT" IS 
+                  'Task success/failure indicator';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."MESSAGE" IS 
+                  'An error message if the task failed';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."ITEMS" IS 
+                  'The list individual data object registrations included in this bulk registration request, in JSON format';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."CREATED" IS 
+                  'The data/time the bulk registration request was submitted';
+COMMENT ON COLUMN public."HPC_BULK_DATA_OBJECT_REGISTRATION_RESULT"."COMPLETED" IS 
+                  'The data/time the bulk registration request was completed';
--- src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_user_bookmark.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_user_bookmark.sql
@@ -25,3 +25,18 @@ CREATE TABLE public."HPC_USER_BOOKMARK"
 WITH (
   OIDS=FALSE
 );
+
+COMMENT ON TABLE public."HPC_USER_BOOKMARK" IS 
+                 'User bookmarks';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."USER_ID" IS 
+                  'The user ID that owns this bookmark';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."BOOKMARK_NAME" IS 
+                  'The bookmark name';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."BOOKMARK_GROUP" IS 
+                  'The bookmark group';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."PATH" IS 
+                  'The bookmark path';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."CREATED" IS 
+                  'The date / time the bookmark was created';
+COMMENT ON COLUMN public."HPC_USER_BOOKMARK"."UPDATED" IS 
+                  'The date / time the bookmark was updated';
