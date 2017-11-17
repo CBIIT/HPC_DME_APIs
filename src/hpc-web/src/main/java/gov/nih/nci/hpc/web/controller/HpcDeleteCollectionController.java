d1f2c7e9144195bc88c01143cbe1e87d55bf25f1
William Y. Liu <william.liu2@nih.gov>
2017-11-17 16:08:11 -0500

    HPCDATAMGM-846: Delete a collection through UI
    Changes to modify collection detail view so that upon clicking delete icon, the confirmation prompt is via a JavaScript confirm dialog instead of custom Bootstrap-based modal dialog.

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcDeleteCollectionController.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcDeleteCollectionController.java
@@ -1,10 +1,15 @@
 package gov.nih.nci.hpc.web.controller;
 
+import gov.nih.nci.hpc.web.util.HpcClientUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -19,8 +24,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/deleteCollection")
 public class HpcDeleteCollectionController extends AbstractHpcController {
 
+    @Value("${gov.nih.nci.hpc.server.collection}")
+    private String serviceURL;
+
+    /**
+     * Controller action to present view that prompts user for confirmation
+     * about deleting a collection.
+     *
+     * @param session The HTTP session object
+     *
+     * @return <code>String</code> indicating which view template to apply
+     */
     @RequestMapping(method = RequestMethod.GET, path = "/confirmationView")
     public String presentConfirmationView() {
+//      @RequestParam("collection") String collectionPath, HttpSession session) {
+/*
+        final String authToken = (String) session.getAttribute("hpcUserToken");
+        HpcCollectionListDTO collection =
+          HpcClientUtil.getCollection(
+            authToken,
+            serviceURL,
+            collectionPath,
+            true,
+            false,
+            sslCertPath,
+            sslCertPassword);
+        boolean emptyFlag =
+          collection.getCollections().get(0).getSubCollections().isEmpty();
+*/
         return "deleteCollectionConfirmation";
     }
 
