c11977e77112007ee52bbf53525ca6e31dcef645
kanozad <douglas.kanoza@nih.gov>
2017-11-24 11:29:52 -0500

    User profile page

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcProfileController.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcProfileController.java
@@ -1,7 +1,13 @@
 package gov.nih.nci.hpc.web.controller;
 
+import gov.nih.nci.hpc.dto.datamanagement.HpcDataManagementModelDTO;
+import gov.nih.nci.hpc.dto.datamanagement.HpcDataManagementRulesDTO;
+import gov.nih.nci.hpc.dto.datamanagement.HpcDocDataManagementRulesDTO;
+import gov.nih.nci.hpc.dto.datamanagement.HpcUserPermsForCollectionsDTO;
+import gov.nih.nci.hpc.dto.security.HpcUserDTO;
 import gov.nih.nci.hpc.web.model.HpcLogin;
 import gov.nih.nci.hpc.web.model.HpcWebUser;
+import gov.nih.nci.hpc.web.util.HpcClientUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import gov.nih.nci.hpc.dto.security.HpcUserDTO;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -23,45 +27,77 @@ import javax.servlet.http.HttpSession;
 @EnableAutoConfiguration
 @RequestMapping("/profile")
 public class HpcProfileController extends AbstractHpcController {
-	@Value("${gov.nih.nci.hpc.server.acl}")
-	private String serverAclURL;
-	@Value("${gov.nih.nci.hpc.server.user}")
-	private String serviceURL;
-	@Value("${gov.nih.nci.hpc.server.collection}")
-	private String serverCollectionURL;
-	@Value("${gov.nih.nci.hpc.server.dataObject}")
-	private String serverDataObjectURL;
-	@Value("${hpc.serviceaccount}")
-	private String serviceAccount;
-	@Value("${gov.nih.nci.hpc.server.user.all}")
-	private String allUsersServiceURL;
-	@Value("${gov.nih.nci.hpc.server.user.active}")
-	private String activeUsersServiceURL;
+    @Value("${gov.nih.nci.hpc.server.acl}")
+    private String serverAclURL;
+    @Value("${gov.nih.nci.hpc.server.user}")
+    private String serviceURL;
+    @Value("${gov.nih.nci.hpc.server.collection}")
+    private String serverCollectionURL;
+    @Value("${gov.nih.nci.hpc.server.dataObject}")
+    private String serverDataObjectURL;
+    @Value("${hpc.serviceaccount}")
+    private String serviceAccount;
+    @Value("${gov.nih.nci.hpc.server.user.all}")
+    private String allUsersServiceURL;
+    @Value("${gov.nih.nci.hpc.server.user.active}")
+    private String activeUsersServiceURL;
+    @Value("${gov.nih.nci.hpc.server.collection.acl.user}")
+    private String collectionAclURL;
+    @Value("${gov.nih.nci.hpc.server.model}")
+    private String hpcModelURL;
 
-	// The logger instance.
-	private final Logger logger =
-		LoggerFactory.getLogger(this.getClass().getName());
+    // The logger instance.
+    private final Logger logger =
+        LoggerFactory.getLogger(this.getClass().getName());
 
 
-	/**
-	 *
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.GET)
+    /**
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
     public String profile(@RequestBody(required = false) String q, Model model, BindingResult bindingResult,
                           HttpSession session, HttpServletRequest request) {
-    	HpcUserDTO user = (HpcUserDTO) session.getAttribute("hpcUser");
-		if (user == null) {
-			ObjectError error = new ObjectError("hpcLogin", "Invalid user session!");
-			bindingResult.addError(error);
-			HpcLogin hpcLogin = new HpcLogin();
-			model.addAttribute("hpcLogin", hpcLogin);
-			return "redirect:/login?returnPath=profile";
-		}
+        HpcUserDTO user = (HpcUserDTO) session.getAttribute("hpcUser");
 
-		HpcWebUser webUser = new HpcWebUser();
-		model.addAttribute("hpcWebUser", webUser);
-		model.addAttribute("profile", user);
+        if (user == null) {
+            ObjectError error = new ObjectError("hpcLogin", "Invalid user session!");
+            bindingResult.addError(error);
+            HpcLogin hpcLogin = new HpcLogin();
+            model.addAttribute("hpcLogin", hpcLogin);
+            return "redirect:/login?returnPath=profile";
+        }
+        String authToken = (String) session.getAttribute("hpcUserToken");
+        String userId = (String) session.getAttribute("hpcUserId");
+
+        HpcDataManagementModelDTO modelDTO = (HpcDataManagementModelDTO) session.getAttribute("userDOCModel");
+        if (modelDTO == null) {
+            modelDTO = HpcClientUtil.getDOCModel(authToken, hpcModelURL, sslCertPath, sslCertPassword);
+            session.setAttribute("userDOCModel", modelDTO);
+        }
+
+        HpcClientUtil.populateBasePaths(session, model, modelDTO, authToken, userId, collectionAclURL,
+            sslCertPath, sslCertPassword);
+
+        String queryParams = "?";
+
+        for (HpcDocDataManagementRulesDTO docRule : modelDTO.getDocRules()) {
+            for (HpcDataManagementRulesDTO rule : docRule.getRules()) {
+                queryParams += "collectionPath=" + rule.getBasePath() + "&";
+            }
+        }
+
+       HpcUserPermsForCollectionsDTO permissions = HpcClientUtil.getPermissionForCollections(authToken,  collectionAclURL+"/"+userId,  queryParams,
+            sslCertPath, sslCertPassword);
+
+        log.info("authToken: " + authToken);
+        log.info("userId: " + userId);
+        log.info("userDOCModel: " + modelDTO);
+        log.info("permissions: " + permissions);
+
+        HpcWebUser webUser = new HpcWebUser();
+        model.addAttribute("profile", user);
+        model.addAttribute("userDOCModel", modelDTO);
+        model.addAttribute("permissions", permissions);
         return "profile";
     }
 
