7bbe462303c3521a4e20cbc00ff45683d5346e1c
William Y. Liu <william.liu2@nih.gov>
2017-11-29 12:33:28 -0500

    HPCDATAMGM-846: Delete a collection through UI
    Changes to fix logic that determines whether deleting a Collection is allowed.  Specifically, changes address determining whether current authenticated User has Group Admin Role or System Admin Role and also has Own permission on the Collection in context.
    
    Added HpcCollectionUtil and HpcIdentityUtil utility classes to contain reusable methods related to logic dealing with a Collection and logic dealing with User identity info, respectively.

---
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/util/HpcIdentityUtil.java
@@ -0,0 +1,69 @@
+package gov.nih.nci.hpc.web.util;
+
+import gov.nih.nci.hpc.dto.security.HpcUserDTO;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Utility class for methods supporting User identity related logic.
+ *
+ * @author liuwy
+ */
+public class HpcIdentityUtil {
+
+    /**
+     * Check if current authenticated User has the Role of Group Admin.
+     *
+     * @param session  The HTTP session
+     * @return true if User has Group Admin Role, false otherwise
+     */
+    public static boolean isUserGroupAdmin(HttpSession session) {
+        boolean retVal = false;
+        if (null != session &&
+            session.getAttribute("hpcUser") instanceof HpcUserDTO)
+        {
+            HpcUserDTO userDto = (HpcUserDTO) session.getAttribute("hpcUser");
+            retVal = "GROUP_ADMIN".equals(userDto.getUserRole());
+        }
+        return retVal;
+    }
+
+
+    /**
+     * Check if current authenticated User has the Role of System Admin.
+     *
+     * @param session  The HTTP session
+     * @return true if User has System Admin Role, false otherwise
+     */
+    public static boolean isUserSystemAdmin(HttpSession session) {
+        boolean retVal = false;
+        if (null != session &&
+            session.getAttribute("hpcUser") instanceof HpcUserDTO)
+        {
+            HpcUserDTO userDto = (HpcUserDTO) session.getAttribute("hpcUser");
+            retVal = "SYSTEM_ADMIN".equals(userDto.getUserRole());
+        }
+        return retVal;
+    }
+
+
+    /**
+     * Check if current authenticated User has the Role of System Admin or
+     * the Role of Group Admin.
+     *
+     * @param session  The HTTP session
+     * @return true if User has System Admin Role or Group Admin Role, false
+     *          otherwise
+     */
+    public static boolean iUserSystemAdminOrGroupAdmin(HttpSession session) {
+        boolean retVal = false;
+        if (null != session &&
+            session.getAttribute("hpcUser") instanceof HpcUserDTO)
+        {
+            HpcUserDTO userDto = (HpcUserDTO) session.getAttribute("hpcUser");
+            retVal = "SYSTEM_ADMIN".equals(userDto.getUserRole()) ||
+                     "GROUP_ADMIN".equals(userDto.getUserRole());
+        }
+        return retVal;
+    }
+
+}
