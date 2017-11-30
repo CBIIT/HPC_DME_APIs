7ea559d83b1a3cad594dd7df308d539ac1aafa2d
Prasad Konka <prasad.konka@gmail.com>
2017-11-30 10:13:53 -0500

    Error message update

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/HPCCmdCollection.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/HPCCmdCollection.java
@@ -248,7 +248,7 @@ public class HPCCmdCollection extends HPCCmdClient {
 			}
 
 		} catch (Exception e) {
-			System.out.println("Cannot read the input file: "+e.getMessage());
+			System.out.println(e.getMessage());
 		}
 		return success;
 	}
