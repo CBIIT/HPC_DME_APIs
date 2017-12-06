bd2caad09c3603b18dc8e07c0b042b980b169bb1
Eran Rosenberg <eran.rosenberg@nih.gov>
2017-12-05 18:22:46 -0800

    Fix pattern starts with '/'

--- src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataTransferServiceImpl.java
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataTransferServiceImpl.java
@@ -1071,21 +1071,17 @@ public class HpcDataTransferServiceImpl implements HpcDataTransferService {
    * @return The REGEX pattern.
    */
   private String toRegex(String pattern) {
+    // Ensure the pattern starts with '/'.
+    String regex = !pattern.startsWith("/") ? "/" + pattern : pattern;
+
     // Convert the '**' to regex.
-    String regex = pattern.replaceAll(Pattern.quote("**"), ".*");
+    regex = regex.replaceAll(Pattern.quote("**"), ".*");
 
     // Convert the '*' to regex.
     regex = regex.replaceAll("([^\\.])\\*", "$1[^/]*");
 
     // Convert the '?' to regex.
-    regex = regex.replaceAll(Pattern.quote("?"), ".");
-
-    // Ensure the pattern starts with '/'.
-    if (!regex.startsWith("/")) {
-      regex = "/" + regex;
-    }
-
-    return regex;
+    return regex.replaceAll(Pattern.quote("?"), ".");
   }
 
   // Second hop download.
