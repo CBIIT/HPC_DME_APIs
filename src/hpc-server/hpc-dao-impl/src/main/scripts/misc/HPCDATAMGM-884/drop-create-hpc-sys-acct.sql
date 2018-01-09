3ca1fd8e09842a7a4c28486d7c35b84562c61675
William Y. Liu <william.liu2@nih.gov>
2018-01-09 13:58:37 -0500

    HPCDATAMGM-884: Setting Globus application accounts group
    dev-hpc-sys-acct-table-2018jan09.backup is backup of DEV database's "HPC_SYSTEM_ACCOUNT" table.
    
    orig-hpc-sys-acct-definition.sql is DDL export of "HPC_SYSTEM_ACCOUNT" table from DEV database.
    
    drop-create-hpc-sys-acct.sql is SQL script intended for modifying "HPC_SYSTEM_ACCOUNT" table for the work on this issue.

---
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/misc/HPCDATAMGM-884/drop-create-hpc-sys-acct.sql
@@ -0,0 +1,67 @@
+﻿START TRANSACTION;
+
+CREATE TEMPORARY TABLE hpc_tmp_sys_acct
+AS (
+   SELECT "SYSTEM" system_name, "CLASSIFIER" classifier, "DATA_TRANSFER_TYPE" transfer_type, 
+          "USERNAME" username, "PASSWORD" psswd
+   FROM public."HPC_SYSTEM_ACCOUNT"
+   ORDER BY "SYSTEM", "CLASSIFIER"
+);
+
+DROP TABLE public."HPC_SYSTEM_ACCOUNT";
+
+DROP SEQUENCE IF EXISTS public."HPC_SYSTEM_ACCOUNT_SEQ";
+
+CREATE SEQUENCE public."HPC_SYSTEM_ACCOUNT_SEQ"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  CACHE 1;
+
+ALTER TABLE public."HPC_SYSTEM_ACCOUNT_SEQ"
+  OWNER TO postgres;
+
+CREATE TABLE public."HPC_SYSTEM_ACCOUNT"
+(
+  "ID" integer NOT NULL DEFAULT nextval('"HPC_SYSTEM_ACCOUNT_SEQ"'::regclass), -- The system account ID
+  "SYSTEM" text NOT NULL, -- The system
+  "CLASSIFIER" text, -- A classifier to be more specific under/within system
+  "DATA_TRANSFER_TYPE" text, -- The data transfer type (S3 for Cleversafe etc)
+  "USERNAME" text NOT NULL, -- The user name
+  "PASSWORD" bytea NOT NULL, -- The password
+  CONSTRAINT "HPC_SYSTEM_ACCOUNT_pkey" PRIMARY KEY ("ID")
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE public."HPC_SYSTEM_ACCOUNT" OWNER TO irods;
+
+GRANT ALL ON TABLE public."HPC_SYSTEM_ACCOUNT" TO irods;
+
+COMMENT ON TABLE public."HPC_SYSTEM_ACCOUNT" IS 
+  'System accounts';
+
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."ID" IS 
+  'PK column; the unique ID value for the system account record';
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."SYSTEM" IS 
+  'The system';
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."CLASSIFIER" IS 
+  'A classifier to be more specific at finer grainer than which system';
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."DATA_TRANSFER_TYPE" IS 
+  'The data transfer type (S3 for Cleversafe etc)';
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."USERNAME" IS 
+  'The user name';
+COMMENT ON COLUMN public."HPC_SYSTEM_ACCOUNT"."PASSWORD" IS 
+  'The password';
+
+INSERT INTO "HPC_SYSTEM_ACCOUNT"
+  ("SYSTEM", "CLASSIFIER", "DATA_TRANSFER_TYPE", "USERNAME", "PASSWORD")
+  SELECT system_name, classifier, transfer_type, username, psswd
+  FROM hpc_tmp_sys_acct
+  ORDER BY system_name, classifier;
+
+DROP TABLE hpc_tmp_sys_acct;
+
+COMMIT;
+--ROLLBACK;
