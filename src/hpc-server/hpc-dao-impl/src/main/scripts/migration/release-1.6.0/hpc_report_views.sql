4e3209fce6b17f207d0d566f2efbd54fa45d48ad
Prasad Konka <prasad.konka@gmail.com>
2018-02-05 14:14:37 -0500

    Added missing report views from the main script

--- src/hpc-server/hpc-dao-impl/src/main/scripts/migration/release-1.6.0/hpc_report_views.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/migration/release-1.6.0/hpc_report_views.sql
@@ -4,7 +4,7 @@ AS (
 select distinct c.coll_name, a.meta_attr_name, a.meta_attr_value, c.coll_id, b.meta_id, c.create_ts from public.r_meta_main a, public.r_objt_metamap b, r_coll_main c
 where a.meta_id=b.meta_id and b.object_id = c.coll_id  
 );
-CREATE UNIQUE INDEX r_report_coll_registered_by_path_uidx1 ON r_report_coll_registered_by_path(meta_attr_name, meta_attr_value, coll_id, coll_name);
+CREATE UNIQUE INDEX r_report_coll_registered_by_path_uidx1 ON r_report_coll_registered_by_path(meta_attr_name, meta_attr_value, coll_id, meta_id);
 
 DROP MATERIALIZED VIEW IF EXISTS r_report_registered_by_path;
 CREATE MATERIALIZED VIEW r_report_registered_by_path
@@ -22,3 +22,18 @@ select distinct d.coll_name, b.object_id, c.create_ts from public.r_meta_main a,
 );
 CREATE UNIQUE INDEX r_report_collection_path_uidx1 ON r_report_collection_path(object_id, coll_name);
 
+DROP MATERIALIZED VIEW IF EXISTS r_report_registered_by_basepath;
+CREATE MATERIALIZED VIEW r_report_registered_by_basepath
+AS (
+select distinct d."BASE_PATH", a.meta_attr_name, a.meta_attr_value, b.object_id, b.meta_id, c.create_ts from public.r_meta_main a, public.r_objt_metamap b, r_data_main c, public."HPC_DATA_MANAGEMENT_CONFIGURATION" d 
+	where a.meta_id=b.meta_id and b.object_id = c.data_id and a.meta_attr_name='configuration_id' and a.meta_attr_value=d."ID"
+);
+CREATE UNIQUE INDEX r_report_registered_by_basepath_uidx1 ON r_report_registered_by_basepath(meta_attr_name, meta_attr_value, object_id, "BASE_PATH");
+
+DROP MATERIALIZED VIEW IF EXISTS r_report_coll_registered_by_basepath;
+CREATE MATERIALIZED VIEW r_report_coll_registered_by_basepath
+AS (
+select distinct d."BASE_PATH", a.meta_attr_name, a.meta_attr_value, c.coll_id, b.meta_id, c.create_ts from public.r_meta_main a, public.r_objt_metamap b, r_coll_main c, public."HPC_DATA_MANAGEMENT_CONFIGURATION" d
+where a.meta_id=b.meta_id and b.object_id = c.coll_id and a.meta_attr_name='configuration_id' and a.meta_attr_value=d."ID" 
+);
+CREATE UNIQUE INDEX r_report_coll_registered_by_basepath_uidx1 ON r_report_coll_registered_by_basepath(meta_attr_name, meta_attr_value, coll_id, "BASE_PATH");
