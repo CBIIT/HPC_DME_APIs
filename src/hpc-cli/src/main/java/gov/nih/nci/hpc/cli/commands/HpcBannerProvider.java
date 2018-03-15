28d193a3a87388cc2c494c8f60e467ebe12733d4
Prasad Konka <prasad.konka@gmail.com>
2018-03-14 23:14:35 -0400

    Version number

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Copyright SVG, Inc.
  * Copyright Leidos Biomedical Research, Inc.
- *  
+ *
  * Distributed under the OSI-approved BSD 3-Clause License.
  * See https://github.com/CBIIT/HPC_DME_APIs/LICENSE.txt for details.
  ******************************************************************************/
 /*
  * Copyright 2011-2012 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "1.5.0";
+		return "1.7.0";
 	}
 
 	public String getWelcomeMessage() {
