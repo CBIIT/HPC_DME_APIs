55334597a62286704087f1ce6931618dc4f1bc4f
menons <sunita.menon@nih.gov>
2018-04-16 10:45:59 -0400

    HPCDATAMGM-857: Update add/update bookmark API to include userId and permission, revoke change to add user API

--- src/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcSecurityBusServiceImpl.java
+++ src/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcSecurityBusServiceImpl.java
@@ -19,11 +19,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import gov.nih.nci.hpc.bus.HpcSecurityBusService;
-import gov.nih.nci.hpc.domain.databrowse.HpcBookmark;
-import gov.nih.nci.hpc.domain.datamanagement.HpcPermission;
-import gov.nih.nci.hpc.domain.datamanagement.HpcPermissionForCollection;
-import gov.nih.nci.hpc.domain.datamanagement.HpcSubjectPermission;
-import gov.nih.nci.hpc.domain.datamanagement.HpcSubjectType;
 import gov.nih.nci.hpc.domain.error.HpcErrorType;
 import gov.nih.nci.hpc.domain.error.HpcRequestRejectReason;
 import gov.nih.nci.hpc.domain.model.HpcAuthenticationTokenClaims;
@@ -80,8 +75,6 @@ public class HpcSecurityBusServiceImpl implements HpcSecurityBusService {
   // The data management (iRODS) service.
   @Autowired private HpcDataManagementService dataManagementService = null;
   
-  @Autowired private HpcDataBrowseService dataBrowseService = null;
-
   // LDAP authentication on/off switch.
   @Value("${hpc.bus.ldapAuthentication}")
   private Boolean ldapAuthentication = null;
@@ -167,30 +160,8 @@ public class HpcSecurityBusServiceImpl implements HpcSecurityBusService {
       // Add the user to the system.
       securityService.addUser(nciAccount);
       
-      //Set collection permissions
-      List<HpcPermissionForCollection> collectionsPermission = userRegistrationRequest.getCollectionsPermission();
-      if(!collectionsPermission.isEmpty()) {
-    	  for(HpcPermissionForCollection collectionPermission: collectionsPermission) {
-    		  HpcSubjectPermission subjectPermission = new HpcSubjectPermission();
-    		    subjectPermission.setPermission(collectionPermission.getPermission());
-    		  	subjectPermission.setSubject(nciUserId);
-    		  	subjectPermission.setSubjectType(HpcSubjectType.USER);
-    		  	dataManagementService.setCollectionPermission(collectionPermission.getCollectionPath(), subjectPermission);
-    	  }
-      }
-      
-      //Add  bookmarks
-      List<HpcBookmark> bookmarks = userRegistrationRequest.getBookmarks();
-      if(!bookmarks.isEmpty()) {
-    	  for(HpcBookmark bookmark: bookmarks) {
-    		  
-    	    bookmark.setCreated(Calendar.getInstance());
-    		dataBrowseService.saveBookmark(nciUserId, bookmark);
-    	 }	  
-      }  
-      
       registrationCompleted = true;
-
+      
     } finally {
       if (!registrationCompleted) {
         // Registration failed. Remove the data management account.
