25fa9e6f247ceaaf51f195bccccd95cc0e0d4a6d
Eran Rosenberg <eran.rosenberg@nih.gov>
2018-06-18 13:29:53 -0400

    Cleversafe report  - override default cert check

--- src/cleversafe-reports/api/src/main/java/gov/nih/nci/hpc/reports/app/ReportsApplication.java
+++ src/cleversafe-reports/api/src/main/java/gov/nih/nci/hpc/reports/app/ReportsApplication.java
@@ -6,11 +6,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
 @SpringBootApplication
 @ComponentScan({"gov.nih.nci.hpc.reports.*"})
 public class ReportsApplication {
 
 	public static void main(String[] args) {
+
+		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		});
+
 		SpringApplication.run(ReportsApplication.class, args);
 	}
 }
