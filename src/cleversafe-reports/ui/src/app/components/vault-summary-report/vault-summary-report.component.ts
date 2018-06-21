3195fc5efb486b4c98e12d490ac6171cdc6a7c97
Eran Rosenberg <eran.rosenberg@nih.gov>
2018-06-20 21:13:59 -0700

    Cleversafe report app - ui to not wrap creation date

--- src/cleversafe-reports/ui/src/app/components/vault-summary-report/vault-summary-report.component.ts
+++ src/cleversafe-reports/ui/src/app/components/vault-summary-report/vault-summary-report.component.ts
@@ -8,23 +8,7 @@ import { VaultSummaryReportService} from "../../services/vault-summary-report.se
   styleUrls: ['./vault-summary-report.component.css']
 })
 export class VaultSummaryReportComponent implements OnInit {
-  /*vaults: VaultSummary[] = [
-    {
-      id: 1,
-      name: 'DSE-Vault1',
-      description: 'HPC-DME DEV vault',
-      used: 9147841410,
-      capacity: 16578821846
-    },
-    {
-      id: 2,
-      name: 'DSE-Vault2',
-      description: 'HPC-DME UAT vault',
-      used: 247324723236,
-      capacity: 8789798789787897
-    }];*/
-
-    vaults: VaultSummary[];
+  vaults: VaultSummary[];
 
   constructor(private vaultSummaryService: VaultSummaryReportService) { }
 
--- src/cleversafe-reports/ui/src/app/services/vault-summary-report.service.ts
+++ src/cleversafe-reports/ui/src/app/services/vault-summary-report.service.ts
@@ -10,22 +10,6 @@ import { MessageService} from "./message.service";
 export class VaultSummaryReportService {
   private vaultSummaryReportUrl = 'https://fr-s-hpcdm-gp-d.ncifcrf.gov:7738/reports/vaultsummary';
 
-  vaults: VaultSummary[] = [
-    {
-      id: 1,
-      name: 'DSE-Vault1-SERVICE',
-      description: 'HPC-DME DEV vault',
-      used: 9147841410,
-      capacity: 16578821846
-    },
-    {
-      id: 2,
-      name: 'DSE-Vault2-SERVICE',
-      description: 'HPC-DME UAT vault',
-      used: 247324723236,
-      capacity: 8789798789787897
-    }];
-
   constructor(private httpClient: HttpClient, private messageService: MessageService) { }
 
   getVaultSummaryReport(): Observable<VaultSummary[]> {
