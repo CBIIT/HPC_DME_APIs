9d60e4ced5200e9a11af5ef51f7b8a1a2fb6500e
menons <sunita.menon@nih.gov>
2018-06-28 09:25:02 -0400

    Cleaned up sample name derivation

--- scripts/CCR-SF/archive-transfer/app_meta.py
+++ scripts/CCR-SF/archive-transfer/app_meta.py
@@ -151,7 +151,8 @@ def register_object(filepath, type, tarfile_name, has_parent, fullpath):
         json.dump(object_metadata, fp)
 
     #register the object
-    archive_path = SFCollection.get_archive_path(tarfile_name, filepath.rsplit("/", 1)[0], type)
+    #archive_path = SFCollection.get_archive_path(tarfile_name, filepath.rsplit("/", 1)[0], type)
+    archive_path = SFCollection.get_archive_path(tarfile_name, filepath, type)
     archive_path = archive_path + '/' + file_name
 
     command = "dm_register_dataobject jsons_dryrun/" + json_file_name + " " + archive_path + " " + fullpath
