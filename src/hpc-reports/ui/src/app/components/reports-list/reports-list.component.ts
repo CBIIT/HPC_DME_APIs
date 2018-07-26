bff86b6b5f88cee0744bf89491bb95c015f7a9be
Eran Rosenberg <eran.rosenberg@nih.gov>
2018-07-25 18:20:00 -0700

    Cleversafe report app - new structure

---
+++ src/hpc-reports/ui/src/app/components/reports-list/reports-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Report} from "../../models/reports-list.model";
+import {ReportType} from "../../models/reports-list.model";
+
+@Component({
+  selector: 'app-reports-list',
+  templateUrl: './reports-list.component.html',
+  styleUrls: ['./reports-list.component.css']
+})
+export class ReportsListComponent implements OnInit {
+  reports: Report[] = [
+    {
+      type: ReportType.VAULT_SUMMARY,
+      name: 'Vault Summary'
+    },
+    {
+      type: ReportType.OTHER,
+      name: 'Other'
+    }];
+
+  selectedReport: ReportType;
+  none: ReportType
+  vaultSummaryReport: ReportType = ReportType.VAULT_SUMMARY;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.none = ReportType.NONE;
+    this.selectedReport = ReportType.VAULT_SUMMARY;
+  }
+}
---
+++ src/hpc-reports/ui/src/app/models/vault-summary.model.ts
@@ -0,0 +1,8 @@
+export class VaultSummary {
+  id: number;
+  name: string;
+  description: string;
+  capacity: number;
+  used: number;
+  creationDate: string;
+}
