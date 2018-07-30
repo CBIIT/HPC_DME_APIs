70af8d445aadc04404c49de7866cf79232c362a4
menons <sunita.menon@nih.gov>
2018-07-30 00:05:04 -0400

    fixed du option

--- scripts/CCR-SF/archive-transfer/common/sf_audit.py
+++ scripts/CCR-SF/archive-transfer/common/sf_audit.py
@@ -88,7 +88,7 @@ class SFAudit(object):
 
             # Get size of file in bytes
             if(fullpath.endswith("tar") or fullpath.endswith("tar.gz")):
-                filesize = os.system('du -u ' + self.extract_path)
+                filesize = os.system('du -b ' + self.extract_path)
             else:
                 filesize = os.path.getsize(fullpath)
             logging.info("\nFile size = {0}\n".format(filesize))
