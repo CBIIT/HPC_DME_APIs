e321ae6f6cdd5a80121d73ddab1ad948fd408640
menons <sunita.menon@nih.gov>
2018-07-30 18:02:16 -0400

    fixed filesize computation

--- scripts/CCR-SF/archive-transfer/common/sf_audit.py
+++ scripts/CCR-SF/archive-transfer/common/sf_audit.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import os
+import commands
 
 from metadata.sf_helper import SFHelper
 from common.sf_utils import SFUtils
@@ -88,7 +89,8 @@ class SFAudit(object):
 
             # Get size of file in bytes
             if(fullpath.endswith("tar") or fullpath.endswith("tar.gz")):
-                filesize = os.system('du -sb ' + self.extract_path).rsplit()[0]
+                status, output = commands.getstatusoutput('du -sb ' + self.extract_path)
+                filesize = int(output.split()[0])
             else:
                 filesize = os.path.getsize(fullpath)
             logging.info("\nFile size = {0}\n".format(filesize))
