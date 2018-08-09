36c69e9ebe080cac01e84d3cd851709408c94bff
menons <sunita.menon@nih.gov>
2018-08-08 22:30:40 -0400

    temporarily restore older Auto hierarchy

--- scripts/CCR-SF/archive-transfer/metadata/sf_collection.py
+++ scripts/CCR-SF/archive-transfer/metadata/sf_collection.py
@@ -47,8 +47,8 @@ class SFCollection(SFParent):
 
         archive_path = "/FNL_SF_Archive" #super(SFCollection, self).get_archive_path()
 
-        pi_coll_path = "/PI_Lab_" + SFHelper.get_pi_name(path, False)
-        project_path = "/Auto_Project_" + SFHelper.get_project_name(path, tarfile_name).replace(' ', '_')
+        pi_coll_path = "/Auto_PI_Lab_" + SFHelper.get_pi_name(path, False)
+        project_path = "/Project_" + SFHelper.get_project_name(path, tarfile_name).replace(' ', '_')
         flowcell_path = "/Flowcell_" + SFHelper.get_flowcell_id(tarfile_name, False)
 
 
