33ee514bfcf02bd295d8c8764601fa51fd3982f6
menons <sunita.menon@nih.gov>
2018-08-30 13:10:14 -0400

    Code cleanup

--- scripts/CMM/archive-transfer/metadata/sf_object.py
+++ scripts/CMM/archive-transfer/metadata/sf_object.py
@@ -1,6 +1,6 @@
 import logging
 
-from metadata.sf_helper import SFHelper
+from metadata.meta_helper import MetaHelper
 
 
 from collections import OrderedDict
@@ -10,46 +10,64 @@ import json
 class SFObject(object):
 
 
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self, filepath, parent):
+        self.file_path = filepath
         self.metadata = OrderedDict()
+        self.parent = parent
         self.metadata["metadataEntries"] = []
-        self.extensions = {"md5": "MD5SUM", "bai": "INDEX", "bam": "BAM", "fastq.gz": "FASTQ", "html": "HTML"}
         self.archive_path = None
-
-
-    def register(self):
-        self.build_metadata()
-        logging.info(self.metadata)
+        self.base_path = "/CCR_CMM_Archive"
+        self.extensions = {"md5": "MD5SUM", "bai": "INDEX", "bam": "BAM", "fastq.gz": "FASTQ", "html": "HTML", "dm4": "DM4"}
 
 
 
     def build_metadata(self):
-        self.set_object_name()
-        self.set_file_type()
 
+        name = self.file_path.split("/")[-1]
+        MetaHelper.add_metadata(self.metadata["metadataEntries"], "object_name", name)
 
-    def set_object_name(self):
-        name = self.filepath.split("/")[-1]
-        self.set_attribute("object_name", name)
-
-
-    def set_file_type(self):
+        match_found = False;
         for ext, type in self.extensions.items():
-            if self.filepath.endswith(ext):
-                self.set_attribute("file_type", type)
-                return
+            if self.file_path.endswith(ext):
+                MetaHelper.add_metadata(self.metadata["metadataEntries"], "file_type", type)
+                match_found = True
+                break
 
+        if not match_found:
+            extension = self.file_path.split('.')[-1]
+            MetaHelper.add_metadata(self.metadata["metadataEntries"], "file_type", extension)
 
-    def set_attribute(self, key, value):
-        item = {}
-        item["attribute"] = key;
-        item["value"] = value;
-        self.metadata["metadataEntries"].append(item);
+        logging.info(self.metadata)
+        #print self.metadata
+
+        return self.metadata
 
 
 
     def get_metadata(self):
-        if(not any(self.metadata["metadataEntries"])):
-            self.build_metadata()
+        #if(not any(self.metadata["metadataEntries"])):
+            #self.build_metadata()
         return self.metadata
+
+
+
+    def get_archive_path(self):
+        if self.archive_path is None:
+            self.set_archive_path()
+
+        return self.archive_path
+
+
+
+    def set_archive_path(self):
+
+        if self.parent is None:
+            parent_archive_path = self.base_path
+        else:
+            parent_archive_path = self.parent.archive_path
+
+        file_name = self.file_path.rsplit('/', 1)[-1]
+        self.archive_path = parent_archive_path + '/' + file_name
+
+
+        logging.info("Object archive path for " + self.file_path + "is: " + self.archive_path)
