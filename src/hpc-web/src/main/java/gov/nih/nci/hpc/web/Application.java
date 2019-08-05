1a2d5c4068a67e73419481f74c29b4742df4b287
menons <sunita.menon@nih.gov>
2019-08-05 16:49:19 -0400

    Revoked change to fix the InstanceAlreadyExistsException for MBean

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-//@SpringBootApplication - TBD: Remove this class
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
