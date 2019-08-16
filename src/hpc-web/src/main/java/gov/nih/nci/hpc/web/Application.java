67087f104d1bb87065ef43ebebee9edbc33e5be5
menons <sunita.menon@nih.gov>
2019-08-16 11:42:07 -0400

    HPCDATAMGM-1121: Added spring cache for storing DOCModel info

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/Application.java
@@ -3,8 +3,10 @@ package gov.nih.nci.hpc.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
