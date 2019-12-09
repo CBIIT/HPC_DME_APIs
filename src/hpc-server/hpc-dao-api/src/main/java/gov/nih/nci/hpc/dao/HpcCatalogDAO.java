a79ff176085afa0c8edb4d4fc9a668549a4ca2ba
dinhys <yuri.dinh@nih.gov>
2019-12-09 09:26:48 -0500

    HPCDATAMGM-1163: Provide REST API to retrieve Catalog Metadata

---
+++ src/hpc-server/hpc-dao-api/src/main/java/gov/nih/nci/hpc/dao/HpcCatalogDAO.java
@@ -0,0 +1,50 @@
+/**
+ * HpcCatalogDAO.java
+ *
+ * Copyright SVG, Inc.
+ * Copyright Leidos Biomedical Research, Inc
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+
+package gov.nih.nci.hpc.dao;
+
+import gov.nih.nci.hpc.domain.catalog.HpcCatalogCriteria;
+import gov.nih.nci.hpc.domain.catalog.HpcCatalogMetadataEntry;
+import gov.nih.nci.hpc.exception.HpcException;
+
+import java.util.List;
+
+/**
+ * <p>
+ * HPC Catalog DAO Interface.
+ * </p>
+ *
+ * @author <a href="mailto:yuri.dinh@nih.gov">Yuri Dinh</a>
+ * @version $Id$
+ */
+
+public interface HpcCatalogDAO 
+{    
+    /**
+     * Get catalog entries for the specified criteria. 
+     *
+     * @param criteria The user specified criteria.
+     * @return List of meta data catalog entries.
+     * @throws HpcException on database error.
+     */
+    public List<HpcCatalogMetadataEntry> getCatalog(HpcCatalogCriteria criteria, int offset, int limit) throws HpcException;
+    
+    /**
+     * Get count of catalog entries for the specified criteria. 
+     *
+     * @param criteria The user specified criteria.
+     * @return Count of meta data catalog entries.
+     * @throws HpcException on database error.
+     */
+    public int getCatalogCount(HpcCatalogCriteria criteria) throws HpcException;
+    
+}
+
+ 
