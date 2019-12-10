ce9f41bcc921a9ab99caeb2ba5e1f7bcff98f995
dinhys <yuri.dinh@nih.gov>
2019-12-10 12:19:03 -0500

    HPCDATAMGM-1187: Auditing Improvements - Record registration information

--- src/hpc-server/hpc-dao-impl/src/main/scripts/migration/release-1.21.0/hpc_report_views.sql
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/migration/release-1.21.0/hpc_report_views.sql
@@ -16,26 +16,25 @@ DROP MATERIALIZED VIEW IF EXISTS r_report_registered_by_audit;
 CREATE MATERIALIZED VIEW r_report_registered_by_audit
 AS (
 select
-  size.data_id,
+  data.data_id,
   basepath."BASE_PATH",
   doc."DOC",
-  datapath.data_path,
+  data.data_path,
   CAST(size.meta_attr_value AS BIGINT)      as "SIZE",
   user_id.meta_attr_value   as "USER_ID",
-  type.meta_attr_value      as "DATA_TRANSFER_TYPE",
+  transferType.meta_attr_value      as "DATA_TRANSFER_TYPE",
+  sourceEndpoint.meta_attr_value	as "SOURCE_ENDPOINT",
   CAST(created.meta_attr_value AS TIMESTAMP)   as "CREATED",
   CAST(completed.meta_attr_value AS TIMESTAMP) as "COMPLETED"
 from
-  r_report_data_objects size, r_report_data_objects user_id, r_report_data_objects type, r_report_data_objects created,
-  r_report_data_objects completed, r_report_registered_by_basepath basepath, r_report_registered_by_doc doc,
-  r_data_main datapath
-where doc.object_id = size.data_id and basepath.object_id = size.data_id and datapath.data_id = doc.object_id and
-      size.meta_attr_name = 'source_file_size'
-      and user_id.meta_attr_name = 'registered_by'
-      and type.meta_attr_name = 'data_transfer_type'
-      and created.meta_attr_name = 'data_transfer_started'
-      and completed.meta_attr_name = 'data_transfer_completed'
-      and size.data_id = user_id.data_id and size.data_id = type.data_id and size.data_id = created.data_id and
-      size.data_id = completed.data_id
+  r_data_main data
+  join r_report_registered_by_basepath basepath on data.data_id=basepath.object_id
+  join r_report_registered_by_doc doc on data.data_id=doc.object_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'source_file_size') size on data.data_id=size.data_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'registered_by') user_id on data.data_id=user_id.data_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'data_transfer_type') transferType on data.data_id=transferType.data_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'data_transfer_started') created on data.data_id=created.data_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'data_transfer_completed') completed on data.data_id=completed.data_id
+  left join (select * from r_report_data_objects where meta_attr_name = 'source_file_container_id') sourceEndpoint on data.data_id=sourceEndpoint.data_id
 );
 CREATE UNIQUE INDEX r_report_registered_by_audit_uidx1 ON r_report_registered_by_audit(data_id);
