34f96f33e78509982296c8d45cc07dfb75b47a23
dinhys <yuri.dinh@nih.gov>
2019-12-26 19:23:40 -0500

    Revert "Created 1.22.0 release branch"
    This reverts commit 2ede81001ed34ba1c38f2ccd51fdd2b8acb5924b.

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "1.22.0";
+		return "1.21.0";
 	}
 
 	public String getWelcomeMessage() {
