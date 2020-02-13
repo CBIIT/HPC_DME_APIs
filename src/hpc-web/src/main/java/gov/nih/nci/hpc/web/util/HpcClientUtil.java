1ff2a9f3cdf120029d572e55f66b218d3ab38959
dinhys <dinhys@NCI-02096745-L.nci.nih.gov>
2020-02-13 18:32:46 -0500

    HPCDATAMGM-1215: Cancel download feature on GUI

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/util/HpcClientUtil.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/util/HpcClientUtil.java
@@ -1833,6 +1833,7 @@ public class HpcClientUtil {
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
     false);
     mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+    mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
     MappingJsonFactory factory = new MappingJsonFactory(mapper);
     JsonParser parser;
     try {
@@ -1859,6 +1860,38 @@ public class HpcClientUtil {
     }
   }
 
+  public static boolean cancelDownloadTask(String token,
+	      String hpcQueryURL, String hpcCertPath, String hpcCertPassword) {
+    try {
+
+        WebClient client = HpcClientUtil.getWebClient(hpcQueryURL, hpcCertPath, hpcCertPassword);
+        client.header("Authorization", "Bearer " + token);
+
+        Response restResponse = client.invoke("POST", "{}");
+
+      if (restResponse.getStatus() == 200) {
+        return true;
+      } else {
+        ObjectMapper mapper = new ObjectMapper();
+        AnnotationIntrospectorPair intr = new AnnotationIntrospectorPair(
+            new JaxbAnnotationIntrospector(TypeFactory.defaultInstance()),
+            new JacksonAnnotationIntrospector());
+        mapper.setAnnotationIntrospector(intr);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        MappingJsonFactory factory = new MappingJsonFactory(mapper);
+        JsonParser parser = factory.createParser((InputStream) restResponse.getEntity());
+
+        HpcExceptionDTO exception = parser.readValueAs(HpcExceptionDTO.class);
+        throw new HpcWebException("Failed to cancel download task: " + exception.getMessage());
+      }
+    } catch (HpcWebException e) {
+      throw e;
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new HpcWebException("Failed to cancel download task due to: " + e.getMessage());
+    }
+  }
 
   public static HpcMetadataAttributesListDTO getMetadataAttrNames(String token,
       String hpcMetadataAttrsURL, String hpcCertPath, String hpcCertPassword) {
