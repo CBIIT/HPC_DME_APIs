6ede54bc57e009e9739a082c332cf64cc9d5392f
Eran Rosenberg <eran.rosenberg@nih.gov>
2020-02-22 11:45:14 -0500

    HPCDATAMGM-1255 - Added fault interceptor to clean up file in case user cancels sync download on the fly

--- src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
+++ src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/impl/HpcDataManagementRestServiceImpl.java
@@ -75,9 +75,13 @@ public class HpcDataManagementRestServiceImpl extends HpcRestServiceImpl
   // Constants
   // ---------------------------------------------------------------------//
 
-  // The attribue name to save download file path on the message context.
+  // The attribute name to save download file path on the message context.
   public static final String DATA_OBJECT_DOWNLOAD_FILE_MC_ATTRIBUTE =
       "gov.nih.nci.hpc.ws.rs.impl.HpcDataManagementRestService.dataObjectDownloadFile";
+
+  // The attribute name to save download file path on the message context.
+  public static final String DATA_OBJECT_DOWNLOAD_TASK_ID_MC_ATTRIBUTE =
+      "gov.nih.nci.hpc.ws.rs.impl.HpcDataManagementRestService.dataObjectDownloadTaskId";
 
   // ---------------------------------------------------------------------//
   // Instance members
@@ -776,6 +780,8 @@ public class HpcDataManagementRestServiceImpl extends HpcRestServiceImpl
       // delete it after the file was received by the caller.
       messageContext.put(DATA_OBJECT_DOWNLOAD_FILE_MC_ATTRIBUTE,
           downloadResponse.getDestinationFile());
+      messageContext.put(DATA_OBJECT_DOWNLOAD_TASK_ID_MC_ATTRIBUTE,
+          downloadResponse.getTaskId());
       response = okResponse(downloadResponse.getDestinationFile(),
           MediaType.APPLICATION_OCTET_STREAM_TYPE);
     } else {
--- src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/interceptor/HpcCleanupFaultInterceptor.java
+++ src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/interceptor/HpcCleanupFaultInterceptor.java
@@ -1,5 +1,5 @@
 /**
- * HpcCleanupInterceptor.java
+ * HpcCleanupFaultInterceptor.java
  *
  * Copyright SVG, Inc. Copyright Leidos Biomedical Research, Inc
  * 
@@ -9,8 +9,6 @@
 
 package gov.nih.nci.hpc.ws.rs.interceptor;
 
-import java.io.File;
-import org.apache.commons.io.FileUtils;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
@@ -22,13 +20,13 @@ import gov.nih.nci.hpc.ws.rs.impl.HpcDataManagementRestServiceImpl;
 
 /**
  * <p>
- * HPC Cleanup Interceptor.
+ * HPC Cleanup Fault Interceptor.
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  */
 
-public class HpcCleanupInterceptor extends AbstractPhaseInterceptor<Message> {
+public class HpcCleanupFaultInterceptor extends AbstractPhaseInterceptor<Message> {
   // ---------------------------------------------------------------------//
   // Instance members
   // ---------------------------------------------------------------------//
@@ -48,28 +46,13 @@ public class HpcCleanupInterceptor extends AbstractPhaseInterceptor<Message> {
    * Default Constructor.
    * 
    */
-  private HpcCleanupInterceptor() {
-    super(Phase.SEND_ENDING);
+  private HpcCleanupFaultInterceptor() {
+    super(Phase.PREPARE_SEND);
   }
 
   // ---------------------------------------------------------------------//
   // Methods
   // ---------------------------------------------------------------------//
-
-  /**
-   * Delete the file staged for synchronous download.
-   *
-   * @param fileObj The file to delete
-   * @param logger Logger to log error if deletion failed.
-   */
-  static public void deleteSynchronousDownloadFile(Object fileObj, Logger logger) {
-    if (fileObj != null && fileObj instanceof File) {
-      File file = (File) fileObj;
-      if (!FileUtils.deleteQuietly(file)) {
-        logger.error("Failed to delete a file: " + file.getAbsolutePath());
-      }
-    }
-  }
 
   // ---------------------------------------------------------------------//
   // AbstractPhaseInterceptor<Message> Interface Implementation
@@ -77,11 +60,16 @@ public class HpcCleanupInterceptor extends AbstractPhaseInterceptor<Message> {
 
   @Override
   public void handleMessage(Message message) {
+    // Intentionally left empty.
+  }
+
+  @Override
+  public void handleFault(Message message) {
     // Close the connection to Data Management.
     systemBusService.closeConnection();
 
     // Clean up files returned by the data object download service.
-    deleteSynchronousDownloadFile(message.getContextualProperty(
+    HpcCleanupInterceptor.deleteSynchronousDownloadFile(message.getContextualProperty(
         HpcDataManagementRestServiceImpl.DATA_OBJECT_DOWNLOAD_FILE_MC_ATTRIBUTE), logger);
   }
 }
