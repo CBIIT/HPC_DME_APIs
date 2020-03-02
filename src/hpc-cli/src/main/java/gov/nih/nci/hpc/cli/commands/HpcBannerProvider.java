7b1b1ff99b19015b302e22a6b2d2418611c163e3
dinhys <yuri.dinh@nih.gov>
2020-03-02 11:40:50 -0500

    Updating to 1.24.0.

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "1.23.0";
+		return "1.24.0";
 	}
 
 	public String getWelcomeMessage() {
