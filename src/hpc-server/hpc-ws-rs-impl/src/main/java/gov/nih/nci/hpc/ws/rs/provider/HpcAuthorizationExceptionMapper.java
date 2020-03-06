bc1e7afbc51027de6f47e485f8e1b09f810370fd
Eran Rosenberg <eran.rosenberg@nih.gov>
2020-03-06 15:48:19 -0500

    HPCDATAMGM-1257 - Added an exception mapper to provide a standard HPC error message when sync upload size exceeds the limit

--- src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/provider/HpcAuthorizationExceptionMapper.java
+++ src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/provider/HpcAuthorizationExceptionMapper.java
@@ -1,11 +1,10 @@
 /**
  * HpcAuthorizationExceptionMapper.java
  *
- * Copyright SVG, Inc.
- * Copyright Leidos Biomedical Research, Inc
+ * Copyright SVG, Inc. Copyright Leidos Biomedical Research, Inc
  * 
- * Distributed under the OSI-approved BSD 3-Clause License.
- * See http://ncip.github.com/HPC/LICENSE.txt for details.
+ * Distributed under the OSI-approved BSD 3-Clause License. See
+ * http://ncip.github.com/HPC/LICENSE.txt for details.
  */
 
 package gov.nih.nci.hpc.ws.rs.provider;
@@ -20,55 +19,51 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 import org.apache.cxf.interceptor.security.AccessDeniedException;
+
 /**
  * <p>
- * Mapping HPC Exceptions to REST response.
+ * Mapping CXF Exception to REST response.
  * </p>
  *
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
- * @version $Id$
  */
 
 public class HpcAuthorizationExceptionMapper extends HpcExceptionMapper
-             implements ExceptionMapper<AccessDeniedException>
-{
-    //---------------------------------------------------------------------//
-    // Constructors
-    //---------------------------------------------------------------------//
-     
-    /**
-     * Constructor for Spring Dependency Injection.
-     * 
-     * @param stackTraceEnabled If set to true, stack trace will be attached to
-     *                          exception DTO.
-     */
-    public HpcAuthorizationExceptionMapper(boolean stackTraceEnabled)
-    {
-    	super(stackTraceEnabled);
-    }  
-    
-    //---------------------------------------------------------------------//
-    // Methods
-    //---------------------------------------------------------------------//
-    
-    //---------------------------------------------------------------------//
-    // ExceptionMapper<HpcAuthenticationException>
-    //---------------------------------------------------------------------//  
-    
-    @Override
-    public Response toResponse(AccessDeniedException exception) 
-    {
-        HpcExceptionDTO exceptionDTO = new HpcExceptionDTO();
-    	exceptionDTO.setMessage("Access Denied: " + exception.getMessage());
-    	exceptionDTO.setErrorType(HpcErrorType.REQUEST_AUTHENTICATION_FAILED);
-    	
-    	if(getStackTraceEnabled()) {
-           StringWriter writer = new StringWriter();
-           exception.printStackTrace(new PrintWriter(writer));
-     	   exceptionDTO.setStackTrace(writer.toString());
-     	}
-    	
-        return Response.status(Response.Status.UNAUTHORIZED).entity(exceptionDTO).
-        	   type(getAcceptedMediaType()).build();
+    implements ExceptionMapper<AccessDeniedException> {
+  // ---------------------------------------------------------------------//
+  // Constructors
+  // ---------------------------------------------------------------------//
+
+  /**
+   * Constructor for Spring Dependency Injection.
+   * 
+   * @param stackTraceEnabled If set to true, stack trace will be attached to exception DTO.
+   */
+  public HpcAuthorizationExceptionMapper(boolean stackTraceEnabled) {
+    super(stackTraceEnabled);
+  }
+
+  // ---------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------//
+
+  // ---------------------------------------------------------------------//
+  // ExceptionMapper<HpcAuthenticationException>
+  // ---------------------------------------------------------------------//
+
+  @Override
+  public Response toResponse(AccessDeniedException exception) {
+    HpcExceptionDTO exceptionDTO = new HpcExceptionDTO();
+    exceptionDTO.setMessage("Access Denied: " + exception.getMessage());
+    exceptionDTO.setErrorType(HpcErrorType.REQUEST_AUTHENTICATION_FAILED);
+
+    if (getStackTraceEnabled()) {
+      StringWriter writer = new StringWriter();
+      exception.printStackTrace(new PrintWriter(writer));
+      exceptionDTO.setStackTrace(writer.toString());
     }
+
+    return Response.status(Response.Status.UNAUTHORIZED).entity(exceptionDTO)
+        .type(getAcceptedMediaType()).build();
+  }
 }
