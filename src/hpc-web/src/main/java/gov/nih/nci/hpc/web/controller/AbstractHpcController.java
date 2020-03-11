f940dd24b33e4bf41e133b959f9b0a964afead2a
Menon <menons2@nci-02181184-ml.nih.gov>
2020-03-11 01:32:29 -0400

    HPCDATAMGM-1245: Include DOC in search criteria for user search

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/AbstractHpcController.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/AbstractHpcController.java
@@ -1,25 +1,36 @@
 package gov.nih.nci.hpc.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
+
+import gov.nih.nci.hpc.dto.security.HpcUserDTO;
+import gov.nih.nci.hpc.web.util.HpcClientUtil;
 
 public abstract class AbstractHpcController {
 	@Value("${gov.nih.nci.hpc.ssl.cert}")
 	protected String sslCertPath;
 	@Value("${gov.nih.nci.hpc.ssl.cert.password}")
 	protected String sslCertPassword;
+	@Value("${gov.nih.nci.hpc.server.model}")
+	private String hpcModelURL;
 	@Value("${hpc.SYSTEM_ADMIN}")
 	protected String SYSTEM_ADMIN;
 	@Value("${hpc.GROUP_ADMIN}")
 	protected String GROUP_ADMIN;
 	@Value("${hpc.USER}")
 	protected String USER;
+	
 
 	protected Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -41,5 +52,17 @@ public abstract class AbstractHpcController {
 		response.setHeader("Content-Type", "application/json");
 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		return new HpcResponse("Error occurred", ex.toString());
+	}
+	
+	
+	protected void populateDOCs(Model model, String authToken, HpcUserDTO user, HttpSession session) {
+		List<String> userDOCs = new ArrayList<String>();
+		if (user.getUserRole().equals("SYSTEM_ADMIN")) {
+			List<String> docs = HpcClientUtil.getDOCs(authToken, hpcModelURL, sslCertPath, sslCertPassword, session);
+			model.addAttribute("docs", docs);
+		} else {
+			userDOCs.add(user.getDoc());
+			model.addAttribute("docs", userDOCs);
+		}
 	}
 }
