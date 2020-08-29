bd16e4d6811320183aaacc9b121a701553bc7f93
Eran Rosenberg <eran.rosenberg@nih.gov>
2020-08-29 11:24:32 -0400

    Upgraded CXF and apache commons versions

--- src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/interceptor/HpcIPAddressRestrictionInterceptor.java
+++ src/hpc-server/hpc-ws-rs-impl/src/main/java/gov/nih/nci/hpc/ws/rs/interceptor/HpcIPAddressRestrictionInterceptor.java
@@ -9,8 +9,10 @@
 package gov.nih.nci.hpc.ws.rs.interceptor;
 
 import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
-import org.apache.commons.lang.StringUtils;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.interceptor.security.AccessDeniedException;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
@@ -22,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.util.CollectionUtils;
+
 import gov.nih.nci.hpc.domain.user.HpcUserRole;
 
 /**
@@ -30,68 +33,68 @@ import gov.nih.nci.hpc.domain.user.HpcUserRole;
  * @author <a href="mailto:yuri.dinh@nih.gov">Yuri Dinh</a>
  */
 public class HpcIPAddressRestrictionInterceptor extends AbstractPhaseInterceptor<Message> {
-  //---------------------------------------------------------------------//
-  // Instance members
-  //---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Instance members
+	// ---------------------------------------------------------------------//
 
-  //Restricted list of IP address.
-  @Value("#{'${hpc.ws.rs.auth.restrictedIPAddress}'.split(',')}")
-  private List<String> restrictedIPAddress = null;
+	// Restricted list of IP address.
+	@Value("#{'${hpc.ws.rs.auth.restrictedIPAddress}'.split(',')}")
+	private List<String> restrictedIPAddress = null;
 
-  //The logger instance.
-  private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+	// The logger instance.
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-  //---------------------------------------------------------------------//
-  // Constructors
-  //---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Constructors
+	// ---------------------------------------------------------------------//
 
-  /** Constructor for Spring Dependency Injection. */
-  public HpcIPAddressRestrictionInterceptor() {
-    super(Phase.RECEIVE);
-  }
+	/** Constructor for Spring Dependency Injection. */
+	public HpcIPAddressRestrictionInterceptor() {
+		super(Phase.RECEIVE);
+	}
 
-  //---------------------------------------------------------------------//
-  // Methods
-  //---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Methods
+	// ---------------------------------------------------------------------//
 
-  //---------------------------------------------------------------------//
-  // AbstractPhaseInterceptor<Message> Interface Implementation
-  //---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// AbstractPhaseInterceptor<Message> Interface Implementation
+	// ---------------------------------------------------------------------//
 
-  @Override
-  public void handleMessage(Message message) {
-    // Validate the caller's IP address and restrict user's role if required.
-    if (!CollectionUtils.isEmpty(restrictedIPAddress) && StringUtils.isNotEmpty(restrictedIPAddress.get(0))) {
-      try {
-        HttpServletRequest request =
-            (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
-        String remoteAddress = request.getRemoteAddr();
+	@Override
+	public void handleMessage(Message message) {
+		// Validate the caller's IP address and restrict user's role if required.
+		if (!CollectionUtils.isEmpty(restrictedIPAddress) && StringUtils.isNotEmpty(restrictedIPAddress.get(0))) {
+			try {
+				HttpServletRequest request = (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
+				String remoteAddress = request.getRemoteAddr();
 
-        if (StringUtils.isNotBlank(request.getHeader("X-Forwarded-For"))) {
-          remoteAddress = request.getHeader("X-Forwarded-For");
-        }
+				if (StringUtils.isNotBlank(request.getHeader("X-Forwarded-For"))) {
+					remoteAddress = request.getHeader("X-Forwarded-For");
+				}
 
-        if (isRestrictedIPAddress(remoteAddress)) {
-          // Set a security context with the restricted user's role.
-          HpcSecurityContext sc = new HpcSecurityContext(HpcUserRole.RESTRICTED.value());
-          message.put(SecurityContext.class, sc);
-        }
-      } catch (Exception e) {
-        logger.error("Error occurred during IP address restriction validation.", e);
-        throw new AccessDeniedException("Error occurred during IP address restriction validation.");
-      }
-    }
-  }
+				if (isRestrictedIPAddress(remoteAddress)) {
+					// Set a security context with the restricted user's role.
+					HpcSecurityContext sc = new HpcSecurityContext(HpcUserRole.RESTRICTED.value());
+					message.put(SecurityContext.class, sc);
+				}
+			} catch (Exception e) {
+				logger.error("Error occurred during IP address restriction validation.", e);
+				throw new AccessDeniedException("Error occurred during IP address restriction validation.");
+			}
+		}
+	}
 
-  //---------------------------------------------------------------------//
-  // Helper Methods
-  //---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Helper Methods
+	// ---------------------------------------------------------------------//
 
-  private boolean isRestrictedIPAddress(String remoteAddress) {
-    for (String ip : restrictedIPAddress) {
-      IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(ip);
-      if (ipAddressMatcher.matches(remoteAddress)) return true;
-    }
-    return false;
-  }
+	private boolean isRestrictedIPAddress(String remoteAddress) {
+		for (String ip : restrictedIPAddress) {
+			IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(ip);
+			if (ipAddressMatcher.matches(remoteAddress))
+				return true;
+		}
+		return false;
+	}
 }
