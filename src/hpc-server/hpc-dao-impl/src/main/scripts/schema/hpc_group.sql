e5e6946b71d18f4cafc1aa98912a26bfaafeb4be
Menon <menons2@nci-02181184-ml.nih.gov>
2020-09-24 15:08:00 -0400

    HPCDATAMGM-1366: Script for new table and data migration

---
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/schema/hpc_group.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS public."HPC_GROUP";
+create table PUBLIC."HPC_GROUP"
+(
+    "GROUP_NAME"                  text not null
+        constraint "HPC_GROUP_pkey"
+            primary key,
+    "CREATED"                  timestamp,
+    "LAST_UPDATED"             timestamp,
+    "DOC"                      text,
+    "ACTIVE"                   boolean,
+    "ACTIVE_UPDATED_BY"        text
+);
+
+comment on table "HPC_GROUP" is 'HPC-DME Groups';
+
+comment on column "HPC_GROUP"."GROUP_NAME" is 'The group name';
+
+comment on column "HPC_GROUP"."CREATED" is 'The date / time the group was created';
+
+comment on column "HPC_GROUP"."LAST_UPDATED" is 'The date / time the group was updated';
+
+comment on column "HPC_GROUP"."DOC" is 'The DOC the group belongs to';
+
+comment on column "HPC_GROUP"."ACTIVE" is 'User active indicator';
+
+comment on column "HPC_GROUP"."ACTIVE_UPDATED_BY" is 'The administrator user ID that activated this group';
+
+
+alter table "HPC_GROUP"
+    owner to irods;
+
