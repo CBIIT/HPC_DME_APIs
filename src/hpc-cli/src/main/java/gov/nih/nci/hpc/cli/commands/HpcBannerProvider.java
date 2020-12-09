e188fa2e2c7e2b8364118dad5a911ec5dec50e4a
Yuri Dinh <yuri.dinh@nih.gov>
2020-12-09 11:20:06 -0500

    Created branch 2.3.0

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "2.2.0";
+		return "2.3.0";
 	}
 
 	public String getWelcomeMessage() {
