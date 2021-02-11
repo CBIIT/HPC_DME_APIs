8f5cb5c44f115f9f18e76ee7563046ef20614938
Menon <menons2@nci-02181184-ml.nih.gov>
2021-02-11 11:15:17 -0500

    HPCDATAMGM-1427: Add ability to delete metadata during update

--- src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcMetadataServiceImpl.java
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcMetadataServiceImpl.java
@@ -158,7 +158,7 @@ public class HpcMetadataServiceImpl implements HpcMetadataService {
 	public void addMetadataToCollection(String path, List<HpcMetadataEntry> metadataEntries, String configurationId)
 			throws HpcException {
 		// Input validation.
-		if (path == null || !isValidMetadataEntries(metadataEntries)) {
+		if (path == null || !isValidMetadataEntries(metadataEntries, false)) {
 			throw new HpcException(INVALID_PATH_METADATA_MSG, HpcErrorType.INVALID_REQUEST_INPUT);
 		}
 
@@ -174,7 +174,7 @@ public class HpcMetadataServiceImpl implements HpcMetadataService {
 	public void updateCollectionMetadata(String path, List<HpcMetadataEntry> metadataEntries, String configurationId)
 			throws HpcException {
 		// Input validation.
-		if (path == null || !isValidMetadataEntries(metadataEntries)) {
+		if (path == null || !isValidMetadataEntries(metadataEntries, true)) {
 			throw new HpcException(INVALID_PATH_METADATA_MSG, HpcErrorType.INVALID_REQUEST_INPUT);
 		}
 
@@ -400,7 +400,7 @@ public class HpcMetadataServiceImpl implements HpcMetadataService {
 	public HpcMetadataEntry addMetadataToDataObject(String path, List<HpcMetadataEntry> metadataEntries,
 			String configurationId, String collectionType) throws HpcException {
 		// Input validation.
-		if (path == null || !isValidMetadataEntries(metadataEntries)) {
+		if (path == null || !isValidMetadataEntries(metadataEntries, false)) {
 			throw new HpcException(INVALID_PATH_METADATA_MSG, HpcErrorType.INVALID_REQUEST_INPUT);
 		}
 
@@ -698,7 +698,7 @@ public class HpcMetadataServiceImpl implements HpcMetadataService {
 	public void updateDataObjectMetadata(String path, List<HpcMetadataEntry> metadataEntries, String configurationId,
 			String collectionType, boolean extractedMetadata) throws HpcException {
 		// Input validation.
-		if (path == null || !isValidMetadataEntries(metadataEntries)) {
+		if (path == null || !isValidMetadataEntries(metadataEntries, true)) {
 			throw new HpcException(INVALID_PATH_METADATA_MSG, HpcErrorType.INVALID_REQUEST_INPUT);
 		}
 
