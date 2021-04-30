e9e7ffe85fc6320921cdb02eb4c83f94c778ec9d
Menon <menons2@nci-02181184-ml.nih.gov>
2021-04-30 03:32:57 -0400

    Fixed column name

--- src/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/oracle/impl/HpcDataManagementConfigurationDAOImpl.java
+++ src/hpc-server/hpc-dao-impl/src/main/java/gov/nih/nci/hpc/dao/oracle/impl/HpcDataManagementConfigurationDAOImpl.java
@@ -77,7 +77,7 @@ public class HpcDataManagementConfigurationDAOImpl implements HpcDataManagementC
 		// Map the Globus configuration.
 		HpcDataTransferConfiguration globusConfiguration = new HpcDataTransferConfiguration();
 		globusConfiguration.setUrlOrRegion(rs.getString("GLOBUS_URL"));
-		globusConfiguration.setEncryptedTransfer(rs.getBoolean("GLOBUS_ENCRYPED_TRANSFER"));
+		globusConfiguration.setEncryptedTransfer(rs.getBoolean("GLOBUS_ENCRYPTED_TRANSFER"));
 
 		HpcArchive globusBaseArchiveDestination = new HpcArchive();
 		HpcFileLocation globusArchiveLocation = new HpcFileLocation();
