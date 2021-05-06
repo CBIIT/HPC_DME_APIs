8368da83c6f87b8d35aff68c063b73a60bac3b5a
Eran Rosenberg <eran.rosenberg@nih.gov>
2021-05-06 17:46:33 -0400

    HPCDATAMGM-1460 - disable sudo for POSIX sync download until server config is ready

--- src/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/globus/impl/HpcDataTransferProxyImpl.java
+++ src/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/globus/impl/HpcDataTransferProxyImpl.java
@@ -246,24 +246,23 @@ public class HpcDataTransferProxyImpl implements HpcDataTransferProxy {
 			String archiveFilePath = downloadRequest.getArchiveLocation().getFileId().replaceFirst(
 					baseArchiveDestination.getFileLocation().getFileId(), baseArchiveDestination.getDirectory());
 			
+			/*
 			try {
-				//exec("mkdir -p " + archiveDirectory, sudoPassword);
 				exec("cp " + archiveFilePath + " " + downloadRequest.getFileDestination(), downloadRequest.getSudoPassword());
 
 			} catch (HpcException e) {
 				throw new HpcException(
 						"Failed to copy file from POSIX archive: " + archiveFilePath + "[" + e.getMessage() + "]",
 						HpcErrorType.DATA_TRANSFER_ERROR, e);
-			}
+			}*/
 			
-			/*
 			try {
 				// Copy the file to the download stage area.
 				FileUtils.copyFile(new File(archiveFilePath), downloadRequest.getFileDestination());
 			} catch (IOException e) {
 				throw new HpcException("Failed to stage file from file system archive: " + archiveFilePath,
 						HpcErrorType.DATA_TRANSFER_ERROR, e);
-			}*/
+			}
 
 			return String.valueOf(downloadRequest.getFileDestination().hashCode());
 
