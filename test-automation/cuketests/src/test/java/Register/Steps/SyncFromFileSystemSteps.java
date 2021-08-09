6a077dc1c58caec9398546297e9a0ab824dc8d0e
saradachintala <schintal@nih.gov>
2021-08-09 12:12:34 -0400

    Initial skeleton for Cucumber tests

---
+++ test-automation/cuketests/src/test/java/Register/Steps/SyncFromFileSystemSteps.java
@@ -0,0 +1,50 @@
+package Register.Steps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
+
+public class SyncFromFileSystemSteps {
+
+	@Given("I am a valid user with token")
+	public void i_am_a_valid_user_with_token() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I add base_path as {string}")
+	public void i_add_base_path_as(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I add collection_type as {string}")
+	public void i_add_collection_type_as(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I add data_file_path as {string}")
+	public void i_add_data_file_path_as(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I add meta_data as")
+	public void i_add_meta_data_as(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+
+}
