1d8315c1488f487232dd94a33e37d8cccdbcb40b
saradachintala <schintal@nih.gov>
2021-08-27 11:06:00 -0400

    Add Sync Upload

--- test-automation/cuketests/src/test/java/Register/Steps/SyncFromFileSystemSteps.java
+++ test-automation/cuketests/src/test/java/Register/Steps/SyncFromFileSystemSteps.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 
 import Register.Pojo.ParentMetadataPojo;
@@ -14,7 +17,6 @@ import Register.Pojo.RegisterPojo;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.messages.internal.com.google.gson.Gson;
 import io.cucumber.datatable.DataTable;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -32,19 +34,29 @@ public class SyncFromFileSystemSteps {
 	
 	RegisterPojo registerBody = new RegisterPojo();
 	String datafile;
-	String tokenStr = "xyz";
+	String sourcePath;
+	String destinationPath;
+	String tokenStr = "eyJhbGciOiJIUzI1NiJ9.eyJEYXRhTWFuYWdlbWVudEFjY291bnQiOiJ7XCJwYXNzd29yZFwiOlwiZGVhcmRhc2hzcGFya3l0YXJzXCIsXCJpbnRlZ3JhdGVkU3lzdGVtXCI6XCJJUk9EU1wiLFwicHJvcGVydGllc1wiOntcIlBST1hZX05BTUVcIjpcIlwiLFwiUE9SVFwiOlwiMTI0N1wiLFwiREVGQVVMVF9TVE9SQUdFX1JFU09VUkNFXCI6XCJkZW1vUmVzY1wiLFwiSE9NRV9ESVJFQ1RPUllcIjpcIlwiLFwiWk9ORVwiOlwidGVtcFpvbmVcIixcIkhPU1RcIjpcImZzZG1lbC1pcm9kczAxZC5uY2lmY3JmLmdvdlwiLFwiUFJPWFlfWk9ORVwiOlwiXCJ9LFwidXNlcm5hbWVcIjpcInNjaGludGFsXCJ9IiwiVXNlck5hbWUiOiJzY2hpbnRhbCIsIkRhdGFNYW5hZ2VtZW50QWNjb3VudEV4cGlyYXRpb24iOjE2MjcwMTE2MjQyNTYsImV4cCI6MTgwNjk4MjgyNH0.fjnw6bvqcsGtHBLBhDMCF_dTOVV2F_0HCgH4pM5RjsM";
 
 	@Given("I am a valid user with token")
 	public void i_am_a_valid_user_with_token() {
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new io.cucumber.java.PendingException();
-		//registerBody = new RegisterPojo();
+	    
+	}
+	
+	@Given("I add source_path as {string}")
+	public void i_add_source_path_as(String source_path) {
+	    this.sourcePath = source_path;
+	}
+
+	@Given("I add destination_path as {string}")
+	public void i_add_destination_path_as(String destination_path) {
+	    this.destinationPath = destination_path;
 	}
 
 	@Given("I add base_path as {string}")
 	public void i_add_base_path_as(String base_path) {
 	    //request.setCallerObjectId(base_path);
-		registerBody.setCallerObjectId(base_path);
+		registerBody.setBasePath(base_path);
 	}
 
 	@Given("I add collection_type as {string}")
@@ -82,28 +94,30 @@ public class SyncFromFileSystemSteps {
 		List<Map<String, String>> tablerows = dataTable.asMaps(String.class, String.class);
 		ParentMetadataPojo p = new ParentMetadataPojo();
 		p.setDefaultCollectionMetadataEntries(tablerows);
-		registerBody.setParentCollectionsBulkMetadataEntries(p);
+		//registerBody.setParentCollectionsBulkMetadataEntries(p);
 	}
 	
 	
 	@When("I click Register")
 	public void i_click_register() {
-	    // Write code here that turns the phrase above into concrete actions
-	    String token = this.tokenStr;
-		RestAssured.baseURI = "https://fsdmel-dsapi01d.ncifcrf.gov/";
-		//RestAssured.baseURI = "https://localhost/";
+	    
+		String token = this.tokenStr;
+		RestAssured.baseURI = "https://localhost/";
 	    RestAssured.port = 7738;
 	    RequestSpecification request = RestAssured.given().relaxedHTTPSValidation();
 		request.header("Accept", "application/json");
 		request.header("Authorization", "Bearer "+ token);
-		//request.header("Content-Type", "application/json");
-		request.multiPart("file", new File(this.datafile));
-	     Gson gson = new Gson();
-		 String registerBodyJson = gson.toJson(registerBody);
-		 System.out.println("Final JSON");
-		 System.out.println(registerBodyJson);
-	     
-		Response response = request.body(registerBodyJson).put("hpc-server/v2/dataObject");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String registerBodyJson = gson.toJson(registerBody);
+		System.out.println("Final JSON Body");
+		System.out.println(registerBodyJson);
+		// Setting multipart fields in the request
+		System.out.println(this.sourcePath);
+		System.out.println(this.destinationPath);
+		request.multiPart("dataObjectRegistration",registerBodyJson, "application/json");
+		request.multiPart("dataObject", new File(this.sourcePath), "application/octet-stream"); 
+		
+		Response response = request.put("/hpc-server/v2/dataObject"+ this.destinationPath);
 	    System.out.println(response.asString());
 	    System.out.println(response.getBody());
 	    System.out.println(response.getStatusCode());
