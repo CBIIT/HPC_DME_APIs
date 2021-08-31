02fa59c12f0743105b7e1177a6eca678bbed0684
Eran Rosenberg <eran.rosenberg@nih.gov>
2021-08-31 17:30:38 -0400

    Attempt to resolve class loader issue introduced w/ GAE

--- src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcMimeMessagePreparator.java
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcMimeMessagePreparator.java
@@ -12,12 +12,17 @@ package gov.nih.nci.hpc.service.impl;
 
 import java.util.List;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import gov.nih.nci.hpc.domain.notification.HpcEventPayloadEntry;
 import gov.nih.nci.hpc.domain.notification.HpcEventType;
 import gov.nih.nci.hpc.domain.notification.HpcSystemAdminNotificationType;
+import gov.nih.nci.hpc.exception.HpcException;
 
 /**
  * HPC MIME Message Preparator.
@@ -25,60 +30,77 @@ import gov.nih.nci.hpc.domain.notification.HpcSystemAdminNotificationType;
  * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
  */
 public class HpcMimeMessagePreparator {
-  // ---------------------------------------------------------------------//
-  // Constants
-  // ---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Constants
+	// ---------------------------------------------------------------------//
 
-  // NIH email domain
-  private static final String NIH_EMAIL_DOMAIN = "mail.nih.gov";
+	// NIH email domain
+	private static final String NIH_EMAIL_DOMAIN = "mail.nih.gov";
 
-  // ---------------------------------------------------------------------//
-  // Instance members
-  // ---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Instance members
+	// ---------------------------------------------------------------------//
 
-  // The notification formatter.
-  @Autowired
-  HpcNotificationFormatter notificationFormatter = null;
+	// The notification formatter.
+	@Autowired
+	HpcNotificationFormatter notificationFormatter = null;
 
-  // ---------------------------------------------------------------------//
-  // Methods
-  // ---------------------------------------------------------------------//
+	// ---------------------------------------------------------------------//
+	// Methods
+	// ---------------------------------------------------------------------//
 
-  /**
-   * Instantiate a MIME message preparator for an event.
-   *
-   * @param userId The recipient user ID
-   * @param eventType The event type to generate the message for.
-   * @param payloadEntries The payload entries to use for the message text and subject arguments.
-   * @return MimeMessagePreparator
-   */
-  public MimeMessagePreparator getPreparator(String userId, HpcEventType eventType,
-      List<HpcEventPayloadEntry> payloadEntries) {
-    return (mimeMessage) -> {
-      mimeMessage.setRecipient(Message.RecipientType.TO,
-          new InternetAddress(userId + "@" + NIH_EMAIL_DOMAIN));
-      mimeMessage.setSubject(notificationFormatter.formatSubject(eventType, payloadEntries));
-      mimeMessage.setText(notificationFormatter.formatText(eventType, payloadEntries), "UTF-8",
-          "html");
-      mimeMessage.setFrom(new InternetAddress("DME Notification <dme-notification@doNotReply.nih.gov>"));
-    };
-  }
+	/**
+	 * Instantiate a MIME message preparator for an event.
+	 *
+	 * @param userId         The recipient user ID
+	 * @param eventType      The event type to generate the message for.
+	 * @param payloadEntries The payload entries to use for the message text and
+	 *                       subject arguments.
+	 * @return MimeMessagePreparator
+	 */
+	public MimeMessagePreparator getPreparator(String userId, HpcEventType eventType,
+			List<HpcEventPayloadEntry> payloadEntries) throws HpcException {
+		/*
+		 * return (mimeMessage) -> { mimeMessage.setRecipient(Message.RecipientType.TO,
+		 * new InternetAddress(userId + "@" + NIH_EMAIL_DOMAIN));
+		 * mimeMessage.setSubject(notificationFormatter.formatSubject(eventType,
+		 * payloadEntries));
+		 * mimeMessage.setText(notificationFormatter.formatText(eventType,
+		 * payloadEntries), "UTF-8", "html"); mimeMessage.setFrom(new
+		 * InternetAddress("DME Notification <dme-notification@doNotReply.nih.gov>"));
+		 * };
+		 */
 
-  /**
-   * Instantiate a MIME message preparator for an event.
-   *
-   * @param userId The recipient user ID
-   * @param notificationType The system admin notification type to generate the message for.
-   * @param payloadEntries The payload entries to use for the message text and subject arguments.
-   * @return MimeMessagePreparator
-   */
-  public MimeMessagePreparator getPreparator(String userId,
-      HpcSystemAdminNotificationType notificationType, List<HpcEventPayloadEntry> payloadEntries) {
-    return (mimeMessage) -> {
-      mimeMessage.setRecipient(Message.RecipientType.TO,
-          new InternetAddress(userId + "@" + NIH_EMAIL_DOMAIN));
-      mimeMessage.setSubject(notificationFormatter.formatSubject(notificationType, payloadEntries));
-      mimeMessage.setText(notificationFormatter.formatText(notificationType, payloadEntries));
-    };
-  }
+		String subject = notificationFormatter.formatSubject(eventType, payloadEntries);
+		String text = notificationFormatter.formatText(eventType, payloadEntries);
+
+		return new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws MessagingException {
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+				message.setFrom("DME Notification <dme-notification@doNotReply.nih.gov>");
+				message.setTo(userId + "@" + NIH_EMAIL_DOMAIN);
+				message.setSubject(subject);
+				message.setText(text, true);
+			}
+		};
+	}
+
+	/**
+	 * Instantiate a MIME message preparator for an event.
+	 *
+	 * @param userId           The recipient user ID
+	 * @param notificationType The system admin notification type to generate the
+	 *                         message for.
+	 * @param payloadEntries   The payload entries to use for the message text and
+	 *                         subject arguments.
+	 * @return MimeMessagePreparator
+	 */
+	public MimeMessagePreparator getPreparator(String userId, HpcSystemAdminNotificationType notificationType,
+			List<HpcEventPayloadEntry> payloadEntries) {
+		return (mimeMessage) -> {
+			mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(userId + "@" + NIH_EMAIL_DOMAIN));
+			mimeMessage.setSubject(notificationFormatter.formatSubject(notificationType, payloadEntries));
+			mimeMessage.setText(notificationFormatter.formatText(notificationType, payloadEntries));
+		};
+	}
 }
