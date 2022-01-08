47467b7411810410e7332827d9191754e634ca80
Menon <menons2@nci-02181184-ml.nih.gov>
2022-01-07 23:02:44 -0500

    HPCDATAMGM-1541: Enable S3 download task to be run on different servers

--- src/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcDataTransferService.java
+++ src/hpc-server/hpc-app-service-api/src/main/java/gov/nih/nci/hpc/service/HpcDataTransferService.java
@@ -768,13 +768,14 @@ public interface HpcDataTransferService {
 	/**
 	 * Get inprocess data object download count.
 	 *
-	 * @param dataTransferType The data transfer type.
-	 * @param destinationType  The destination type.
+	 * @param dataTransferType  		The data transfer type.
+	 * @param destinationType   		The destination type.
+	 * @param s3DownloadTaskServerId  	The server ID of the executing s3 download task.
 	 * @return A total count of completed download requests.
 	 * @throws HpcException on database error.
 	 */
 	public int getInProcessDataObjectDownloadTasksCount(HpcDataTransferType dataTransferType,
-			HpcDataTransferType destinationType) throws HpcException;
+			HpcDataTransferType destinationType, String s3DownloadTaskServerId) throws HpcException;
 
 	/**
 	 * Get the download results page size.
