9ff7fe96db75a7e1c4bfe4b3ab9f1ae90f75ea71
saradachintala <schintal@nih.gov>
2022-02-14 10:08:20 -0500

    Add Aws bulk registration

--- test-automation/cuketests/src/test/java/Runners/RegisterRunner.java
+++ test-automation/cuketests/src/test/java/Runners/RegisterRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		 features="src/test/java/Register/Features"
-		, glue="Register.Steps"
+		, glue={"Register.Steps", "Auth"}
 		, dryRun = false
 		, monochrome=true
 		
