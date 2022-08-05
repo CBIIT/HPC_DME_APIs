e8b585465b09191da9e183a3722ceb263f81416b
Sarada <schintal@nih.gov>
2022-08-05 12:03:18 -0400

    Change banner to 2.23.0

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "2.21.0";
+		return "2.23.0";
 	}
 
 	public String getWelcomeMessage() {
