5c2cca76683392e5afef4e685b6b186b9347d3e3
Eran Rosenberg <eran.rosenberg@nih.gov>
2023-02-02 17:48:30 -0500

    Added a metadata retriever + iRODs impl of it, and refactored the code to use that retriever

---
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcIrodsMetadataRetrieverImpl.java
@@ -0,0 +1,69 @@
+/**
+ * HpcEmailNotificationSenderImpl.java
+ *
+ * <p>Copyright SVG, Inc. Copyright Leidos Biomedical Research, Inc
+ *
+ * <p>Distributed under the OSI-approved BSD 3-Clause License. See
+ * http://ncip.github.com/HPC/LICENSE.txt for details.
+ */
+package gov.nih.nci.hpc.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import gov.nih.nci.hpc.domain.notification.HpcEventPayloadEntry;
+import gov.nih.nci.hpc.domain.notification.HpcEventType;
+import gov.nih.nci.hpc.domain.notification.HpcSystemAdminNotificationType;
+import gov.nih.nci.hpc.exception.HpcException;
+
+/**
+ * HPC Email Notification Sender
+ *
+ * @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+ */
+public class HpcIrodsMetadataRetrieverImpl implements HpcNotificationSender {
+	// ---------------------------------------------------------------------//
+	// Instance members
+	// ---------------------------------------------------------------------//
+
+	// Mail Sender.
+	@Autowired
+	JavaMailSender mailSender = null;
+
+	// MIME Message Preparator.
+	@Autowired
+	HpcMimeMessagePreparator messagePreparator = null;
+
+	// ---------------------------------------------------------------------//
+	// Methods
+	// ---------------------------------------------------------------------//
+
+	// ---------------------------------------------------------------------//
+	// HpcNotificationSender Interface Implementation
+	// ---------------------------------------------------------------------//
+
+	@Override
+	public void sendNotification(String userId, HpcEventType eventType, String doc, List<HpcEventPayloadEntry> payloadEntries, String attachment)
+			throws HpcException {
+		try {
+			mailSender.send(messagePreparator.getPreparator(userId, eventType, doc, payloadEntries, attachment));
+
+		} catch (MailException e) {
+			throw new HpcException(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void sendNotification(String userId, HpcSystemAdminNotificationType notificationType,
+			List<HpcEventPayloadEntry> payloadEntries) throws HpcException {
+		try {
+			mailSender.send(messagePreparator.getPreparator(userId, notificationType, payloadEntries));
+
+		} catch (MailException e) {
+			throw new HpcException(e.getMessage(), e);
+		}
+	}
+}
