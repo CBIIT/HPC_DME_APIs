763a8c1e51e65480473e70eeeed27745716b1257
Sarada <schintal@nih.gov>
2023-05-11 10:27:21 -0400

    Add structures for aws registration

---
+++ test-automation/cuketests/src/test/java/Register/Pojo/DirectoryScanPathMapPojo.java
@@ -0,0 +1,12 @@
+package Register.Pojo;
+
+public class DirectoryScanPathMapPojo {
+	/*<xsd:complexType name="HpcDirectoryScanPathMap">
+		<xsd:sequence>
+			<xsd:element name="fromPath" type="xsd:string" />
+			<xsd:element name="toPath" type="xsd:string" />
+			<xsd:element name="regexPathMap" type="xsd:boolean" minOccurs="0" />
+		</xsd:sequence>
+	</xsd:complexType>*/
+
+}
