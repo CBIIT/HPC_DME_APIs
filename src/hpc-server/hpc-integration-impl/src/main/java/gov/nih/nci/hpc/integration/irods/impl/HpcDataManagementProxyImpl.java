17770d6eb7b58b05ce313148b48eec4c0834ebea
Eran Rosenberg <eran.rosenberg@nih.gov>
2023-08-02 18:16:10 -0400

    HPCDATAMGM-1802 - refined code to throw exception on path not found / not accessible and change impl of interrogatePathRef

--- src/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
+++ src/hpc-server/hpc-integration-impl/src/main/java/gov/nih/nci/hpc/integration/irods/impl/HpcDataManagementProxyImpl.java
@@ -348,28 +348,6 @@ public class HpcDataManagementProxyImpl implements HpcDataManagementProxy {
 	}
 
 	@Override
-	public boolean interrogatePathRef(Object authenticatedToken, String path) throws HpcException {
-		IRODSFile file = null;
-		try {
-			file = irodsConnection.getIRODSFileFactory(authenticatedToken).instanceIRODSFile(getAbsolutePath(path));
-			if (file.exists()) {
-				return file.isDirectory();
-			} else {
-				throw new HpcException(String.format("Failed to find item at following path: %s", path),
-						HpcErrorType.INVALID_REQUEST_INPUT);
-			}
-
-		} catch (JargonException e) {
-			throw new HpcException(String
-					.format("Failed to check whether item at following path is " + "collection or data file: %s", path),
-					HpcErrorType.INVALID_REQUEST_INPUT, e);
-
-		} finally {
-			closeIRODSFile(file);
-		}
-	}
-
-	@Override
 	public HpcPathAttributes getPathAttributes(Object authenticatedToken, String path) throws HpcException {
 		IRODSFile file = null;
 		HpcPathAttributes attributes = new HpcPathAttributes();
