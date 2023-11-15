7426ec8a895620a45e785d70c84bccc6ee724cb4
Eran Rosenberg <eran.rosenberg@nih.gov>
2023-11-15 17:31:11 -0500

    DB migration script for bulk download to aspera

---
+++ src/hpc-server/hpc-dao-impl/src/main/scripts/migration/release-2.38.0/hpc_data_object_download_task.sql
@@ -0,0 +1,36 @@
+--
+-- hpc_data_object_download_task.sql
+--
+-- Copyright SVG, Inc.
+-- Copyright Leidos Biomedical Research, Inc
+-- 
+-- Distributed under the OSI-approved BSD 3-Clause License.
+-- See http://ncip.github.com/HPC/LICENSE.txt for details.
+--
+--
+-- @author <a href="mailto:eran.rosenberg@nih.gov">Eran Rosenberg</a>
+--
+
+ALTER TABLE HPC_DATA_OBJECT_DOWNLOAD_TASK add (ASPERA_ACCOUNT_USER BLOB);
+COMMENT ON COLUMN HPC_DATA_OBJECT_DOWNLOAD_TASK.ASPERA_ACCOUNT_USER IS
+                 'The Aspera account user';
+                 
+ALTER TABLE HPC_DATA_OBJECT_DOWNLOAD_TASK add (ASPERA_ACCOUNT_PASSWORD BLOB);
+COMMENT ON COLUMN HPC_DATA_OBJECT_DOWNLOAD_TASK.ASPERA_ACCOUNT_PASSWORD IS
+                 'The Aspera account password';
+
+ALTER TABLE HPC_DATA_OBJECT_DOWNLOAD_TASK add (ASPERA_ACCOUNT_HOST VARCHAR2(250));
+COMMENT ON COLUMN HPC_DATA_OBJECT_DOWNLOAD_TASK.ASPERA_ACCOUNT_HOST IS
+                 'The Aspera account host';
+                 
+ALTER TABLE HPC_COLLECTION_DOWNLOAD_TASK add (ASPERA_ACCOUNT_USER BLOB);
+COMMENT ON COLUMN HPC_COLLECTION_DOWNLOAD_TASK.ASPERA_ACCOUNT_USER IS
+                 'The Aspera account user';
+                 
+ALTER TABLE HPC_COLLECTION_DOWNLOAD_TASK add (ASPERA_ACCOUNT_PASSWORD BLOB);
+COMMENT ON COLUMN HPC_COLLECTION_DOWNLOAD_TASK.ASPERA_ACCOUNT_PASSWORD IS
+                 'The Aspera account password';
+
+ALTER TABLE HPC_COLLECTION_DOWNLOAD_TASK add (ASPERA_ACCOUNT_HOST VARCHAR2(250));
+COMMENT ON COLUMN HPC_COLLECTION_DOWNLOAD_TASK.ASPERA_ACCOUNT_HOST IS
+                 'The Aspera account host';
