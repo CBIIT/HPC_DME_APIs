8a768771bb7fb71ae7a112002334f61edcca8a99
Sarada <schintal@nih.gov>
2023-11-27 18:16:20 -0500

    Add cucumber test for Aspera download

--- test-automation/cuketests/src/test/java/Download/pojos/DestinationLocationPojo.java
+++ test-automation/cuketests/src/test/java/Download/pojos/DestinationLocationPojo.java
@@ -2,12 +2,14 @@ package Download.pojos;
 
 import Register.Pojo.S3AccountPojo;
 import Register.Pojo.SourceLocationPojo;
+import common.AsperaAccountPojo;
 
 public class DestinationLocationPojo {
 
 	SourceLocationPojo destinationLocation;
-	S3AccountPojo account;
+	S3AccountPojo account; // AWS S3
 	String accessToken; // Used by Google Cloud and Google Drive
+	AsperaAccountPojo asperaAccount; // Used by DbGap Aspera
 
 	public SourceLocationPojo getDestinationLocation() {
 		return destinationLocation;
@@ -31,6 +33,14 @@ public class DestinationLocationPojo {
 
 	public void setAccessToken(String accessToken) {
 		this.accessToken = accessToken;
+	}
+
+	public AsperaAccountPojo getAsperaAccount() {
+		return asperaAccount;
+	}
+
+	public void setAsperaAccount(AsperaAccountPojo asperaAccount) {
+		this.asperaAccount = asperaAccount;
 	}
 
 }
