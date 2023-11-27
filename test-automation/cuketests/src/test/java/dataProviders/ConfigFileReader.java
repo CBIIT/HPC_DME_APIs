aa1d657c4537637841738023669488f598994dfb
Sarada <schintal@nih.gov>
2023-11-27 18:16:20 -0500

    Add cucumber test for Aspera download

--- test-automation/cuketests/src/test/java/dataProviders/ConfigFileReader.java
+++ test-automation/cuketests/src/test/java/dataProviders/ConfigFileReader.java
@@ -8,85 +8,125 @@ import java.util.Properties;
 import com.google.gson.Gson;
 
 public class ConfigFileReader {
-    
-    private Properties properties;
-    private final String propertyFilePath= System.getProperty("user.dir") + "/configs/Configuration.properties";
 
-    
-    public ConfigFileReader(){
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(propertyFilePath));
-            properties = new Properties();
-            try {
-                properties.load(reader);
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
-        }       
-    }
-    
-    public String getDriverPath(){
-        String driverPath = properties.getProperty("driverPath");
-        if(driverPath!= null) return driverPath;
-        else throw new RuntimeException("driverPath not specified in the Configuration.properties file.");      
-    }
-    
-    public long getImplicitlyWait() {       
-        String implicitlyWait = properties.getProperty("implicitlyWait");
-        if(implicitlyWait != null) return Long.parseLong(implicitlyWait);
-        else throw new RuntimeException("implicitlyWait not specified in the Configuration.properties file.");      
-    }
-    
-    public String getApplicationUrl() {
-        String url = properties.getProperty("url");
-        if(url != null) return url;
-        else throw new RuntimeException("url not specified in the Configuration.properties file.");
-    }
-    
-    public String getToken() {
-      String token = properties.getProperty("token");
-      if(token != null) return token;
-      else throw new RuntimeException("token not specified in the Configuration.properties file.");
-    }
+	private Properties properties;
+	private final String propertyFilePath = System.getProperty("user.dir") + "/configs/Configuration.properties";
 
-    public String getGoogleCloudToken() {
-      String refreshToken = properties.getProperty("refreshToken");
-      RefreshTokenPojo refreshTokenObj = new RefreshTokenPojo();
-      refreshTokenObj.client_id = properties.getProperty("googleClientId");
-      refreshTokenObj.client_secret = properties.getProperty("googleClientSecret");
-      refreshTokenObj.refresh_token = properties.getProperty("googleRefreshToken");  
-      Gson gson = new Gson();
-      String refreshTokenString = gson.toJson(refreshTokenObj);
-      refreshTokenString = properties.getProperty("googleRefreshTokenBlock"); //TEMP CODE
-      return refreshTokenString;
-    }
-    
-    public String getAwsAccessKey() {
-      String awsAccessKey = properties.getProperty("awsAccessKey");
-      if(awsAccessKey != null) return awsAccessKey;
-      else throw new RuntimeException("awsAccessKey not specified in the Configuration.properties file.");
-    }
+	public ConfigFileReader() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(propertyFilePath));
+			properties = new Properties();
+			try {
+				properties.load(reader);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+		}
+	}
 
-    public String getAwsSecretKey() {
-      String awsSecretKey = properties.getProperty("awsSecretKey");
-      if(awsSecretKey != null) return awsSecretKey;
-      else throw new RuntimeException("awsSecretKey not specified in the Configuration.properties file.");
-    }
+	public String getDriverPath() {
+		String driverPath = properties.getProperty("driverPath");
+		if (driverPath != null)
+			return driverPath;
+		else
+			throw new RuntimeException("driverPath not specified in the Configuration.properties file.");
+	}
 
-    public String getAwsRegion() {
-        String awsRegion = properties.getProperty("awsRegion");
-        if(awsRegion != null) return awsRegion;
-        else throw new RuntimeException("awsRegion not specified in the Configuration.properties file.");
-      }
+	public long getImplicitlyWait() {
+		String implicitlyWait = properties.getProperty("implicitlyWait");
+		if (implicitlyWait != null)
+			return Long.parseLong(implicitlyWait);
+		else
+			throw new RuntimeException("implicitlyWait not specified in the Configuration.properties file.");
+	}
 
-    public String getGoogleDriveAccessToken() {
-        String googleDriveAccessToken = properties.getProperty("googleDriveAccessToken");
-        if(googleDriveAccessToken != null) return googleDriveAccessToken;
-        else throw new RuntimeException("googleDriveAccessToken not specified in the Configuration.properties file.");
-      }
+	public String getApplicationUrl() {
+		String url = properties.getProperty("url");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("url not specified in the Configuration.properties file.");
+	}
+
+	public String getToken() {
+		String token = properties.getProperty("token");
+		if (token != null)
+			return token;
+		else
+			throw new RuntimeException("token not specified in the Configuration.properties file.");
+	}
+
+	public String getGoogleCloudToken() {
+		String refreshToken = properties.getProperty("refreshToken");
+		RefreshTokenPojo refreshTokenObj = new RefreshTokenPojo();
+		refreshTokenObj.client_id = properties.getProperty("googleClientId");
+		refreshTokenObj.client_secret = properties.getProperty("googleClientSecret");
+		refreshTokenObj.refresh_token = properties.getProperty("googleRefreshToken");
+		Gson gson = new Gson();
+		String refreshTokenString = gson.toJson(refreshTokenObj);
+		refreshTokenString = properties.getProperty("googleRefreshTokenBlock"); // TEMP CODE
+		return refreshTokenString;
+	}
+
+	public String getAwsAccessKey() {
+		String awsAccessKey = properties.getProperty("awsAccessKey");
+		if (awsAccessKey != null)
+			return awsAccessKey;
+		else
+			throw new RuntimeException("awsAccessKey not specified in the Configuration.properties file.");
+	}
+
+	public String getAwsSecretKey() {
+		String awsSecretKey = properties.getProperty("awsSecretKey");
+		if (awsSecretKey != null)
+			return awsSecretKey;
+		else
+			throw new RuntimeException("awsSecretKey not specified in the Configuration.properties file.");
+	}
+
+	public String getAwsRegion() {
+		String awsRegion = properties.getProperty("awsRegion");
+		if (awsRegion != null)
+			return awsRegion;
+		else
+			throw new RuntimeException("awsRegion not specified in the Configuration.properties file.");
+	}
+
+	public String getGoogleDriveAccessToken() {
+		String googleDriveAccessToken = properties.getProperty("googleDriveAccessToken");
+		if (googleDriveAccessToken != null)
+			return googleDriveAccessToken;
+		else
+			throw new RuntimeException("googleDriveAccessToken not specified in the Configuration.properties file.");
+	}
+
+	public String getAsperaUser() {
+		String dbgapUser = properties.getProperty("dbgapUser");
+		if (dbgapUser != null)
+			return dbgapUser;
+		else
+			throw new RuntimeException("dbgapUser not specified in the Configuration.properties file.");
+	}
+
+	public String getAsperaPassword() {
+		String dbgapPassword = properties.getProperty("dbgapPassword");
+		if (dbgapPassword != null)
+			return dbgapPassword;
+		else
+			throw new RuntimeException("dbgapPassword not specified in the Configuration.properties file.");
+	}
+
+	public String getAsperaHost() {
+		String dbgapHost = properties.getProperty("dbgapHost");
+		if (dbgapHost != null)
+			return dbgapHost;
+		else
+			throw new RuntimeException("dbgapHost not specified in the Configuration.properties file.");
+	}
+
 }
