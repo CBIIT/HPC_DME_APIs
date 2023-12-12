db75fbc6035024862985d8a9e320ebf250a7853e
Sarada <schintal@nih.gov>
2023-12-12 11:36:12 -0500

    Add Download test Runner file in cucumber

---
+++ test-automation/cuketests/src/test/java/Runners/DownloadRunner.java
@@ -0,0 +1,20 @@
+package Runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		 features="src/test/java/Download/Features/DownloadBulk.feature"
+		, glue={"Download.steps"}
+		, dryRun = false
+		, monochrome=true
+		, tags="@download"
+//,format= {"pretty","html:test-output_1", "json:target/cucumber-reports/CucumberTestReport.json"}
+,plugin = {"pretty", "json:target/cucumber-reports/CucumberTestReport.json",
+		 "html:target/cucumber-reports/cucumber-reports.html" }
+		)
+
+public class DownloadRunner {
+    
+}
