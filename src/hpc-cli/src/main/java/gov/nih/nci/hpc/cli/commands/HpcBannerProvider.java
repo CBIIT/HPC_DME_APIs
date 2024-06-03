462d242c110815027e5b123346bd47a382844b8c
Sarada <schintal@nih.gov>
2024-06-03 08:58:12 -0400

    Add version 3.3.0 to pom.xml, cli jars and banner

--- src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
+++ src/hpc-cli/src/main/java/gov/nih/nci/hpc/cli/commands/HpcBannerProvider.java
@@ -44,7 +44,7 @@ public class HpcBannerProvider extends DefaultBannerProvider {
 	}
 
 	public String getVersion() {
-		return "3.0.0";
+		return "3.3.0";
 	}
 
 	public String getWelcomeMessage() {
