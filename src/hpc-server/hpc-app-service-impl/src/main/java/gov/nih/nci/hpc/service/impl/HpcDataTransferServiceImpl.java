2679932ab6f4c4017f062e1455591423a5c528e4
Sarada <schintal@nih.gov>
2026-01-19 21:38:33 -0500

    HPCDATAMGM-2097: Temporarily commenting out 2097

--- src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataTransferServiceImpl.java
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcDataTransferServiceImpl.java
@@ -1168,13 +1168,14 @@ public class HpcDataTransferServiceImpl implements HpcDataTransferService {
 		}
 
 		// If from an external archive, delete the path from IRODs
+		/* Temporarily commenting cout
 		if (downloadTask.getExternalArchiveFlag()) {
 			try {
-				dataManagementService.delete(downloadTask.getPath(), true);
+				dataManagementService.delete(downloadTask.getPath(), false);
 			} catch (HpcException e) {
 				logger.error("Failed to delete file from datamanagement", e);
 			}
-		}
+		}*/
 		logger.info("2097: app:Transfer completeDataObjectDownloadTask: successfully deleted path from IRODs");
 
 		// Create a task result object.
