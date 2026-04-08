60b4fd608ab10aef0ed3a27be45ed32c4b7bc92f
Yuri Dinh <yuri.dinh@nih.gov>
2026-04-08 15:13:05 -0400

    HPCDATAMGM-2130: REST-API to update System Metadata for System Administrators

--- src/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcDataManagementBusServiceImpl.java
+++ src/hpc-server/hpc-bus-service-impl/src/main/java/gov/nih/nci/hpc/bus/impl/HpcDataManagementBusServiceImpl.java
@@ -3674,10 +3674,13 @@ public class HpcDataManagementBusServiceImpl implements HpcDataManagementBusServ
 		boolean updated = true;
 		String message = null;
 		try {
+			//If system admin, allow system metadata update, otherwise only allow user metadata update.
+			HpcRequestInvoker invoker = securityService.getRequestInvoker();
+			boolean allowSystemMetadataUpdate = HpcUserRole.SYSTEM_ADMIN.equals(invoker.getUserRole());
 			if (!metadataContained(metadataEntries, metadataBefore.getSelfMetadataEntries())) {
 				synchronized (this) {
 					metadataService.updateCollectionMetadata(path, metadataEntries,
-							systemGeneratedMetadata.getConfigurationId(), false);
+							systemGeneratedMetadata.getConfigurationId(), allowSystemMetadataUpdate);
 				}
 			} else {
 				logger.info(
@@ -3736,12 +3739,16 @@ public class HpcDataManagementBusServiceImpl implements HpcDataManagementBusServ
 					HpcErrorType.REQUEST_REJECTED).withSuppressStackTraceLogging(true);
 		}
 
+		
 		// Update the metadata.
 		boolean updated = true;
 		String message = null;
 		try {
+			//If system admin, allow system metadata update, otherwise only allow user metadata update.
+			HpcRequestInvoker invoker = securityService.getRequestInvoker();
+			boolean allowSystemMetadataUpdate = HpcUserRole.SYSTEM_ADMIN.equals(invoker.getUserRole());
 			metadataService.updateDataObjectMetadata(path, metadataEntries,
-					systemGeneratedMetadata.getConfigurationId(), collectionType, false, false);
+					systemGeneratedMetadata.getConfigurationId(), collectionType, false, allowSystemMetadataUpdate);
 
 		} catch (HpcException e) {
 			// Data object metadata update failed. Capture this in the audit record.
