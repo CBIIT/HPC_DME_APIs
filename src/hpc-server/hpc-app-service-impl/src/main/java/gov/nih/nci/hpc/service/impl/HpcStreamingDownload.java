9f804d42f490cb4c3f9477b8ca1ebabe611e1fac
Sarada <schintal@nih.gov>
2026-05-21 19:45:33 -0400

    HPCDATAMGM-2169: Save External Archive Flag in DB for downloads from external archives

--- src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcStreamingDownload.java
+++ src/hpc-server/hpc-app-service-impl/src/main/java/gov/nih/nci/hpc/service/impl/HpcStreamingDownload.java
@@ -215,6 +215,7 @@ public class HpcStreamingDownload implements HpcDataTransferProgressListener {
 		downloadTask.setFirstHopRetried(false);
 		downloadTask.setRetryTaskId(downloadRequest.getRetryTaskId());
 		downloadTask.setRetryUserId(downloadRequest.getRetryUserId());
+		downloadTask.setExternalArchiveFlag(downloadRequest.getExternalArchiveFlag());
 
 		if (downloadTask.getS3DownloadDestination() != null) {
 			downloadTask.setDataTransferType(HpcDataTransferType.S_3);
