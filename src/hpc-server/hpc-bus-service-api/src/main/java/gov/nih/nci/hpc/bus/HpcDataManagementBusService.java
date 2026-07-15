131434e077e2567b8543653bcf6d5bc11dc13069
Sarada <schintal@nih.gov>
2026-07-15 12:28:19 -0400

    HPCDATAMGM-2168: Determine user inputed path from the temporary archive link

--- src/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataManagementBusService.java
+++ src/hpc-server/hpc-bus-service-api/src/main/java/gov/nih/nci/hpc/bus/HpcDataManagementBusService.java
@@ -475,11 +475,10 @@ public interface HpcDataManagementBusService {
 	 * @param path            The data object path.
 	 * @param downloadRequest The download request DTO.
 	 * @param externalArchiveFlag      If true, the data object is in an external archive and the download will be handled differently.
-	 * @param userInputtedExternalPath  The user inputted external path for the data object. This is only set for external archive downloads.
 	 * @return Download ResponseDTO
 	 * @throws HpcException on service failure.
 	 */
-	public HpcDataObjectDownloadResponseDTO downloadDataObject(String path, HpcDownloadRequestDTO downloadRequest, boolean externalArchiveFlag, String userInputtedExternalPath)
+	public HpcDataObjectDownloadResponseDTO downloadDataObject(String path, HpcDownloadRequestDTO downloadRequest, boolean externalArchiveFlag)
 			throws HpcException;
 
 	/**
@@ -521,14 +520,13 @@ public interface HpcDataManagementBusService {
 	 * @param collectionDownloadTaskId (Optional) The collection download task ID if
 	 *                                 this request is part of a collection download
 	 *                                 task
-	 * @param externalArchiveFlag      If true, the data object is in an external
-	 * @param userInputtedExternalPath  The user inputted external path for the data object. This is only set for external archive downloads.
+	 * @param externalArchiveFlag      If true, the data object is in an external archive
 	 * @return Download ResponseDTO
 	 * @throws HpcException on service failure.
 	 */
 	public HpcDataObjectDownloadResponseDTO downloadDataObject(String path, HpcDownloadRequestDTO downloadRequest,
 			String retryTaskId, String userId, String retryUserId, boolean completionEvent,
-			String collectionDownloadTaskId, boolean externalArchiveFlag, String userInputtedExternalPath) throws HpcException;
+			String collectionDownloadTaskId, boolean externalArchiveFlag) throws HpcException;
 
 	/**
 	 * Download Data Object from External Source. In this overloaded method, the request invoker will be
