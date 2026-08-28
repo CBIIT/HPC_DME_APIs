c08b28fafaef70c89f276a7b2cc513da8b357e20
Sarada <schintal@nih.gov>
2026-08-28 11:11:45 -0400

    Add debug statement

--- src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcDownloadController.java
+++ src/hpc-web/src/main/java/gov/nih/nci/hpc/web/controller/HpcDownloadController.java
@@ -439,7 +439,7 @@ public class HpcDownloadController extends AbstractHpcController {
 				boxDestination.setAccessToken(accessBoxToken);
 				boxDestination.setRefreshToken(refreshBoxToken);
 				dto.setBoxDownloadDestination(boxDestination);
-			logger.debug("BOX JSON before downloading=" + gson.toJson(dto));				
+			logger.info("BOX JSON before downloading=" + gson.toJson(dto));				
 			}
 			if("collection".equals(downloadFile.getDownloadType())) {
 				if (downloadFile.getDownloadDestinationType() != null
